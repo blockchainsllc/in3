6603c238a114aaa5bab07b29913685f3e4a28f83
Simon Jentzsch <simon@slock.it>
2019-01-17 16:44:39 +0100

    debug

--- src/eth_full/call.c
+++ src/eth_full/call.c
@@ -10,6 +10,8 @@ void evm_free(evm_t* evm) {
   if (evm->return_data.data) _free(evm->return_data.data);
   if (evm->stack.b.data) _free(evm->stack.b.data);
   if (evm->memory.b.data) _free(evm->memory.b.data);
+  printf("#  FREE EVM 1 \n");
+
   logs_t* l;
   while (evm->logs) {
     l = evm->logs;
@@ -21,6 +23,7 @@ void evm_free(evm_t* evm) {
 
   account_t* ac = NULL;
   storage_t* s;
+  printf("#  FREE EVM 2 \n");
 
   while (evm->accounts) {
     ac = evm->accounts;
@@ -34,6 +37,8 @@ void evm_free(evm_t* evm) {
     evm->accounts = ac->next;
     _free(ac);
   }
+
+  printf("#  FREE EVM 3 \n");
 }
 
 #ifdef EVM_GAS
@@ -221,10 +226,12 @@ int evm_call(in3_vctx_t* vc,
              uint64_t  gas,
              bytes_t** result) {
   evm_t evm;
+  printf("#start EVM_CALL\n");
 
   int res = evm_prepare_evm(&evm, address, address, caller, caller, in3_get_env, vc);
 
 #ifdef EVM_GAS
+  printf("#EVM_GAS\n");
   evm.root = &evm;
   if (res == 0) res = transfer_value(&evm, caller, address, value, l_value);
 #endif
@@ -232,10 +239,13 @@ int evm_call(in3_vctx_t* vc,
   evm.gas            = gas;
   evm.call_data.data = data;
   evm.call_data.len  = l_data;
+  printf("#START_RUN %i\n", res);
   if (res == 0) res = evm_run(&evm);
+  printf("#END_RUN %i\n", res);
   if (res == 0 && evm.return_data.data)
     *result = b_dup(&evm.return_data);
   evm_free(&evm);
+  printf("#EVM_FREE %i\n", res);
 
   return res;
 }
