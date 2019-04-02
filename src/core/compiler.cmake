f16f0373a68be0f8470010c937fdf6a3c6272370
Shoaib Ahmed <shoaib.ahmed@slock.it>
2019-04-02 12:14:11 +0200

    Add add_dead_strip cmake function

--- src/core/compiler.cmake
+++ src/core/compiler.cmake
@@ -3,6 +3,25 @@ set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -Wall -funsigned-char -Wextra  -s
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 set_property(GLOBAL PROPERTY C_STANDARD 99)
 
+function(add_dead_strip)
+  if(NOT CYGWIN AND NOT MINGW)
+    if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+      SET(CMAKE_CXX_FLAGS
+              "${CMAKE_CXX_FLAGS}  -ffunction-sections -fdata-sections"
+              PARENT_SCOPE)
+    endif()
+  endif()
+  if(NOT LLVM_NO_DEAD_STRIP)
+    if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+      add_link_options(-Wl,-dead_strip)
+    elseif(NOT WIN32)
+      add_link_options(-Wl,--gc-sections)
+    endif()
+  endif()
+endfunction(add_dead_strip)
+
+add_dead_strip()
+
 OPTION(ZEPHYR "ZEPHYR" OFF)
 IF(ZEPHYR)
   add_definitions(-DZEPHYR_OS)
