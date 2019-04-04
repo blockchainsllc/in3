6920ab76dedb21dbf342672cc8709561ee30a148
Simon Jentzsch <simon@slock.it>
2019-04-04 12:07:31 +0200

    added usn_api

---
+++ src/usn_api/usn_api.h
@@ -0,0 +1,38 @@
+/** @file 
+ * USN API.
+ * 
+ * This header-file defines easy to use function, which are verifying USN-Messages.
+ * */
+
+#ifndef USN_API_H
+#define USN_API_H
+
+#include "../core/client/client.h"
+#include "../core/util/data.h"
+#include "../core/util/utils.h"
+
+typedef enum {
+  USN_ACTION,
+  USN_REQUEST,
+  USN_RESPONSE
+
+} usn_msg_type_t;
+
+typedef struct {
+  bool           accepted;
+  char*          error_msg;
+  char*          action;
+  usn_msg_type_t msg_type;
+} usn_msg_result_t;
+
+typedef struct {
+  char*     device_url;
+  address_t contract;
+  bytes32_t device_id;
+  uint64_t  chain_id;
+  uint64_t  now;
+} usn_device_conf_t;
+
+usn_msg_result_t usn_verify_message(in3_t* c, char* message, usn_device_conf_t* conf);
+
+#endif
