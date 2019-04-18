41e92e43f9cc5ab5affec58c8e7b0f64296ec2fa
Shoaib Ahmed <shoaib.ahmed@slock.it>
2019-04-18 19:44:35 +0200

    Add test to detect key collision

---
+++ test/test_key_hash.c
@@ -0,0 +1,96 @@
+#ifndef TEST
+#define TEST
+#endif
+
+#include <core/util/data.h>
+#include <core/util/mem.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "test_utils.h"
+
+static void test_key_hash_collisions();
+
+/*
+ * Main
+ */
+int main() {
+  TESTS_BEGIN();
+  RUN_TEST(test_key_hash_collisions);
+  return TESTS_END();
+}
+
+static uint16_t substrtokey(char* s, char* delim1, char* delim2) {
+  char *target, *start, *end;
+  target = start = end = NULL;
+  uint16_t k           = 0;
+
+  if ((start = strstr(s, delim1))) {
+    start += strlen(delim1);
+    if ((end = strstr(start, delim2))) {
+      target = start;
+      *end   = '\0';
+      k      = key(target);
+    }
+  }
+  return k;
+}
+
+static int compare(const void* a, const void* b) {
+  return (*(uint16_t*) a - *(uint16_t*) b);
+}
+
+static uint16_t collision(uint16_t* hashes, size_t sz) {
+  qsort(hashes, sz, sizeof(uint16_t), compare);
+
+  bool dup = false;
+  int  i   = 1;
+  for (; i < sz; ++i) {
+    if (hashes[i] && hashes[i] == hashes[i - 1]) {
+      dup = true;
+      break;
+    }
+  }
+  return dup ? hashes[i] : 0;
+}
+
+static char* filetostr(const char* filename) {
+  char* buffer = NULL;
+  long  length;
+  FILE* f = fopen(filename, "r");
+  if (f) {
+    fseek(f, 0, SEEK_END);
+    length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    buffer = _malloc(length);
+    if (buffer) {
+      fread(buffer, 1, length, f);
+    }
+    fclose(f);
+  }
+  return buffer;
+}
+
+void test_key_hash_collisions() {
+  size_t    cap    = 10;
+  uint16_t* hashes = _malloc(cap * sizeof(*hashes));
+  TEST_ASSERT(hashes != NULL);
+
+  const char* delim      = "\n";
+  char*       keyfilestr = filetostr("../src/core/client/keys.h");
+  TEST_ASSERT_MESSAGE(keyfilestr != NULL, "File keys.h not found!");
+
+  char* tok = strtok(keyfilestr, delim);
+  int   i   = 0;
+  for (; tok != NULL; i++, tok = strtok(NULL, delim)) {
+    if (i == cap) {
+      hashes = _realloc(hashes, cap * 2 * sizeof(*hashes), cap);
+      cap *= 2;
+    }
+    hashes[i] = substrtokey(tok, "key(\"", "\")");
+  }
+  _free(keyfilestr);
+
+  TEST_ASSERT_EQUAL(0, collision(hashes, i));
+  _free(hashes);
+}
---
+++ test/test_utils.h
@@ -0,0 +1,32 @@
+#ifndef IN3_TEST_UTILS_H
+#define IN3_TEST_UTILS_H
+
+#include "unity/unity.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TESTS_BEGIN() UNITY_BEGIN()
+#define TESTS_END() UNITY_END()
+#define TEST_LOG(fmt_, ...) printf("%s:%d:%s:LOG:" fmt_, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define TEST_LOG_INTERNAL(f_, fmt_, ...) printf("%s:%d:%s:LOG:" fmt_, __FILE__, __LINE__, f_, __VA_ARGS__)
+
+// Timing
+#define TIMING_START() gettimeofday(&begin, NULL)
+#define TIMING_END() gettimeofday(&end, NULL)
+#define TIMING_GET() ((double) (end.tv_usec - begin.tv_usec) / 1000000 + (double) (end.tv_sec - begin.tv_sec))
+
+#define RUN_TIMED_TEST(t)                                      \
+  do {                                                         \
+    TIMING_START();                                            \
+    RUN_TEST(t);                                               \
+    TIMING_END();                                              \
+    TEST_LOG_INTERNAL(#t, "Completed in %fs\n", TIMING_GET()); \
+  } while (0)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //IN3_TEST_UTILS_H
