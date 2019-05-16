ca7c71ceb74e641ca39c906ff23954312fca7e8d
Shoaib Ahmed <shoaib.ahmed@slock.it>
2019-05-16 14:40:31 +0200

    Make filter validation methods static

--- src/eth_basic/filter.h
+++ src/eth_basic/filter.h
@@ -7,7 +7,5 @@ in3_filter_opt_t* filter_opt_new(); /**< creates and returns a in3_filter_opt_t 
 in3_filter_t*     filter_new(in3_filter_type_t ft);
 size_t            filter_add(in3_t* in3, in3_filter_type_t type, in3_filter_opt_t* options);
 bool              filter_remove(in3_t* in3, size_t id);
-bool              filter_addrs_valid(d_token_t* addr);
-bool              filter_topics_valid(d_token_t* topics);
 
 #endif //FILTER_H
