e416ca4170435221b8771d4fb2a959b0b4dbf741
Klaus Geweniger <klaus.geweniger@slock.it>
2019-05-24 18:19:42 +0200

    zephyr-smartmeter compilable again

--- src/zephyr-smartmeter/src/meterReadings.h
+++ src/zephyr-smartmeter/src/meterReadings.h
@@ -26,4 +26,8 @@ extern void                         meterReadingsSetIN3(in3_t* pIN3);
 extern getContractVersion_RSP_t*    meterReadings_getContractVersion();
 extern getReading_RSP_t*            meterReadings_getLastReading();
 extern getReading_RSP_t*            meterReadings_getReading(uint32_t ixReading);
-extern uint64_t                     meterReadings_addReading(uint48,int24,int24,uint32):(uint256)
+uint64_t meterReadings_addReading(  uint64_t  _timestampYYYYMMDDhhmmss,
+                                    int32_t   _voltage_mV,
+                                    int32_t   _current_mA,
+                                    uint32_t  _counter_mWh
+                                  );
