6801df3378fc3aeecb6f733c1b43f23de0e4bb33
Klaus Geweniger <klaus.geweniger@slock.it>
2019-05-24 18:18:59 +0200

    zephyr-motor-boot compilable again

---
+++ src/zephyr-motor-boot/include_eth_nano.cmake
@@ -0,0 +1,11 @@
+
+target_sources( app PRIVATE 
+../eth_nano/eth_nano.c 
+../eth_nano/rlp.c 
+../eth_nano/merkle.c 
+../eth_nano/serialize.c 
+../eth_nano/blockheader.c
+../eth_nano/signature.c
+../eth_nano/txreceipt.c
+
+)
--- src/zephyr-motor-boot/src/rent.c
+++ src/zephyr-motor-boot/src/rent.c
@@ -175,7 +175,7 @@ int in3_can_rent(struct in3_client *c, char *resp, char *amsg) {
     // no event found,
 	if (!log) goto out; 
 
-	char* url         = d_get_string(message->items,"url");
+	char* url         = d_get_string(message->result,"url");
     //bytes_t* deviceId = d_get_bytes_at(d_get(log,key("topics")),1);  // we need to store and compare the deviceId
 	bytes_t* data     = d_get_bytes(log,"data");
 	
@@ -198,13 +198,13 @@ int in3_can_rent(struct in3_client *c, char *resp, char *amsg) {
 
 
     // prepare message hash
-	sprintf(tmp, "%s%d%s{}", url, c->rent->when, d_get_string(message->items,"action"));
+	sprintf(tmp, "%s%d%s{}", url, c->rent->when, d_get_string(message->result,"action"));
 	sprintf(mhash, "\031Ethereum Signed Message:\n%d%s", strlen(tmp), tmp);
 	bytes_t msg= { .data = (uint8_t*) &mhash, .len = strlen(mhash) };
     hash = sha3(&msg);
 
     // get the signature
-	signer = ecrecover_signature(hash,  d_get(message->items,key("signature")));
+	signer = ecrecover_signature(hash,  d_get(message->result,key("signature")));
 	if (signer==NULL) goto out; 
 
     // check if the signer is the same as the controller in the event.
