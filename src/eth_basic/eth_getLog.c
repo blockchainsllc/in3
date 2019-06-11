80e2989737b95991e63dfa62583ceed44a8e2074
Shoaib Ahmed <shoaib.ahmed@slock.it>
2019-06-11 17:03:26 +0200

    Check if eth_getLogs() result matches requested filter

--- src/eth_basic/eth_getLog.c
+++ src/eth_basic/eth_getLog.c
@@ -20,6 +20,83 @@ typedef struct receipt {
   uint32_t  transaction_index;
 } receipt_t;
 
+static bool matches_filter_address(d_token_t* tx_params, bytes_t addrs) {
+  d_token_t* jaddrs = d_getl(tx_params, K_ADDRESS, 20);
+  if (jaddrs == NULL) {
+    return true; // address param is optional
+  } else if (d_type(jaddrs) == T_BYTES) {
+    return !!bytes_cmp(addrs, d_to_bytes(jaddrs));
+  } else if (d_type(jaddrs) == T_ARRAY) { // must match atleast one in array
+    int l = d_len(jaddrs);
+    for (int i = 0; i < l; i++, jaddrs = d_next(jaddrs)) {
+      if (bytes_cmp(addrs, d_to_bytes(jaddrs))) return true;
+    }
+  }
+  return false;
+}
+
+static bool matches_filter_from_to(d_token_t* tx_params, const d_key_t k, uint64_t blockno) {
+  d_token_t* jrange = d_get(tx_params, k);
+  if (d_type(jrange) == T_INTEGER || d_type(jrange) == T_BYTES) {
+    if (k == K_FROM_BLOCK)
+      return blockno >= d_long(jrange);
+    else if (k == K_TO_BLOCK)
+      return blockno <= d_long(jrange);
+  }
+  return true;
+}
+
+static bool matches_filter_range(d_token_t* tx_params, uint64_t blockno, bytes_t blockhash) {
+  d_token_t* jblkhash = d_getl(tx_params, K_BLOCK_HASH, 32);
+  if (jblkhash == NULL) {
+    if (!matches_filter_from_to(tx_params, K_FROM_BLOCK, blockno) || !matches_filter_from_to(tx_params, K_TO_BLOCK, blockno)) {
+      return false;
+    }
+  } else if (d_type(jblkhash) == T_BYTES) {
+    return !!bytes_cmp(blockhash, d_to_bytes(jblkhash));
+  }
+  return false;
+}
+
+static bool matches_filter_topics(d_token_t* tx_params, d_token_t* topics) {
+  d_token_t* jts = d_get(tx_params, K_TOPICS);
+  int        l   = d_len(jts);
+  if (jts == NULL)
+    return true; // topics param is optional
+  else if (d_type(jts) != T_ARRAY)
+    return true; // Unlikely
+  else if (l == 0)
+    return true; // [] matches anything
+  else if (l > d_len(topics))
+    return false;
+
+  d_iterator_t it1 = d_iter(jts);
+  d_iterator_t it2 = d_iter(topics);
+  for (int i = 0; i < l; i++, d_iter_next(&it1), d_iter_next(&it2)) {
+    if (d_type(it1.token) == T_NULL) {
+      continue; // null matches anything in this position
+    } else if (d_type(it1.token) == T_BYTES && !bytes_cmp(d_to_bytes(it1.token), d_to_bytes(it2.token))) {
+      return false;
+    } else if (d_type(it1.token) == T_ARRAY) { // must match atleast one in array
+      bool found = false;
+      for (d_iterator_t it_ = d_iter(topics); it_.left; d_iter_next(&it_)) {
+        if (bytes_cmp(d_to_bytes(it_.token), d_to_bytes(it2.token))) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) return false;
+    }
+  }
+  return true;
+}
+
+static bool matches_filter(d_token_t* req, bytes_t addrs, uint64_t blockno, bytes_t blockhash, d_token_t* topics) {
+  d_token_t* tx_params = d_get(req, K_PARAMS);
+  if (!tx_params || d_type(tx_params + 1) != T_OBJECT) return false;
+  return matches_filter_address(tx_params + 1, addrs) && matches_filter_range(tx_params + 1, blockno, blockhash) && matches_filter_topics(tx_params + 1, topics);
+}
+
 in3_ret_t eth_verify_eth_getLog(in3_vctx_t* vc, int l_logs) {
   in3_ret_t res = IN3_OK, i = 0;
   receipt_t receipts[l_logs];
@@ -118,6 +195,8 @@ in3_ret_t eth_verify_eth_getLog(in3_vctx_t* vc, int l_logs) {
     if (!bytes_cmp(d_to_bytes(d_getl(it.token, K_BLOCK_HASH, 32)), bytes(r->block_hash, 32))) return vc_err(vc, "invalid blockhash");
     if (d_get_intk(it.token, K_REMOVED)) return vc_err(vc, "must be removed=false");
     if (d_get_intk(it.token, K_TRANSACTION_INDEX) != r->transaction_index) return vc_err(vc, "wrong transactionIndex");
+
+    if (matches_filter(vc->request, d_to_bytes(d_getl(it.token, K_ADDRESS, 20)), d_get_longk(it.token, K_BLOCK_NUMBER), d_to_bytes(d_getl(it.token, K_BLOCK_HASH, 32)), d_get(it.token, K_TOPICS))) return vc_err(vc, "filter mismatch");
   }
 
   return res;
