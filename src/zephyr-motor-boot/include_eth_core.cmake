fe1b4d816b05cc84278ebfa2c508fae0fc138012
Klaus Geweniger <klaus.geweniger@slock.it>
2019-06-13 01:22:34 +0200

    make zephyr-motor-boot compile-/linkable again

--- src/zephyr-motor-boot/include_eth_core.cmake
+++ src/zephyr-motor-boot/include_eth_core.cmake
@@ -10,6 +10,7 @@ target_sources( app PRIVATE
     ../core/util/data.c
     ../core/util/debug.c
     ../core/util/bytes.c
+    ../core/util/log.c
     ../core/util/scache.c
     ../core/util/utils.c
     ../core/util/mem.c
