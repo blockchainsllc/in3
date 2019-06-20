48c891c4f3e02bdeb5d9e9368f2ae67dcdef19d5
Shoaib Ahmed <shoaib.ahmed@slock.it>
2019-06-20 23:31:50 +0200

    Impl alternate diff strategy

--- test/test_bitset.c
+++ test/test_bitset.c
@@ -163,13 +163,16 @@ static char* filetostr(const char* filename) {
 }
 
 static in3_ret_t incache(uint8_t* varr, size_t l, uint8_t* v) {
-  for (int i = 0; i < l; i += 20)
-    if (!memcmp(varr + i, v, 20))
-      return (i / 20);
+  if (v) {
+    for (int i = 0; i < l; i += 20)
+      if (!memcmp(varr + i, v, 20))
+        return (i / 20);
+  }
   return IN3_EFIND;
 }
 
 static void printcache(uint8_t* varr, size_t l) {
+  printf("--- CACHE ---\n");
   for (int i = 0; i < l; i += 20) {
     ba_print(&varr[i], 20);
     printf("\n");
@@ -215,6 +218,7 @@ typedef struct {
   uint16_t pos;
   uint8_t* v;
   uint64_t blk;
+  size_t   len;
 } vdiff_t;
 
 typedef struct {
@@ -222,17 +226,22 @@ typedef struct {
   size_t   len;
 } vhist_t;
 
-in3_ret_t diff_emplace(vhist_t* h, uint16_t pos, bytes_t* b, uint64_t blk) {
+in3_ret_t diff_emplace(vhist_t* h, uint16_t pos, bytes_t* b, uint64_t blk, size_t len) {
   vdiff_t* d_ = _realloc(h->diffs, sizeof(*d_) * (h->len + 1), sizeof(*d_) * h->len);
   if (d_ == NULL) return IN3_ENOMEM;
   d_[h->len].pos = pos;
-  d_[h->len].v   = _malloc(20);
-  if (d_[h->len].v == NULL) {
-    _free(d_);
-    return IN3_ENOMEM;
+  if (b) {
+    d_[h->len].v = _malloc(20);
+    if (d_[h->len].v == NULL) {
+      _free(d_);
+      return IN3_ENOMEM;
+    }
+    memcpy(d_[h->len].v, b->data, b->len);
+  } else {
+    d_[h->len].v = NULL;
   }
-  memcpy(d_[h->len].v, b->data, b->len);
   d_[h->len].blk = blk;
+  d_[h->len].len = len;
   h->diffs       = d_;
   h->len++;
   return h->len;
@@ -247,22 +256,25 @@ static void test_vlist_diff() {
   bytes_builder_t* bb  = bb_new();
   bytes_t*         b   = NULL;
   in3_ret_t        ret = IN3_OK;
+  vhist_t          h   = {.diffs = NULL, .len = 0};
   int              i   = 0;
+  uint64_t         blk = 0;
 
   for (d_iterator_t sitr = d_iter(ss); sitr.left; d_iter_next(&sitr)) {
-    vs = d_get(sitr.token, K_VALIDATORS);
-    //    printf("----\n");
-    //    printf("Block [%" PRIu64 "]\n", d_get_longk(sitr.token, K_BLOCK));
+    vs  = d_get(sitr.token, K_VALIDATORS);
+    blk = d_get_longk(sitr.token, K_BLOCK);
     if (d_type(vs) == T_ARRAY) {
       i = 0;
       for (d_iterator_t vitr = d_iter(vs); vitr.left; d_iter_next(&vitr)) {
         b   = d_bytesl(vitr.token, 20);
         ret = incache(bb->b.data, bb->b.len, b->data);
         if (ret == IN3_EFIND) {
-          //          printf("+ %d\n", i);
+          printf("+ %d [%" PRIu64 "]\n", i, blk);
+          diff_emplace(&h, i, b, blk, d_len(vs) * 20);
           bb_write_fixed_bytes(bb, b);
         } else if (ret != i) {
-          //          printf("-/+ %d, %d\n", i, ret);
+          printf("-/+ %d, %d [%" PRIu64 "]\n", i, ret, blk);
+          diff_emplace(&h, i, b, blk, d_len(vs) * 20);
           bb_replace(bb, i * 20, 20, bb->b.data + (ret * 20), 20);
           for (int k = (ret * 20); k + 20 < bb->b.len / 20; k += 20) {
             memmove(&bb->b.data + k, &bb->b.data + k + 20, 20);
@@ -272,8 +284,31 @@ static void test_vlist_diff() {
       }
       bb->b.len = d_len(vs) * 20;
     }
-    printcache(bb->b.data, bb->b.len);
   }
+
+  bb_free(bb);
+  bb         = bb_new();
+  vdiff_t* d = NULL;
+  for (int j = 0; j < h.len; ++j) {
+    d = &h.diffs[j];
+    ret = incache(bb->b.data, bb->b.len, d->v);
+    if (ret == IN3_EFIND) {
+      printf("+ %d [%" PRIu64 "]\n", d->pos, d->blk);
+      bytes_t b_ = {.data = d->v, .len = 20};
+      bb_write_fixed_bytes(bb, &b_);
+    } else if (ret != d->pos) {
+      printf("-/+ %d, %d [%" PRIu64 "]\n", d->pos, ret, d->blk);
+      bb_replace(bb, d->pos * 20, 20, bb->b.data + (ret * 20), 20);
+      for (int k = (ret * 20); k + 20 < bb->b.len / 20; k += 20) {
+        memmove(&bb->b.data + k, &bb->b.data + k + 20, 20);
+      }
+    } else {
+      printf("In place... [%llu]\n", d->blk);
+    }
+    bb->b.len = d->len;
+  }
+
+  _free(h.diffs);
   bb_free(bb);
   _free(nodeliststr);
 }
@@ -283,11 +318,12 @@ static void test_vlist_diff() {
  */
 int main() {
   TESTS_BEGIN();
-  RUN_TEST(test_bitset_op);
-  RUN_TEST(test_bitset_transition);
-  RUN_TEST(test_bitset_realloc);
-  RUN_TEST(test_bitset_edge_cases);
-  RUN_TEST(test_bitset_clone);
-  RUN_TEST(test_bitset_out_of_range);
+  //  RUN_TEST(test_bitset_op);
+  //  RUN_TEST(test_bitset_transition);
+  //  RUN_TEST(test_bitset_realloc);
+  //  RUN_TEST(test_bitset_edge_cases);
+  //  RUN_TEST(test_bitset_clone);
+  //  RUN_TEST(test_bitset_out_of_range);
+  RUN_TEST(test_vlist_diff);
   return TESTS_END();
-}}
+}
