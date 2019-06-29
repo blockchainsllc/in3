b1397e376b0f2823eb00d8cd5d65013c15684252
Simon Jentzsch <simon@slock.it>
2019-06-29 22:34:56 +0200

    added windows build

---
+++ build_win.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+docker run \
+  --rm \
+  -v $(pwd):/src \
+  docker.slock.it/build-images/cmake:gcc7-mingw \
+  /bin/bash -c "cd /src; rm -rf build; mkdir build; cd build; cmake -DCMAKE_BUILD_TYPE=Release -DLIBCURL_LINKTYPE=static .. && make -j8 in3"
