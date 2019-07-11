60888912ad0c61be1bcb583f012171bb08856d98
Shoaib Ahmed <shoaib.ahmed@slock.it>
2019-07-11 19:17:21 +0200

    Automatically import dependency headers from public header

--- scripts/build_includeh.sh
+++ scripts/build_includeh.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 cd ..
-rm -rfv include
+rm -rf include
 mkdir -p include/in3
-find src -type d > include/in3/.dirs
+find src -type d >include/in3/.dirs
 cd include/in3
-xargs mkdir -p < .dirs
-cd ../../
-find src -name "*.h" > include/in3/.headers
-cd include/in3
-xargs touch < .headers
-
+xargs mkdir -p <.dirs
+cd ../..
+while read path; do
+  # find headers in path
+  find $path -name "*.h" -maxdepth 1 -exec bash -c 'for file in "$1";
+    do
+    # check if public header
+    if grep -q "@PUBLIC_HEADER" "$file"; then
+      cp "$file" "include/in3/$file"
+      projectdir=`pwd`
+      fdir=`dirname "$file"`
+      cd "$fdir"
+      # echo "$fdir"
+      # extract and copy public includes
+      grep "#include \"" `basename "$file"` | while read incl
+      do
+        header=`echo "$incl" | sed -e "s/.*include \"\(.*\)\".*/\1/"`
+        cp "$header" "$projectdir/include/in3/$fdir/$header"
+      done
+    fi
+    done' none {} \;
+done <include/in3/.dirs
+rm include/in3/.dirs
