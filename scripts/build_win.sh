5ce851f9cab2ab8c0dccf6686e7100fe75a8e680
Simon Jentzsch <simon@slock.it>
2019-07-25 22:13:18 +0200

    update releasetype

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -4,5 +4,5 @@ docker run \
   --rm \
   -v $(pwd):/src \
   docker.slock.it/build-images/cmake:gcc7-mingw \
-  /bin/bash -c "cd /src; rm -rf build; mkdir build; cd build; cmake -DCMAKE_BUILD_TYPE=Release -DLIBCURL_LINKTYPE=static .. && make -j8 in3"
+  /bin/bash -c "cd /src; rm -rf build; mkdir build; cd build; cmake -DCMAKE_BUILD_TYPE=MINSIZEREL -DUSE_CURL=true -DLIBCURL_LINKTYPE=static .. && make -j8 in3"
 cd scripts
