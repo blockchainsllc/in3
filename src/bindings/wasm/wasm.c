4302fdd393b28ff9c4bd6b2508951ce67bb7f78a
Simon Jentzsch <simon@slock.it>
2019-07-25 21:53:33 +0200

    add wasm

--- src/bindings/wasm/wasm.c
+++ src/bindings/wasm/wasm.c
@@ -1,35 +1,38 @@
+// example.cpp
 #include "../../core/client/client.h"
+#include "../../core/client/context.h"
+#include "../../core/client/send.h"
 #include "../../verifier/eth1/full/eth_full.h"
 #include <emscripten.h>
-#include <emscripten/fetch.h>
-#include <stdio.h>
-#include <string.h>
 
-int fetch(char** urls, int urls_len, char* payload, in3_response_t* result) {
+// clang-format off
+EM_JS(void, transport_send, (in3_response_t* result,  char* url, char* payload), {
+  Module.transport(UTF8ToString(url),UTF8ToString(payload))
+     .then(res => Module.ccall('request_set_result','void',['number','string'],[result,res]) )
+     .catch(res => Module.ccall('request_set_error','void',['number','string'],[result,res.message || res]) );
+})
+// clang-format on
 
-  const char* headers[3];
-  headers[0] = "Content-Type";
-  headers[1] = "application/json";
-  headers[2] = NULL;
-  int res    = 0;
-
-  for (int i = 0; i < urls_len; i++) {
-    emscripten_fetch_attr_t attr;
-    emscripten_fetch_attr_init(&attr);
-    strcpy(attr.requestMethod, "POST");
-    attr.requestData          = payload;
-    attr.attributes           = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY | EMSCRIPTEN_FETCH_SYNCHRONOUS;
-    attr.requestHeaders       = headers;
-    emscripten_fetch_t* fetch = emscripten_fetch(&attr, urls[i]); // Blocks here until the operation is complete.
-    if (fetch->status == 200)
-      sb_add_range(&result[i].result, fetch->data, 0, fetch->numBytes);
-    else {
-      sb_add_chars(&result[i].error, "Error fetching the RPC-call");
-      res = -1;
+int in3_fetch(char** urls, int urls_len, char* payload, in3_response_t* result) {
+  for (int i = 0; i < urls_len; i++)
+    transport_send(result + i, urls[i], payload);
+  while (true) {
+    emscripten_sleep(50);
+    int done = 0, err = 0;
+    for (int i = 0; i < urls_len; i++) {
+      if (result[i].error.len) err++;
+      if (result[i].error.len || result[i].result.len) done++;
     }
-    emscripten_fetch_close(fetch);
+    if (done == urls_len) return err ? IN3_ETRANS : IN3_OK;
   }
-  return res;
+  return IN3_OK;
+}
+
+static char* last_error = NULL;
+
+static void in3_set_error(char* data) {
+  if (last_error) free(last_error);
+  last_error = data ? _strdupn(data, -1) : NULL;
 }
 
 in3_t* EMSCRIPTEN_KEEPALIVE in3_create() {
@@ -37,46 +40,65 @@ in3_t* EMSCRIPTEN_KEEPALIVE in3_create() {
   in3_register_eth_full();
 
   in3_t* c     = in3_new();
-  c->transport = fetch;
-  printf("init in3\n");
+  c->transport = in3_fetch;
+  in3_set_error(NULL);
   return c;
 }
-static char* last_error;
+/* frees the references of the client */
+void EMSCRIPTEN_KEEPALIVE in3_dispose(in3_t* a) {
+  in3_free(a);
+  in3_set_error(NULL);
+}
 
 char* EMSCRIPTEN_KEEPALIVE in3_last_error() {
   return last_error;
 }
 
-/* sends a request and stores the result in the provided buffer */
-char* EMSCRIPTEN_KEEPALIVE in3_send(in3_t* c, char* method, char* params) {
-  if (last_error) free(last_error);
-  last_error   = NULL;
-  char *result = NULL, *error = NULL;
-  int   res = in3_client_rpc(c, method, params, &result, &error);
-  if (error) {
-    last_error = error;
-    return NULL;
-  } else if (res < 0 || !result) {
-    char* msg  = "unknown error while execxuting in3-request";
-    last_error = malloc(strlen(msg) + 1);
-    strcpy(last_error, msg);
+in3_ctx_t* EMSCRIPTEN_KEEPALIVE in3_create_request(in3_t* c, char* payload) {
+  char* src_data = _strdupn(payload, -1);
+  free(payload);
+  in3_ctx_t* ctx = new_ctx(c, src_data);
+  if (ctx->error) {
+    in3_set_error(ctx->error);
+    free_ctx(ctx);
     return NULL;
   }
-
-  return result;
+  return ctx;
 }
 
-/* frees the references of the client */
-void EMSCRIPTEN_KEEPALIVE in3_dispose(in3_t* a) {
-  in3_free(a);
+void EMSCRIPTEN_KEEPALIVE in3_send_request(in3_ctx_t* ctx) {
+  in3_set_error(NULL);
+  in3_send_ctx(ctx);
+  ctx->client = NULL;
 }
 
-int main(int argc, char* argv[]) {
-  (void) argc;
-  (void) argv;
-  in3_t* c   = in3_create();
-  char*  res = in3_send(c, "eth_blockNumber", "[]");
-  if (!res) res = in3_last_error();
-  printf("lastBlock : %s\n", res);
-  in3_dispose(c);
+void EMSCRIPTEN_KEEPALIVE in3_free_request(in3_ctx_t* ctx) {
+  if (ctx->request_context && ctx->request_context->c) free(ctx->request_context->c);
+  free_ctx(ctx);
+}
+
+bool EMSCRIPTEN_KEEPALIVE request_is_done(in3_ctx_t* r) {
+  return r->client == NULL;
+}
+
+char* EMSCRIPTEN_KEEPALIVE request_get_result(in3_ctx_t* r) {
+  if (r->error) return NULL;
+  // we have a result and copy it
+  str_range_t s = d_to_json(r->responses[0]);
+  s.data[s.len] = 0;
+  return s.data;
+}
+
+char* EMSCRIPTEN_KEEPALIVE request_get_error(in3_ctx_t* r) {
+  return r->error;
+}
+
+void EMSCRIPTEN_KEEPALIVE request_set_result(in3_response_t* r, char* data) {
+  sb_add_chars(&r->result, data);
+  free(data);
+}
+
+void EMSCRIPTEN_KEEPALIVE request_set_error(in3_response_t* r, char* data) {
+  sb_add_chars(&r->error, data);
+  free(data);
 }
