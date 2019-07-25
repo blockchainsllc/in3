e3a4a46015b1104b35c04912db4e812023fdce27
Simon Jentzsch <simon@slock.it>
2019-07-25 21:49:05 +0200

    fix tag

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-cd ..
+cd ..; rm -rf build/*
 docker run \
   --rm \
   -v $(pwd):/src \
   -u emscripten \
-  trzeci/emscripten \
-  /bin/bash -c "rm -rf build; mkdir build; cd build; emconfigure cmake -DWASM=true -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=false -DCMD=false -DEXAMPLES=false .. && make -j8"
-cd scripts
+  trzeci/emscripten:sdk-tag-1.38.32-64bit \
+  /bin/bash -c "cd build;  emconfigure cmake -DWASM=true -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=false -DCMD=false -DEXAMPLES=false -DCMAKE_BUILD_TYPE=MINSIZEREL .. && make -j8 in3w"
+
+cd build/bin
+../../scripts/merge_wasm.sh ../../src/bindings/wasm
+cd ../../scripts
