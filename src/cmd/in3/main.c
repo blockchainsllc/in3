bf0d5fc927dfde1eebe580356995313fd5806cb5
Simon Jentzsch <simon@slock.it>
2019-08-07 14:01:09 +0200

    add createkey

--- src/cmd/in3/main.c
+++ src/cmd/in3/main.c
@@ -9,6 +9,7 @@
 #include "../../core/util/log.h"
 #include "../../core/util/mem.h"
 #include "../../third-party/crypto/ecdsa.h"
+#include "../../third-party/crypto/rand.h"
 #include "../../third-party/crypto/secp256k1.h"
 #ifdef USE_CURL
 #include "../../transport/curl/in3_curl.h"
@@ -578,6 +579,13 @@ int main(int argc, char* argv[]) {
     return 0;
   } else if (strcmp(method, "autocompletelist") == 0) {
     printf("send call abi_encode abi_decode key keystore unlock pk2address mainnet tobalaba kovan goerli local volta true false latest -np -debug -c -chain -p -proof -s -signs -b -block -to -d -data -gas_limit -value -w -wait -hex -json in3_nodeList in3_stats in3_sign web3_clientVersion web3_sha3 net_version net_peerCount net_listening eth_protocolVersion eth_syncing eth_coinbase eth_mining eth_hashrate eth_gasPrice eth_accounts eth_blockNumber eth_getBalance eth_getStorageAt eth_getTransactionCount eth_getBlockTransactionCountByHash eth_getBlockTransactionCountByNumber eth_getUncleCountByBlockHash eth_getUncleCountByBlockNumber eth_getCode eth_sign eth_sendTransaction eth_sendRawTransaction eth_call eth_estimateGas eth_getBlockByHash eth_getBlockByNumber eth_getTransactionByHash eth_getTransactionByBlockHashAndIndex eth_getTransactionByBlockNumberAndIndex eth_getTransactionReceipt eth_pendingTransactions eth_getUncleByBlockHashAndIndex eth_getUncleByBlockNumberAndIndex eth_getCompilers eth_compileLLL eth_compileSolidity eth_compileSerpent eth_newFilter eth_newBlockFilter eth_newPendingTransactionFilter eth_uninstallFilter eth_getFilterChanges eth_getFilterLogs eth_getLogs eth_getWork eth_submitWork eth_submitHashrate\n");
+    return 0;
+  } else if (strcmp(method, "createkey") == 0) {
+    time_t t;
+    srand((unsigned) time(&t));
+    printf("0x");
+    for (i = 0; i < 32; i++) printf("%02x", rand() % 256);
+    printf("\n");
     return 0;
   } else if (strcmp(method, "pk2address") == 0) {
     bytes32_t prv_key;
