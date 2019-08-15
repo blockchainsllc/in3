aea8fa9fde3a39c55c514e4863b4910fa94cc2e4
Simon Jentzsch <simon@slock.it>
2019-08-15 09:03:23 +0200

    updated headers

--- include/in3/client.h
+++ include/in3/client.h
@@ -16,7 +16,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define IN3_PROTO_VER 0x1
+#define IN3_PROTO_VER 0x2
 
 /** the type of the chain. 
  * 
@@ -113,6 +113,8 @@ typedef struct in3_chain {
   in3_node_weight_t* weights;        /**< stats and weights recorded for each node */
   bytes_t**          initAddresses;  /**< array of addresses of nodes that should always part of the nodeList */
   bytes_t*           contract;       /**< the address of the registry contract */
+  bytes32_t          registry_id;    /**< the identifier of the registry */
+  uint8_t            version;        /**< version of the chain */
   json_ctx_t*        spec;           /**< optional chain specification, defining the transaitions and forks*/
 } in3_chain_t;
 
@@ -334,11 +336,13 @@ in3_ret_t in3_client_rpc(
 
 /** registers a new chain or replaces a existing (but keeps the nodelist)*/
 in3_ret_t in3_client_register_chain(
-    in3_t*           client,   /**< [in] the pointer to the incubed client config. */
-    uint64_t         chain_id, /**< [in] the chain id. */
-    in3_chain_type_t type,     /**< [in] the verification type of the chain. */
-    address_t        contract, /**< [in] contract of the registry. */
-    json_ctx_t*      spec      /**< [in] chainspec or NULL. */
+    in3_t*           client,      /**< [in] the pointer to the incubed client config. */
+    uint64_t         chain_id,    /**< [in] the chain id. */
+    in3_chain_type_t type,        /**< [in] the verification type of the chain. */
+    address_t        contract,    /**< [in] contract of the registry. */
+    bytes32_t        registry_id, /**< [in] the identifier of the registry. */
+    uint8_t          version,     /**< [in] the chain version. */
+    json_ctx_t*      spec         /**< [in] chainspec or NULL. */
 );
 
 /** adds a node to a chain ore updates a existing node */
@@ -368,5 +372,13 @@ void in3_free(in3_t* a /**< [in] the pointer to the incubed client config to fre
  *
  */
 in3_ret_t in3_cache_init(in3_t* c /**< the incubed client */);
+
+/**
+ * configures the clent based on a json-config.
+ * 
+ * For details about the structure of ther config see https://in3.readthedocs.io/en/develop/api-ts.html#type-in3config
+ * 
+ */
+in3_ret_t in3_configure(in3_t* c, char* config);
 
 #endif
