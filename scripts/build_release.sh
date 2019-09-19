eac42855dd6dbe271a9cf583279f128e13864194
pari <parithosh@slock.it>
2019-09-19 11:21:18 +0200

    removed make install in build release

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -4,7 +4,7 @@ mkdir -p build
 cd build
 rm -rf *
 export CTEST_OUTPUT_ON_FAILURE=1
-cmake -DCMAKE_BUILD_TYPE=MINSIZEREL -DIN3_SERVER=true .. && make && make install
+cmake -DCMAKE_BUILD_TYPE=MINSIZEREL -DIN3_SERVER=true .. && make
 #cmake -DTEST=true -DEVM_GAS=true -DCMAKE_BUILD_TYPE=Debug .. && make && make test
 #cmake -GNinja -DTEST=true -DCMAKE_BUILD_TYPE=Debug .. && ninja && ninja test
 #cmake -DTEST=true -DEVM_GAS=true -GNinja -DCMAKE_BUILD_TYPE=Release .. && ninja
