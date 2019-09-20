6bf8e533f01b2a5b88cf04af2a956d140fa17366
pari <parithosh@slock.it>
2019-09-20 14:57:29 +0200

    commented a bunch out, added dir

--- scripts/debian_dev_install.sh
+++ scripts/debian_dev_install.sh
@@ -5,7 +5,7 @@ set -ex # tells script to exit in case of a non-zero return value
 mkdir -p "$DESTDIR/usr/bin/in3-dev/" # create directory in case it doesn't exist, should always exist though
 mkdir -p "$DESTDIR/usr/lib/in3-dev" # create directory in case it doesn't exist, should always exist though
 #cp build/bin/in3 "$DESTDIR/usr/bin/in3-dev/" # move binary file to location where it can be used
-mkdir -p "$DESTDIR/usr/local/include/in3-dev/"
+mkdir -p "$DESTDIR/usr/include/in3-dev/"
 cp build/bin/in3 "$DESTDIR/usr/bin/" # move binary file to location where it can be used
 cp -r build/lib/*.a "$DESTDIR/usr/lib/in3-dev/" # moving lib files
-cp -r include/in3/* "$DESTDIR/usr/local/include/in3-dev/" # moving include files
+cp -r include/in3/*.h "$DESTDIR/usr/include/in3-dev/" # moving include files
