5c8da34d2cd1016e544903a1202173a4e9293cca
pari <parithosh@slock.it>
2019-09-20 09:14:53 +0200

    reproducing working build

--- scripts/debian_dev_install.sh
+++ scripts/debian_dev_install.sh
@@ -2,8 +2,7 @@
 ### This file is meant to be used by the debian package manager, It moves the binary file to the destination needed by debian
 
 set -ex # tells script to exit in case of a non-zero return value
-#mkdir -p "$DESTDIR/usr/bin" # create directory in case it doesn't exist, should always exist though
-mkdir -p "$DESTDIR/usr/lib/in3" # create directory to move lib files
-#mv build/bin/in3 build/bin/in3-dev
-#cp build/bin/in3-dev "$DESTDIR/usr/bin/in3-dev" # move binary file to location where it can be used
-cp -r build/lib/* "$DESTDIR/usr/lib/in3/" # moving archive files to libarary
+mkdir -p "$DESTDIR/usr/bin" # create directory in case it doesn't exist, should always exist though
+mkdir -p "$DESTDIR/usr/lib/in3" # create directory in case it doesn't exist, should always exist though
+cp build/bin/in3 "$DESTDIR/usr/bin/in3" # move binary file to location where it can be used
+cp -r build/lib/*.a "$DESTDIR/usr/lib/in3/" # moving lib files
