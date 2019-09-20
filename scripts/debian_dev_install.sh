744b95b96ddabfbfba1a2275f5c03f575ef63e8a
pari <parithosh@slock.it>
2019-09-20 14:41:18 +0200

    added folder and bumped version

--- scripts/debian_dev_install.sh
+++ scripts/debian_dev_install.sh
@@ -2,7 +2,7 @@
 ### This file is meant to be used by the debian package manager, It moves the binary file to the destination needed by debian
 
 set -ex # tells script to exit in case of a non-zero return value
-#mkdir -p "$DESTDIR/usr/bin/in3-dev/" # create directory in case it doesn't exist, should always exist though
+mkdir -p "$DESTDIR/usr/bin/in3-dev/" # create directory in case it doesn't exist, should always exist though
 mkdir -p "$DESTDIR/usr/lib/in3-dev" # create directory in case it doesn't exist, should always exist though
 #cp build/bin/in3 "$DESTDIR/usr/bin/in3-dev/" # move binary file to location where it can be used
 mkdir -p "$DESTDIR/usr/local/include/in3-dev/"
