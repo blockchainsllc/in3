9597533d4b0c48e919f2f24cf26c11c3bac98e81
pari <parithosh@slock.it>
2019-09-23 16:16:35 +0200

    adding dockerhub

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -3,9 +3,5 @@ cd ..
 mkdir -p build
 cd build
 rm -rf *
-export CTEST_OUTPUT_ON_FAILURE=1
 cmake -DCMAKE_BUILD_TYPE=MINSIZEREL -DIN3_SERVER=true .. && make
-#cmake -DTEST=true -DEVM_GAS=true -DCMAKE_BUILD_TYPE=Debug .. && make && make test
-#cmake -GNinja -DTEST=true -DCMAKE_BUILD_TYPE=Debug .. && ninja && ninja test
-#cmake -DTEST=true -DEVM_GAS=true -GNinja -DCMAKE_BUILD_TYPE=Release .. && ninja
 cd ../scripts
