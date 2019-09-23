0610610750f4552548458ff8536d286f3daa1d87
Simon Jentzsch <simon@slock.it>
2019-09-23 10:13:36 +0200

    added legal headers

--- src/verifier/eth1/evm/opcodes.h
+++ src/verifier/eth1/evm/opcodes.h
@@ -1,8 +1,42 @@
+/*******************************************************************************
+ * This file is part of the Incubed project.
+ * Sources: https://github.com/slockit/in3-c
+ *
+ * Copyright (C) 2019 slock.it GmbH, Blockchains LLC
+ * Authors Simon Jentzsch<simon@slock.it>
+ *
+ * COMMERCIAL LICENSE USAGE
+ * 
+ * Licensees holding a valid commercial license may use this file in accordance 
+ * with the commercial license agreement provided with the Software or, alternatively, 
+ * in accordance with the terms contained in a written agreement between you and 
+ * slock.it GmbH/Blockchains LLC. For licensing terms and conditions or further 
+ * information please contact slock.it at in3@slock.it.
+ * 	
+ * Alternatively, this file may be used under the AGPL license as follows:
+ *    
+ * AGPL LICENSE USAGE
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * [Permissions of this strong copyleft license are conditioned on making available 
+ * complete source code of licensed works and modifications, which include larger 
+ * works using a licensed work, under the same license. Copyright and license notices 
+ * must be preserved. Contributors provide an express grant of patent rights.]
+ * You should have received a copy of the GNU Affero General Public License along 
+ * with this program. If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *******************************************************************************/
+
 #ifndef opcodes_h__
 #define opcodes_h__
 
 #include "evm.h"
-
 
 /*
 int evm_ensure_memory(evm_t* evm, uint32_t max_pos) {
@@ -38,62 +72,58 @@ int evm_ensure_memory(evm_t* evm, uint32_t max_pos) {
 }
 */
 
+int op_math(evm_t* evm, uint8_t op, uint8_t mod);
 
+int op_signextend(evm_t* evm);
 
-int op_math(evm_t *evm, uint8_t op, uint8_t mod);
+int op_is_zero(evm_t* evm);
 
-int op_signextend(evm_t *evm);
+int op_not(evm_t* evm);
 
-int op_is_zero(evm_t *evm);
+int op_bit(evm_t* evm, uint8_t op);
 
-int op_not(evm_t *evm);
+int op_byte(evm_t* evm);
 
+int op_cmp(evm_t* evm, int8_t eq, uint8_t sig);
 
-int op_bit(evm_t *evm, uint8_t op);
+int op_shift(evm_t* evm, uint8_t left);
 
-int op_byte(evm_t *evm);
+int op_sha3(evm_t* evm);
 
-int op_cmp(evm_t *evm, int8_t eq, uint8_t sig);
+int op_account(evm_t* evm, uint8_t key);
 
-int op_shift(evm_t *evm, uint8_t left);
+int op_dataload(evm_t* evm);
 
-int op_sha3(evm_t *evm);
+int op_datacopy(evm_t* evm, bytes_t* src, uint_fast8_t check_size);
 
-int op_account(evm_t *evm, uint8_t key);
+int op_extcodecopy(evm_t* evm);
 
-int op_dataload(evm_t *evm);
+int op_header(evm_t* evm, uint8_t index);
 
-int op_datacopy(evm_t *evm, bytes_t *src, uint_fast8_t check_size);
+int op_mload(evm_t* evm);
 
-int op_extcodecopy(evm_t *evm);
+int op_mstore(evm_t* evm, uint8_t len);
 
-int op_header(evm_t *evm, uint8_t index);
+int op_sload(evm_t* evm);
 
-int op_mload(evm_t *evm);
+int op_sstore(evm_t* evm);
 
-int op_mstore(evm_t *evm, uint8_t len);
+int op_jump(evm_t* evm, uint8_t cond);
 
-int op_sload(evm_t *evm);
+int op_push(evm_t* evm, wlen_t len);
 
-int op_sstore(evm_t *evm);
+int op_dup(evm_t* evm, uint8_t pos);
 
-int op_jump(evm_t *evm, uint8_t cond);
+int op_swap(evm_t* evm, uint8_t pos);
 
-int op_push(evm_t *evm, wlen_t len);
+int op_log(evm_t* evm, uint8_t len);
 
-int op_dup(evm_t *evm, uint8_t pos);
+int op_return(evm_t* evm, uint8_t revert);
 
-int op_swap(evm_t *evm, uint8_t pos);
+int op_selfdestruct(evm_t* evm);
 
-int op_log(evm_t *evm, uint8_t len);
+int op_create(evm_t* evm, uint_fast8_t use_salt);
 
-int op_return(evm_t *evm, uint8_t revert);
-
-int op_selfdestruct(evm_t *evm);
-
-
-int op_create(evm_t *evm, uint_fast8_t use_salt);
-
-int op_call(evm_t *evm, uint8_t mode);
+int op_call(evm_t* evm, uint8_t mode);
 
 #endif
