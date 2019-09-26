60dc13db018becf45738bc38d1641e3a1eb413d3
Shoaib Ahmed <shoaib.ahmed@slock.it>
2019-09-26 12:51:11 +0200

    Add documentation

--- src/api/eth1/eth_api.h
+++ src/api/eth1/eth_api.h
@@ -11,6 +11,7 @@
 #include "../../core/client/client.h"
 #include <stdarg.h>
 
+/** Initializer macros for eth_blknum_t */
 #define BLKNUM(blk) ((eth_blknum_t){.u64 = blk, .is_u64 = true})
 #define BLKNUM_LATEST() ((eth_blknum_t){.def = BLK_LATEST, .is_u64 = false})
 #define BLKNUM_EARLIEST() ((eth_blknum_t){.def = BLK_EARLIEST, .is_u64 = false})
@@ -25,6 +26,7 @@ typedef struct {
   uint8_t data[32];
 } uint256_t;
 
+/** Optional types */
 DEFINE_OPTIONAL_T(uint64_t);
 DEFINE_OPTIONAL_T(bytes_t);
 DEFINE_OPTIONAL_T(address_t);
@@ -114,44 +116,46 @@ typedef struct {
   bool is_u64;
 } eth_blknum_t;
 
-uint256_t    eth_getStorageAt(in3_t* in3, address_t account, bytes32_t key, uint64_t block); /**< returns the storage value of a given address.*/
-bytes_t      eth_getCode(in3_t* in3, address_t account, uint64_t block);                     /**< returns the code of the account of given address. (Make sure you free the data-point of the result after use.) */
-uint256_t    eth_getBalance(in3_t* in3, address_t account, uint64_t block);                  /**< returns the balance of the account of given address. */
-uint64_t     eth_blockNumber(in3_t* in3);                                                    /**< returns the current price per gas in wei. */
-uint64_t     eth_gasPrice(in3_t* in3);                                                       /**< returns the current blockNumber, if bn==0 an error occured and you should check eth_last_error() */
-eth_block_t* eth_getBlockByNumber(in3_t* in3, uint64_t number, bool include_tx);             /**< returns the block for the given number (if number==0, the latest will be returned). If result is null, check eth_last_error()! otherwise make sure to free the result after using it! */
-eth_block_t* eth_getBlockByHash(in3_t* in3, bytes32_t hash, bool include_tx);                /**< returns the block for the given hash. If result is null, check eth_last_error()! otherwise make sure to free the result after using it! */
-eth_log_t*   eth_getLogs(in3_t* in3, char* fopt);                                            /**< returns a linked list of logs. If result is null, check eth_last_error()! otherwise make sure to free the log, its topics and data after using it! */
-char*        eth_wait_for_receipt(in3_t* in3, bytes32_t tx_hash);
-in3_ret_t    eth_newFilter(in3_t* in3, json_ctx_t* options);
-in3_ret_t    eth_newBlockFilter(in3_t* in3);                                                          /**< creates a new block filter with specified options and returns its id (>0) on success or 0 on failure */
-in3_ret_t    eth_newPendingTransactionFilter(in3_t* in3);                                             /**< creates a new pending txn filter with specified options and returns its id on success or 0 on failure */
-bool         eth_uninstallFilter(in3_t* in3, size_t id);                                              /**< uninstalls a filter and returns true on success or false on failure */
-in3_ret_t    eth_getFilterChanges(in3_t* in3, size_t id, bytes32_t** block_hashes, eth_log_t** logs); /**< sets the logs (for event filter) or blockhashes (for block filter) that match a filter; returns <0 on error, otherwise no. of block hashes matched (for block filter) or 0 (for log filer) */
-in3_ret_t    eth_getFilterLogs(in3_t* in3, size_t id, eth_log_t** logs);                              /**< sets the logs (for event filter) or blockhashes (for block filter) that match a filter; returns <0 on error, otherwise no. of block hashes matched (for block filter) or 0 (for log filer) */
-uint64_t     eth_chainId(in3_t* in3);
-uint64_t     eth_getBlockTransactionCountByHash(in3_t* in3, bytes32_t hash);
-uint64_t     eth_getBlockTransactionCountByNumber(in3_t* in3, eth_blknum_t block);
-json_ctx_t*  eth_call_fn(in3_t* in3, address_t contract, eth_blknum_t block, char* fn_sig, ...);     /**< returns the result of a function_call. If result is null, check eth_last_error()! otherwise make sure to free the result after using it with free_json()! */
-uint64_t     eth_estimate_fn(in3_t* in3, address_t contract, eth_blknum_t block, char* fn_sig, ...); /**< returns the result of a function_call. If result is null, check eth_last_error()! otherwise make sure to free the result after using it with free_json()! */
-eth_tx_t*    eth_getTransactionByHash(in3_t* in3, bytes32_t tx_hash);
-eth_tx_t*    eth_getTransactionByBlockHashAndIndex(in3_t* in3, bytes32_t block_hash, size_t index);
-eth_tx_t*    eth_getTransactionByBlockNumberAndIndex(in3_t* in3, eth_blknum_t block, size_t index);
-uint64_t     eth_getTransactionCount(in3_t* in3, address_t address, eth_blknum_t block);
-eth_block_t* eth_getUncleByBlockNumberAndIndex(in3_t* in3, bytes32_t hash, size_t index);
-uint64_t     eth_getUncleCountByBlockHash(in3_t* in3, bytes32_t hash);
-uint64_t     eth_getUncleCountByBlockNumber(in3_t* in3, eth_blknum_t block);
-bytes_t*     eth_sendTransaction(in3_t* in3, address_t from, address_t to, OPTIONAL_T(uint64_t) gas, OPTIONAL_T(uint64_t) gas_price, OPTIONAL_T(uint256_t) value, OPTIONAL_T(bytes_t) data, OPTIONAL_T(uint64_t) nonce);
-bytes_t*     eth_sendRawTransaction(in3_t* in3, bytes_t data);
+uint256_t         eth_getStorageAt(in3_t* in3, address_t account, bytes32_t key, uint64_t block);          /**< Returns the storage value of a given address.*/
+bytes_t           eth_getCode(in3_t* in3, address_t account, uint64_t block);                              /**< Returns the code of the account of given address. (Make sure you free the data-point of the result after use.) */
+uint256_t         eth_getBalance(in3_t* in3, address_t account, uint64_t block);                           /**< Returns the balance of the account of given address. */
+uint64_t          eth_blockNumber(in3_t* in3);                                                             /**< Returns the current price per gas in wei. */
+uint64_t          eth_gasPrice(in3_t* in3);                                                                /**< Returns the current blockNumber, if bn==0 an error occured and you should check eth_last_error() */
+eth_block_t*      eth_getBlockByNumber(in3_t* in3, uint64_t number, bool include_tx);                      /**< Returns the block for the given number (if number==0, the latest will be returned). If result is null, check eth_last_error()! otherwise make sure to free the result after using it! */
+eth_block_t*      eth_getBlockByHash(in3_t* in3, bytes32_t hash, bool include_tx);                         /**< Returns the block for the given hash. If result is null, check eth_last_error()! otherwise make sure to free the result after using it! */
+eth_log_t*        eth_getLogs(in3_t* in3, char* fopt);                                                     /**< Returns a linked list of logs. If result is null, check eth_last_error()! otherwise make sure to free the log, its topics and data after using it! */
+in3_ret_t         eth_newFilter(in3_t* in3, json_ctx_t* options);                                          /**< Creates a new event filter with specified options and returns its id (>0) on success or 0 on failure */
+in3_ret_t         eth_newBlockFilter(in3_t* in3);                                                          /**< Creates a new block filter with specified options and returns its id (>0) on success or 0 on failure */
+in3_ret_t         eth_newPendingTransactionFilter(in3_t* in3);                                             /**< Creates a new pending txn filter with specified options and returns its id on success or 0 on failure */
+bool              eth_uninstallFilter(in3_t* in3, size_t id);                                              /**< Uninstalls a filter and returns true on success or false on failure */
+in3_ret_t         eth_getFilterChanges(in3_t* in3, size_t id, bytes32_t** block_hashes, eth_log_t** logs); /**< Sets the logs (for event filter) or blockhashes (for block filter) that match a filter; returns <0 on error, otherwise no. of block hashes matched (for block filter) or 0 (for log filter) */
+in3_ret_t         eth_getFilterLogs(in3_t* in3, size_t id, eth_log_t** logs);                              /**< Sets the logs (for event filter) or blockhashes (for block filter) that match a filter; returns <0 on error, otherwise no. of block hashes matched (for block filter) or 0 (for log filter) */
+uint64_t          eth_chainId(in3_t* in3);                                                                 /**< Returns the currently configured chain id */
+uint64_t          eth_getBlockTransactionCountByHash(in3_t* in3, bytes32_t hash);                          /**< Returns the number of transactions in a block from a block matching the given block hash. */
+uint64_t          eth_getBlockTransactionCountByNumber(in3_t* in3, eth_blknum_t block);                    /**< Returns the number of transactions in a block from a block matching the given block number. */
+json_ctx_t*       eth_call_fn(in3_t* in3, address_t contract, eth_blknum_t block, char* fn_sig, ...);      /**< Returns the result of a function_call. If result is null, check eth_last_error()! otherwise make sure to free the result after using it with free_json()! */
+uint64_t          eth_estimate_fn(in3_t* in3, address_t contract, eth_blknum_t block, char* fn_sig, ...);  /**< Returns the result of a function_call. If result is null, check eth_last_error()! otherwise make sure to free the result after using it with free_json()! */
+eth_tx_t*         eth_getTransactionByHash(in3_t* in3, bytes32_t tx_hash);                                 /**< Returns the information about a transaction requested by transaction hash. If result is null, check eth_last_error()! otherwise make sure to free the result after using it! */
+eth_tx_t*         eth_getTransactionByBlockHashAndIndex(in3_t* in3, bytes32_t block_hash, size_t index);   /**< Returns the information about a transaction by block hash and transaction index position. If result is null, check eth_last_error()! otherwise make sure to free the result after using it! */
+eth_tx_t*         eth_getTransactionByBlockNumberAndIndex(in3_t* in3, eth_blknum_t block, size_t index);   /**< Returns the information about a transaction by block number and transaction index position. If result is null, check eth_last_error()! otherwise make sure to free the result after using it! */
+uint64_t          eth_getTransactionCount(in3_t* in3, address_t address, eth_blknum_t block);              /**< Returns the number of transactions sent from an address. */
+eth_block_t*      eth_getUncleByBlockNumberAndIndex(in3_t* in3, bytes32_t hash, size_t index);             /**< Returns information about a uncle of a block by number and uncle index position. If result is null, check eth_last_error()! otherwise make sure to free the result after using it! */
+uint64_t          eth_getUncleCountByBlockHash(in3_t* in3, bytes32_t hash);                                /**< Returns the number of uncles in a block from a block matching the given block hash. */
+uint64_t          eth_getUncleCountByBlockNumber(in3_t* in3, eth_blknum_t block);                          /**< Returns the number of uncles in a block from a block matching the given block number. */
+bytes_t*          eth_sendTransaction(in3_t* in3, address_t from, address_t to, OPTIONAL_T(uint64_t) gas,  /* */
+                                      OPTIONAL_T(uint64_t) gas_price, OPTIONAL_T(uint256_t) value,         /* */
+                                      OPTIONAL_T(bytes_t) data, OPTIONAL_T(uint64_t) nonce);               /**< Creates new message call transaction or a contract creation. Returns (32 Bytes) - the transaction hash, or the zero hash if the transaction is not yet available. Free result after use with b_free(). */
+bytes_t*          eth_sendRawTransaction(in3_t* in3, bytes_t data);                                        /**< Creates new message call transaction or a contract creation for signed transactions. Returns (32 Bytes) - the transaction hash, or the zero hash if the transaction is not yet available. Free after use with b_free(). */
+eth_tx_receipt_t* eth_getTransactionReceipt(in3_t* in3, bytes32_t tx_hash);                                /**< Returns the receipt of a transaction by transaction hash. Free result after use with free_tx_receipt() */
+char*             eth_wait_for_receipt(in3_t* in3, bytes32_t tx_hash);                                     /**< Waits for receipt of a transaction requested by transaction hash. */
+char*             eth_last_error();                                                                        /**< The current error or null if all is ok */
 
-eth_tx_receipt_t* eth_getTransactionReceipt(in3_t* in3, bytes32_t tx_hash);
-
-char*       eth_last_error();       /**< the current error or null if all is ok */
-long double as_double(uint256_t d); /**< converts a uint256_t in a long double. Important: since a long double stores max 16 byte, there is no garantee to have the full precision. */
-uint64_t    as_long(uint256_t d);   /**< converts a uint256_t in a long . Important: since a long double stores 8 byte, this will only use the last 8 byte of the value. */
-uint256_t   to_uint256(uint64_t value);
-in3_ret_t   decrypt_key(d_token_t* key_data, char* password, bytes32_t dst);
-void        free_log(eth_log_t* log);
-void        free_tx_receipt(eth_tx_receipt_t* txr);
+// Helper functions
+long double as_double(uint256_t d);                                          /**< Converts a uint256_t in a long double. Important: since a long double stores max 16 byte, there is no guarantee to have the full precision. */
+uint64_t    as_long(uint256_t d);                                            /**< Converts a uint256_t in a long . Important: since a long double stores 8 byte, this will only use the last 8 byte of the value. */
+uint256_t   to_uint256(uint64_t value);                                      /**< Converts a uint64_t into its uint256_t representation. */
+in3_ret_t   decrypt_key(d_token_t* key_data, char* password, bytes32_t dst); /**< Decrypts the private key from a json keystore file using PBKDF2 or SCRYPT (if enabled) */
+void        free_log(eth_log_t* log);                                        /**< Frees a eth_log_t object */
+void        free_tx_receipt(eth_tx_receipt_t* txr);                          /**< Frees a eth_tx_receipt_t object */
 
 #endif
