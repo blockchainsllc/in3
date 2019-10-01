6c16b1a49e544e5b63e661f9c30cddf7e17f112a
Simon Jentzsch <simon@slock.it>
2019-10-01 09:58:40 +0200

    no staging per default

--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -4,7 +4,7 @@ mkdir -p build
 cd build
 rm -rf *
 export CTEST_OUTPUT_ON_FAILURE=1
-cmake -DTEST=true -DUSE_SEGGER_RTT=false -DBUILD_DOC=true -DJAVA=true -DCMAKE_BUILD_TYPE=Debug -DIN3_STAGING=true .. -DLIBCURL_TYPE=shared && make && make ptest
+cmake -DTEST=true -DUSE_SEGGER_RTT=false -DBUILD_DOC=true -DJAVA=true -DCMAKE_BUILD_TYPE=Debug -DIN3_STAGING=false .. -DLIBCURL_TYPE=shared && make && make ptest
 #cmake -DTEST=true -DEVM_GAS=true -DCMAKE_BUILD_TYPE=Debug .. && make && make test
 #cmake -GNinja -DTEST=true -DCMAKE_BUILD_TYPE=Debug .. && ninja && ninja test
 #cmake -DTEST=true -DEVM_GAS=true -GNinja -DCMAKE_BUILD_TYPE=Release .. && ninja
