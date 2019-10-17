8631b5c6042b6a253284ea706dae2b63e0201b0a
Simon Jentzsch <simon@slock.it>
2019-10-17 21:26:37 +0200

    fixed options

--- scripts/analyse.sh
+++ scripts/analyse.sh
@@ -4,5 +4,5 @@ docker run \
   --rm \
   -v $(pwd):/src \
   docker.slock.it/build-images/cmake:clang9 \
-  /bin/bash -c "cd /src; rm -rf build; mkdir build; cd build; scan-build-9 cmake -DCMAKE_BUILD_TYPE=DEBUG -DIN3_SERVER=true .. && scan-build-9 -v --force-analyze-debug-code make -j8 && cp -r /tmp/scan-build* ."
+  /bin/bash -c "cd /src; rm -rf build; mkdir build; cd build; scan-build-9 cmake -DCMAKE_BUILD_TYPE=DEBUG -DIN3_SERVER=true .. &&  scan-build-9 --exclude ../src/third-party --force-analyze-debug-code -o report  make -j8"
 cd scripts
