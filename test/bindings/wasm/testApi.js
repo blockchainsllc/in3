0cc473082b62da057258586fbf93e97cb3d7c318
Simon Jentzsch <simon@slock.it>
2019-11-04 23:37:27 +0100

    refactored

--- test/bindings/wasm/testApi.js
+++ test/bindings/wasm/testApi.js
@@ -32,60 +32,14 @@
  * with this program. If not, see <https://www.gnu.org/licenses/>.
  *******************************************************************************/
 
-const IN3 = require('../../../build/bin/in3.js')
-const Client = IN3
 require('mocha')
 const { assert } = require('chai')
+const { createClient, mockResponse, IN3, beforeTest } = require('./util/mocker')
 
-let responses = {}
-function test_transport(url, data) {
-    return Promise.resolve(JSON.stringify(JSON.parse(data).map(r => {
-        const names = responses[r.method]
-        if (!names || !names.length) throw new Error('failing to find the request for ' + r.method)
-        const rdata = require('./responses/' + r.method + '.json')
-        const res = rdata[names[0]]
-        if (!res) throw new Error('Could not find response ' + names[0])
-        names.splice(0, 1)
-        return res
-    })))
-}
 
-function mockResponse(method, ...names) {
-    const n = responses[method] || (responses[method] = [])
-    names.forEach(_ => n.push(_))
-}
-
-function createClient(config = {}) {
-    const c = new IN3({
-        requestCount: 1,
-        autoUpdateList: false,
-        proof: 'standard',
-        chainId: '0x1',
-        autoUpdateList: false,
-        maxAttempts: 1,
-        signatureCount: 0,
-        nodes: {
-            '0x1': {
-                needsUpdate: false
-            },
-            '0x5': {
-                needsUpdate: false
-            },
-            '0x2a': {
-                needsUpdate: false
-            }
-        },
-        ...config
-    })
-    IN3.setTransport(test_transport)
-    return c
-}
 
 describe('API-Tests', () => {
-    beforeEach(done => {
-        responses = {}
-        IN3.onInit(done).catch(console.error)
-    })
+    beforeEach(beforeTest)
 
     it('checkSumAddress', async () => {
         IN3.onInit(() => {
