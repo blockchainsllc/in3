725968b0e3ac6e57e5cc3fc3a5e44dd4b03022fb
Simon Jentzsch <simon@slock.it>
2019-11-12 07:26:05 +0100

    cmake scripts

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -4,7 +4,6 @@ docker run \
   --rm \
   -v $(pwd):$(pwd) \
   docker.slock.it/build-images/cmake:clang10 \
-  /bin/bash -c "cd /$(pwd)/build;  emconfigure cmake -DWASM=true -DWASM_EMBED=false  -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DCMAKE_BUILD_TYPE=MINSIZEREL .. && make -j8 in3w"
+  /bin/bash -c "cd /$(pwd)/build;  emconfigure cmake -DWASM=true -DASMJS=false -DWASM_EMMALLOC=true  -DWASM_EMBED=false  -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DCMAKE_BUILD_TYPE=DEBUG .. && make -j8 in3w"
 
 cd scripts
-#  /bin/bash -c "cd /$(pwd)/build;  emconfigure cmake -DWASM=true -DWASM_EMBED=false  -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DCMAKE_BUILD_TYPE=MINSIZEREL .. && make -j8 in3w"
