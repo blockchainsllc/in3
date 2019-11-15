4c0bb85fa0a27058169dca0037d013668496eb39
Simon Jentzsch <simon@slock.it>
2019-11-15 12:49:14 +0100

    fix multiline

--- scripts/build_coverage.sh
+++ scripts/build_coverage.sh
@@ -4,5 +4,5 @@ docker run \
   --rm \
   -v $(pwd):/src \
   silkeh/clang:dev \
-  /bin/bash -c "cd /src; rm -rf build; mkdir build; cd build; cmake -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DTEST=true -DCODE_COVERAGE=true -DUSE_SEGGER_RTT=false -DTRANSPORTS=false -DCMAKE_BUILD_TYPE=Debug ..  && make -j8 &&  make ccov-all && make ccov-all-report && cat ccov/binaries.list | xargs llvm-cov export -instr-profile ccov/all-merged.profdata -format=lcov  > lcov.info; cp -r ccov/all-merged ../coverage_report"
+  /bin/bash -c "cd /src; rm -rf build; mkdir build; cd build; cmake -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DTEST=true -DCODE_COVERAGE=true -DUSE_SEGGER_RTT=false -DTRANSPORTS=false -DCMAKE_BUILD_TYPE=Debug ..  && make -j8 &&  make ccov-all && make ccov-all-report && cat ccov/binaries.list | xargs llvm-cov export -instr-profile ccov/all-merged.profdata -format=lcov  > lcov.info; wget https://raw.githubusercontent.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py ; python lcov_cobertura.py --base-dir ../src lcov.info"
 cd scripts
