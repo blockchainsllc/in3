7b64c1865c04a1cc6c7db4574162883119c11342
Simon Jentzsch <simon@slock.it>
2019-11-19 15:26:40 +0100

    format

--- src/bindings/python/in3.py
+++ src/bindings/python/in3.py
@@ -25,15 +25,21 @@ lib.in3_log_set_quiet_.argtypes = c_bool,
 lib.in3_log_set_quiet_.restype = None
 
 
+
+## adding a transport function..
+
+# the request struct
 class Request(Structure):
     _fields_ = [("payload", POINTER(c_char)),
                ("urls", POINTER(POINTER(c_char))),
                ("urls_len", c_int),
                ("results", c_void_p)]
 
+# declaring the function to report
 lib.in3_req_add_response.argtypes = [c_void_p,c_int,c_bool,c_void_p,c_int]
 lib.in3_req_add_response.restype = None
 
+# the function called for each request
 def transport(req):
     for i in range(0,req[0].urls_len):
         try:
@@ -42,41 +48,43 @@ def transport(req):
             lib.in3_req_add_response(req[0].results, i, False, c_char_p(res.content.decode("utf-8")) ,-1)
         except requests.exceptions.RequestException as e:
             lib.in3_req_add_response(req[0].results, i, True, e.strerror,-1)
-
     return 0
-    
 
 # define a transport function as callback
 transport_fn = CFUNCTYPE(c_int, POINTER(Request))(transport)
+# and set it as default
+lib.in3_set_default_transport(transport_fn)
 
+# register transport and verifiers (needed only once)
+lib.in3_register_eth_full()
+lib.in3_register_eth_api()
+lib.in3_log_set_quiet_(True)
 
 # test
 
 # create the in3-instance
-
 def in3_create():
-    # register transport and verifiers (needed only once)
-    lib.in3_register_eth_full()
-    lib.in3_register_eth_api()
-    lib.in3_set_default_transport(transport_fn)
-    lib.in3_log_set_quiet_(True)
-
     return lib.in3_new()
 
+# free the in3-instance
 def in3_free(in3_ptr):
     lib.in3_free(in3_ptr)
 
+# execute a rpc-request
 def in3_exec(in3_ptr, rpc):
     ptr_res = lib.in3_client_exec_req(in3_ptr, rpc)
     result = cast(ptr_res, c_char_p).value
     lib._free_(ptr_res)
     return result
 
+
+#example....
+
 # create a client
 in3 = in3_create()
 
 # run some rpc-requests
-print("Result:", in3_exec(in3,"{\"method\":\"eth_blockNumber\",\"params\":[]}"))
+print("current block:", in3_exec(in3,"{\"method\":\"eth_blockNumber\",\"params\":[]}"))
 
 # clean up when done
 in3_free(in3)
