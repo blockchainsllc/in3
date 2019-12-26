e04f33436749668e43d3213f561134c1cffb49e2
Simon Jentzsch <simon@slock.it>
2019-12-26 10:19:29 +0100

    use add_ptr

--- src/verifier/eth1/basic/eth_basic.c
+++ src/verifier/eth1/basic/eth_basic.c
@@ -159,10 +159,8 @@ in3_ret_t eth_handle_intern(in3_ctx_t* ctx, in3_response_t** response) {
     // set the new RPC-Request.
     ctx->request_context = parse_json(sb->data);
     ctx->requests[0]     = ctx->request_context->result;
-
-    // we add the request-string to the cache, to make sure the request-string will be cleaned afterwards
-    ctx->cache = in3_cache_add_entry(ctx->cache, bytes(NULL, 0), bytes((uint8_t*) sb->data, sb->len));
-    _free(sb);
+    ctx->cache           = in3_cache_add_ptr(ctx->cache, sb->data); // we add the request-string to the cache, to make sure the request-string will be cleaned afterwards
+    _free(sb);                                                      // and we only free the stringbuilder, but not the data itself.
   } else if (strcmp(d_get_stringk(req, K_METHOD), "eth_newFilter") == 0) {
     d_token_t* tx_params = d_get(req, K_PARAMS);
     if (!tx_params || d_type(tx_params + 1) != T_OBJECT)
