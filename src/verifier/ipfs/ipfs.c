0cddeaea7e4359ecdbdd2730a355ba05e3df8929
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-01-16 16:44:14 +0100

    Add placeholder executable to test build

---
+++ src/verifier/ipfs/ipfs.c
@@ -0,0 +1,15 @@
+
+#include "ipfs.pb.h"
+#include <pb_decode.h>
+#include <pb_encode.h>
+#include <stdio.h>
+
+int main() {
+  Data   data = Data_init_zero;
+  PBLink link = PBLink_init_zero;
+  PBNode node = PBNode_init_zero;
+  (void) data;
+  (void) link;
+  (void) node;
+  return 0;
+}
