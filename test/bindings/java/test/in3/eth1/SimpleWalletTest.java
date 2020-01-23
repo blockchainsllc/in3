fdded3fe057d08546316d42aaefa0b2a641cd51e
leonardocardoso <leonardo.t.cardoso@slock.it>
2020-01-23 13:13:35 +0100

    Build java unit test structure

---
+++ test/bindings/java/test/in3/eth1/SimpleWalletTest.java
@@ -0,0 +1,26 @@
+package in3.eth1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import in3.eth1.SimpleWallet;
+
+public class SimpleWalletTest {
+	private SimpleWallet wallet;
+
+	@BeforeEach
+	public void setupSigner() {
+		wallet = new SimpleWallet();
+	}
+
+    @Test
+    public void sign() {
+        String pk = "0x889dbed9450f7a4b68e0732ccb7cd016dab158e6946d16158f2736fda1143ca6";
+        String address = wallet.addRawKey(pk);
+
+        Assertions.assertEquals(address, "0x082977959d0C5A1bA627720ac753Ec2ADB5Bd7d0".toLowerCase());
+        Assertions.assertEquals(wallet.sign("1e194c68360307cfb715bf17878791ad1ced8da7d2e5f42b691074c577f41eac", address),
+             "0xf16dcaa830a3f710e28444df7df85fa927d8a66f789196fc2a3b934c829dbcaa5329be0711daba3b0c85ab23f1adb32c4e88fd8cb42b951d3be40af1bbd92e7400");
+    }
+}
