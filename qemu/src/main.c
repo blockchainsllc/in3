f5be571db511082564824758eb85dc137eaedd48
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-02-06 21:14:24 +0100

    format

--- qemu/src/main.c
+++ qemu/src/main.c
@@ -1,9 +1,8 @@
-#include "util/log.h"
-#include "util/debug.h"
-#include "util/mem.h"
 #include "eth_api.h"   //wrapper for easier use
 #include "eth_basic.h" // the full ethereum verifier containing the EVM
-//#include "eth_call_response_binary.h"
+#include "util/debug.h"
+#include "util/log.h"
+#include "util/mem.h"
 #include "block_number.h"
 #include "receipt.h"
 #define TEST_Z
@@ -14,26 +13,23 @@
  * In3 Setup and usage
  * **/
 /* Perform in3 requests for http transport */
-in3_ret_t local_transport_func(char **urls, int urls_len, char *payload, in3_response_t *result) {
-  for (int i=0; i<urls_len; i++) {
+in3_ret_t local_transport_func(char** urls, int urls_len, char* payload, in3_response_t* result) {
+  for (int i = 0; i < urls_len; i++) {
     if (strstr(payload, "eth_getTransactionReceipt") != NULL) {
       printk("Returning eth_getTransactionReceipt ...\n");
       sb_add_range(&(result[i].result), mock_tx_receipt, 0, mock_tx_receipt_len);
-    }
-    else if (strstr(payload, "eth_blockNumber") != NULL) {
+    } else if (strstr(payload, "eth_blockNumber") != NULL) {
       printk("Returning eth_blockNumber ...\n");
       sb_add_range(&(result[i].result), block_number_res, 0, block_number_res_len);
-		}
-    else {
-			printk("Not supported for this mock\n");
-		}     
+    } else {
+      printk("Not supported for this mock\n");
+    }
     //sb_add_range(&(result[i].result), mock_eth_call_response, 0, mock_eth_call_response_len);
-    
   }
   return IN3_OK;
 }
 
-in3_ret_t transport_mock(in3_request_t* req){
+in3_ret_t transport_mock(in3_request_t* req) {
   return local_transport_func((char**) req->urls, req->urls_len, req->payload, req->results);
 }
 
@@ -50,33 +46,27 @@ in3_t* init_in3(in3_transport_send custom_transport, chain_id_t chain) {
   in3->include_code     = 1;
   in3->max_attempts     = 1;
   in3->request_count    = 1; // number of requests to sendp
-  in3->chain_id         = chain;  
+  in3->chain_id         = chain;
   in3->auto_update_list = false;
-  in3->use_binary = true;
+  in3->use_binary       = true;
   for (int i = 0; i < in3->chains_length; i++) in3->chains[i].nodelist_upd8_params = NULL;
   return in3;
 }
-
 
 static void test_block_number() {
   in3_t*   in3    = init_in3(transport_mock, 0x5);
   uint64_t blknum = eth_blockNumber(in3);
   printk("BLKNUM : %lld \n", blknum);
-  zassert_true(blknum > 0 , "BLK TEST FAILED");
+  zassert_true(blknum > 0, "BLK TEST FAILED");
   in3_free(in3);
 }
 
-
-
-
 #ifdef TEST_Z
-void test_main(void)
-{
-	ztest_test_suite(framework_tests,
-		ztest_unit_test(test_block_number)
-	);
+void test_main(void) {
+  ztest_test_suite(framework_tests,
+                   ztest_unit_test(test_block_number));
 
-	ztest_run_test_suite(framework_tests);
+  ztest_run_test_suite(framework_tests);
 }
 #else
 void main() {
@@ -94,4 +84,3 @@ void main() {
   in3_free(in3);
 }
 #endif
-
