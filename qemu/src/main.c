0644bcd0e129077e9ddd863b5df516e7b4bfd47c
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-02-10 00:51:01 +0100

    fix sanity check path

--- qemu/src/main.c
+++ qemu/src/main.c
@@ -14,7 +14,7 @@
  * **/
 /* Perform in3 requests for http transport */
 in3_ret_t local_transport_func(char** urls, int urls_len, char* payload, in3_response_t* result) {
-  for (int i = 0; i < urls_len; i++) {
+  for (int i = 0; i < urls_len; i++) {    
     if (strstr(payload, "eth_getTransactionReceipt") != NULL) {
       printk("Returning eth_getTransactionReceipt ...\n");
       sb_add_range(&(result[i].result), mock_tx_receipt, 0, mock_tx_receipt_len);
