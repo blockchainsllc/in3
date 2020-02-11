2565b0c7a40680d1fa76c36fc192616ac07fdfd5
Simon Jentzsch <simon@slock.it>
2020-02-11 22:54:20 +0100

    typo

--- test/bindings/wasm/testUtil.js
+++ test/bindings/wasm/testUtil.js
@@ -37,29 +37,29 @@ const { assert } = require('chai')
 const { IN3, beforeTest } = require('./util/mocker')
 
 describe('Util-Tests', () => {
-    
+
     beforeEach(beforeTest)
-    
+
     afterEach(IN3.freeAll)
 
-      it('abi encode decode', async () => {
+    it('abi encode decode', async () => {
         const sig = "transfer(address,uint,string)"
         const data = "0x965D1C9987BD2c34e151E63d60AFf8E9dB6b1561,74323,Alice"
-        
+
         //const encodeStr = IN3.util.abiEncode(sig,data)
         // assert.equal(IN3.util.abiDecode(sig,encodeStr),data)
-      })
+    })
 
     it('splitSignature', async () => {
 
         let res = IN3.util.splitSignature("0xf596af3336ac65b01ff4b9c632bc8af8043f8c11ae4de626c74d834412cb5a234783c14807e20a9e665b3118dec54838bd78488307d9175dd1ff13eeb67e05941c"
-            ,"0x9fa034abf05bd334e60d92da257eb3d66dd3767bba9a1d7a7575533eb0977465")
+            , "0x9fa034abf05bd334e60d92da257eb3d66dd3767bba9a1d7a7575533eb0977465")
 
-        assert.equal(res.r,"0xf596af3336ac65b01ff4b9c632bc8af8043f8c11ae4de626c74d834412cb5a23")
-        assert.equal(res.s,"0x4783c14807e20a9e665b3118dec54838bd78488307d9175dd1ff13eeb67e0594")
-        assert.equal(res.v,28)
-        assert.equal(res.messageHash,"0xee966c673733bc2c23275a45f712006c0fecc62eba3660574ac738cc31c7c8b8")
-        
+        assert.equal(res.r, "0xf596af3336ac65b01ff4b9c632bc8af8043f8c11ae4de626c74d834412cb5a23")
+        assert.equal(res.s, "0x4783c14807e20a9e665b3118dec54838bd78488307d9175dd1ff13eeb67e0594")
+        assert.equal(res.v, 28)
+        assert.equal(res.messageHash, "0xee966c673733bc2c23275a45f712006c0fecc62eba3660574ac738cc31c7c8b8")
+
     })
 
     it('keccak & toHex', async () => {
@@ -68,17 +68,17 @@ describe('Util-Tests', () => {
     })
 
     it('getAddress', async () => {
-        assert.equal(IN3.util.private2address("0x3f64dd6972bda1e7611dc38a294d7e3404d51c4aff4b09534675ecd43f66d659"), 
+        assert.equal(IN3.util.private2address("0x3f64dd6972bda1e7611dc38a294d7e3404d51c4aff4b09534675ecd43f66d659"),
             "0xeebCfd8F610e497748989B7cbAF0633E644512E6")
     })
 
     it('toMinHex', async () => {
         const expectedHex = "0x203423"
-        assert.equal(IN3.util.toMinHex("0x00000203423"),expectedHex)
-        assert.equal(IN3.util.toMinHex("0x0000203423"),expectedHex)
+        assert.equal(IN3.util.toMinHex("0x00000203423"), expectedHex)
+        assert.equal(IN3.util.toMinHex("0x0000203423"), expectedHex)
 
         //assert.equal(IN3.util.toSimpleHex("0x00000203423"),expectedHex)
-        assert.equal(IN3.util.toSimpleHex("0x0000203423"),expectedHex)
+        assert.equal(IN3.util.toSimpleHex("0x0000203423"), expectedHex)
     })
 
     it('toUtf8', async () => {
@@ -86,15 +86,15 @@ describe('Util-Tests', () => {
         let res = IN3.util.toHex(testStr)
         assert.equal(IN3.util.toUtf8(res), testStr)
     })
- 
+
     it('padding', async () => {
         const testStr = "this is test"
 
-        let res = IN3.util.padStart(testStr,20)
-        assert.equal(res, "        "+testStr)
+        let res = IN3.util.padStart(testStr, 20)
+        assert.equal(res, "        " + testStr)
 
-        let res2 = IN3.util.padEnd(testStr,20)
-        assert.equal(res2, testStr+"        ")
+        let res2 = IN3.util.padEnd(testStr, 20)
+        assert.equal(res2, testStr + "        ")
     })
 
     it('checkSumAddress', async () => {
