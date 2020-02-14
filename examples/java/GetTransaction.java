37cc5a113e3eda7bf8f9bf2316b1c98a85c21082
Vineet Pant <vineetpant@Vineets-MacBook-Pro.local>
2020-02-14 17:02:29 +0100

    formatted code files

--- examples/java/GetTransaction.java
+++ examples/java/GetTransaction.java
@@ -4,43 +4,36 @@ import in3.eth1.*;
 import java.math.BigInteger;
 
 public class GetTransaction {
-  
-  static String TXN_HASH = "0xdd80249a0631cf0f1593c7a9c9f9b8545e6c88ab5252287c34bc5d12457eab0e";	
-  public static void main(String[] args) throws Exception {
-    // create incubed
-    IN3 in3 = IN3.forChain(Chain.MAINNET); // set it to mainnet (which is also dthe default)
 
-    
-    Transaction txn = getTransactionAPI(in3);
-    if(null != txn) {
-    	System.out.println("Block num "+txn.getBlockNumber());
-    }
-    
-    String rpcResponse = getTransactionRPC(in3);
-    
-    if(null != rpcResponse) {
-    	System.out.println(rpcResponse);
-    }
-  }
-  
-  static Transaction getTransactionAPI(IN3 in3) {
-	  
-	  return in3.
-			  getEth1API().
-			  getTransactionByHash(TXN_HASH);
+	static String TXN_HASH = "0xdd80249a0631cf0f1593c7a9c9f9b8545e6c88ab5252287c34bc5d12457eab0e";
 
-	  
-  }
-  
-  static String getTransactionRPC(IN3 in3) {
-	  
-	  
-	  return in3.sendRPC("eth_getTransactionByHash", 
-			  new Object[]{ 
-					  TXN_HASH
-			  		 
-			  		 });
-  }
+	public static void main(String[] args) throws Exception {
+		// create incubed
+		IN3 in3 = IN3.forChain(Chain.MAINNET); // set it to mainnet (which is also dthe default)
+
+		Transaction txn = GetTransactionAPI(in3);
+		if (null != txn) {
+			System.out.println("Block num " + txn.getBlockNumber());
+		}
+
+		String rpcResponse = GetTransactionRPC(in3);
+
+		if (null != rpcResponse) {
+			System.out.println(rpcResponse);
+		}
+	}
+
+	static Transaction GetTransactionAPI(IN3 in3) {
+
+		return in3.getEth1API().getTransactionByHash(TXN_HASH);
+
+	}
+
+	static String GetTransactionRPC(IN3 in3) {
+
+		return in3.sendRPC("eth_getTransactionByHash", new Object[] { TXN_HASH
+
+		});
+	}
 
 }
-
--- examples/java/GetTransactionReceipt.java
+++ examples/java/GetTransactionReceipt.java
@@ -4,41 +4,37 @@ import in3.eth1.*;
 import java.math.BigInteger;
 
 public class GetTransactionReceipt {
-  static String TRANSACTION_HASH = "0xdd80249a0631cf0f1593c7a9c9f9b8545e6c88ab5252287c34bc5d12457eab0e";
-  //
-  public static void main(String[] args) throws Exception {
-    // create incubed
-    IN3 in3 = IN3.forChain(Chain.MAINNET); // set it to mainnet (which is also dthe default)
+	static String TRANSACTION_HASH = "0xdd80249a0631cf0f1593c7a9c9f9b8545e6c88ab5252287c34bc5d12457eab0e";
 
-    
-    TransactionReceipt txn = getTransactionReceiptAPI(in3);
-    if(null != txn) {
-    	System.out.println("Transaction #"+txn.getTransactionIndex()+" Block num "+txn.getBlockNumber()+" Gas used "+txn.getGasUsed()+" status "+txn.getStatus());
-    }
-    
-    String rpcResponse = getTransactionReceiptRPC(in3);
-    
-    if(null != rpcResponse) {
-    	System.out.println(rpcResponse);
-    }
-  }
-  
-  static TransactionReceipt getTransactionReceiptAPI(IN3 in3) {
-	  
-	  return in3.
-			  getEth1API().
-			  getTransactionReceipt(TRANSACTION_HASH);
+	//
+	public static void main(String[] args) throws Exception {
+		// create incubed
+		IN3 in3 = IN3.forChain(Chain.MAINNET); // set it to mainnet (which is also dthe default)
 
-	  
-  }
-  
-  static String getTransactionReceiptRPC(IN3 in3) {
-	  
-	  
+		TransactionReceipt txn = GetTransactionReceiptAPI(in3);
+		if (null != txn) {
+			System.out.println("Transaction #" + txn.getTransactionIndex() + " Block num " + txn.getBlockNumber()
+					+ " Gas used " + txn.getGasUsed() + " status " + txn.getStatus());
+		}
+
+		String rpcResponse = GetTransactionReceiptRPC(in3);
+
+		if (null != rpcResponse) {
+			System.out.println(rpcResponse);
+		}
+	}
+
+	static TransactionReceipt GetTransactionReceiptAPI(IN3 in3) {
+
+		return in3.getEth1API().getTransactionReceipt(TRANSACTION_HASH);
+
+	}
+
+	static String GetTransactionReceiptRPC(IN3 in3) {
+
 		return in3.sendRPC("eth_getTransactionReceipt", new Object[] { TRANSACTION_HASH
 
 		});
-  }
+	}
 
 }
-
