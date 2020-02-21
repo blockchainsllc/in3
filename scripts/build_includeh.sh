013d3abce9c8276c8b4b7b2af57398cf8312fe2e
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-02-22 00:50:47 +0100

    Fix build_includeh.sh

--- scripts/build_includeh.sh
+++ scripts/build_includeh.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd ..
 rm -rf include
-mkdir -p include/in3
-find c -type d >include/in3/.dirs
+mkdir -p c/include/in3
+find c -type d >c/include/in3/.dirs
 while read path; do
   # find headers in path
   find $path -name "*.h" -maxdepth 1 -exec bash -c 'for file in "$1";
     do
     # check if public header
     if grep -q "@PUBLIC_HEADER" "$file"; then
-      cp "$file" "include/in3/`basename $file`"
+      cp "$file" "c/include/in3/`basename $file`"
       projectdir=`pwd`
       fdir=`dirname "$file"`
       cd "$fdir"
@@ -19,13 +19,13 @@ while read path; do
         headers=`echo "$incl" | sed "s/[\]//g"`
         for header in $headers;
         do
-          output="$projectdir/include/in3/`basename $header`"
+          output="$projectdir/c/include/in3/`basename $header`"
           cp "$header" "$output" 2>/dev/null
           sed -i -E "s/include \"\(.*\)\/\(.*h\)\"/include \"\2\"/" "$output" 2>/dev/null
         done
-        rm $projectdir/include/in3/*.h-E
+        rm $projectdir/c/include/in3/*.h-E
       done
     fi
     done' none {} \;
-done <include/in3/.dirs
-rm include/in3/.dirs
+done <c/include/in3/.dirs
+rm c/include/in3/.dirs
