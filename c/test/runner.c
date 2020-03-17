f67d15c7faaa62c9374e2f14fc2824cbf3aedfe5
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-03-17 14:49:44 +0100

    Don't link runner to init mod

--- c/test/runner.c
+++ c/test/runner.c
@@ -42,7 +42,6 @@
 #include "../src/core/util/mem.h"
 #include "../src/verifier/eth1/full/eth_full.h"
 #include "../src/verifier/ipfs/ipfs.h"
-#include "verifier/in3_init.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -450,6 +449,10 @@ int runRequests(char** names, int test_index, int mem_track) {
 int main(int argc, char* argv[]) {
   use_color = 1;
   in3_log_set_level(LOG_INFO);
+  in3_register_eth_full();
+  in3_register_eth_api();
+  in3_register_ipfs();
+
   int    i = 0, size = 1;
   int    testIndex = -1, membrk = -1;
   char** names = malloc(sizeof(char*));
