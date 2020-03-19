bbaaf110957e52830aba9aa2cb5bdaa13b41c0af
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-03-19 17:56:55 +0100

    missing in3 header

---
+++ c/include/in3.h
@@ -0,0 +1,4 @@
+#include "in3/client.h"    // the core client
+#include "in3/eth_api.h"   // wrapper for easier use
+#include "in3/eth_basic.h" // use the basic module
+#include "in3/in3_curl.h"  // transport implementation
