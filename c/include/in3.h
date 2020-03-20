e9a4c65f56fede7bde39077c60f86361fdc18506
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-03-20 12:54:22 +0100

    Update in3.h

--- c/include/in3.h
+++ c/include/in3.h
@@ -1,4 +1,3 @@
-#include "in3/client.h"    // the core client
-#include "in3/eth_api.h"   // wrapper for easier use
-#include "in3/eth_basic.h" // use the basic module
-#include "in3/in3_curl.h"  // transport implementation
+#include "in3/client.h"   // the core client
+#include "in3/eth_api.h"  // wrapper for easier use
+#include "in3/in3_init.h" // init module
