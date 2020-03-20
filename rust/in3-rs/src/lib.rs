818cc27bb12ac010a429bbe906898a8e239463a0
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-03-20 15:07:23 +0100

    Add In3 struct with RAII destruction

--- rust/in3-rs/src/lib.rs
+++ rust/in3-rs/src/lib.rs
@@ -1,5 +1,25 @@
 extern crate in3_sys;
 
+struct In3 {
+    ptr: *mut in3_sys::in3_t,
+}
+
+impl In3 {
+    fn new() -> In3 {
+        unsafe {
+            In3 { ptr: in3_sys::in3_for_chain_auto_init(1) }
+        }
+    }
+}
+
+impl Drop for In3 {
+    fn drop(&mut self) {
+        unsafe {
+            in3_sys::in3_free(self.ptr);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
