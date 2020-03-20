155f864e7a2cf1438378b6bcd18be202de28d308
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-03-20 12:57:15 +0100

    Add test eth_block_number

--- rust/in3-rs/src/lib.rs
+++ rust/in3-rs/src/lib.rs
@@ -7,3 +7,14 @@ pub struct In3 {
     data: bytes_t,
 }
 
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn eth_block_number() {
+        unsafe {
+            // in3_sys::in3_for_chain_auto_init(0);
+            in3_sys::eth_blockNumber(in3_sys::in3_for_chain_auto_init(1));
+        }
+    }
+
+}
