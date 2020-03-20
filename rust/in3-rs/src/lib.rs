7c535ac999614f300c7cadc02cab87b8e7e3d3f0
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-03-20 15:23:52 +0100

    Add test for get balance

--- rust/in3-rs/src/lib.rs
+++ rust/in3-rs/src/lib.rs
@@ -22,13 +22,27 @@ impl Drop for In3 {
 
 #[cfg(test)]
 mod tests {
+    use std::ffi;
+
+    use super::*;
 
     #[test]
-    fn eth_block_number() {
+    fn test_eth_block_number() {
         unsafe {
-            // in3_sys::in3_for_chain_auto_init(0);
-            in3_sys::eth_blockNumber(in3_sys::in3_for_chain_auto_init(1));
+            in3_sys::eth_blockNumber(In3::new().ptr);
         }
     }
 
+    #[test]
+    fn test_eth_get_balance() {
+        let mut null: *mut i8 = std::ptr::null_mut();
+        let mut res: *mut *mut i8 = &mut null;
+        let err: *mut *mut i8 = &mut null;
+        unsafe {
+            let _ = in3_sys::in3_client_rpc(In3::new().ptr, ffi::CString::new("eth_getBalance").unwrap().as_ptr(),
+                                            ffi::CString::new("[\"0xc94770007dda54cF92009BFF0dE90c06F603a09f\", \"latest\"]").unwrap().as_ptr(), res, err);
+            // to view run with `cargo test -- --nocapture`
+            println!("------> balance: {}", ffi::CStr::from_ptr(*res).to_str().unwrap());
+        }
+    }
 }
