78c288ec8fd683ba4384fe85b808ab942401dbda
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-03-24 02:06:41 +0530

    Change In3 -> Client & reformat

--- rust/in3-rs/src/in3.rs
+++ rust/in3-rs/src/in3.rs
@@ -1,37 +1,38 @@
-//extern crate in3_sys;
-use in3_sys::*;
 use std::ffi;
-struct In3 {
+
+use in3_sys::*;
+
+struct Client {
     ptr: *mut in3_sys::in3_t,
 }
 
-impl In3 {
-    fn new() -> In3 {
+impl Client {
+    fn new() -> Client {
         unsafe {
-            In3 { ptr: in3_sys::in3_for_chain_auto_init(1) }
+            Client { ptr: in3_sys::in3_for_chain_auto_init(1) }
         }
     }
-    fn eth_getBalance() ->  String {
-            let mut null: *mut i8 = std::ptr::null_mut();
-            let mut res: *mut *mut i8 = &mut null;
-            let err: *mut *mut i8 = &mut null;
-            unsafe {
-                let _ = in3_sys::in3_client_rpc(In3::new().ptr, ffi::CString::new("eth_getBalance").unwrap().as_ptr(),
-                                                ffi::CString::new("[\"0xc94770007dda54cF92009BFF0dE90c06F603a09f\", \"latest\"]").unwrap().as_ptr(), res, err);
-                // to view run with `cargo test -- --nocapture`
-                ffi::CStr::from_ptr(*res).to_str().unwrap().to_string()
-            }
 
+    fn eth_getBalance() -> String {
+        let mut null: *mut i8 = std::ptr::null_mut();
+        let mut res: *mut *mut i8 = &mut null;
+        let err: *mut *mut i8 = &mut null;
+        unsafe {
+            let _ = in3_sys::in3_client_rpc(Client::new().ptr, ffi::CString::new("eth_getBalance").unwrap().as_ptr(),
+                                            ffi::CString::new("[\"0xc94770007dda54cF92009BFF0dE90c06F603a09f\", \"latest\"]").unwrap().as_ptr(), res, err);
+            // to view run with `cargo test -- --nocapture`
+            ffi::CStr::from_ptr(*res).to_str().unwrap().to_string()
+        }
     }
-    fn eth_blockNumber(in3 : &mut In3) {
+
+    fn eth_blockNumber(in3: &mut Client) {
         unsafe {
             in3_sys::eth_blockNumber(in3.ptr);
         }
     }
-
 }
 
-impl Drop for In3 {
+impl Drop for Client {
     fn drop(&mut self) {
         unsafe {
             in3_sys::in3_free(self.ptr);
@@ -47,12 +48,12 @@ mod tests {
 
     #[test]
     fn test_eth_block_number() {
-        let mut in3 = In3::new();
-        In3::eth_blockNumber(&mut in3);
+        let mut in3 = Client::new();
+        Client::eth_blockNumber(&mut in3);
     }
 
     #[test]
     fn test_eth_get_balance() {
-            println!("------> balance: {}", In3::eth_getBalance());
+        println!("------> balance: {}", Client::eth_getBalance());
     }
 }
