6de4f17bff7190ccbb4d078c72422de526551312
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-03-23 21:28:39 +0100

    added configure

--- rust/in3-rs/src/in3.rs
+++ rust/in3-rs/src/in3.rs
@@ -5,13 +5,14 @@ struct In3 {
     ptr: *mut in3_sys::in3_t,
 }
 
+
 impl In3 {
     fn new() -> In3 {
         unsafe {
             In3 { ptr: in3_sys::in3_for_chain_auto_init(1) }
         }
     }
-    fn eth_getBalance() ->  String {
+    fn eth_get_balance_rpc() ->  String {
             let mut null: *mut i8 = std::ptr::null_mut();
             let mut res: *mut *mut i8 = &mut null;
             let err: *mut *mut i8 = &mut null;
@@ -26,6 +27,13 @@ impl In3 {
     fn eth_blockNumber(in3 : &mut In3) {
         unsafe {
             in3_sys::eth_blockNumber(in3.ptr);
+        }
+    }
+
+    fn configure(in3 : &mut In3, config: String) {
+        unsafe {
+            let config_c = ffi::CString::new(config).expect("CString::new failed");
+            in3_sys::in3_configure(in3.ptr, config_c.as_ptr());
         }
     }
 
@@ -46,13 +54,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_eth_block_number() {
+    fn test_eth_blknum() {
         let mut in3 = In3::new();
         In3::eth_blockNumber(&mut in3);
     }
 
     #[test]
+    fn test_in3_config() {
+        let mut in3 = In3::new();
+        let mut config = String::from("{\"autoUpdateList\":false,\"nodes\":{\"0x7d0\": {\"needsUpdate\":false}}}");
+        let c = In3::configure(&mut in3, config);
+    }
+
+    #[test]
     fn test_eth_get_balance() {
-            println!("------> balance: {}", In3::eth_getBalance());
+            println!("------> balance: {}", In3::eth_get_balance_rpc());
     }
 }
