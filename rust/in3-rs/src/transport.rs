fbae7e3e76b626d1aa6c6e49bde66a95e3b8cfd6
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-03-24 22:35:01 +0100

    Working HTTP impl

--- rust/in3-rs/src/transport.rs
+++ rust/in3-rs/src/transport.rs
@@ -2,10 +2,14 @@ extern crate reqwest;
 
 use std::collections::HashMap;
 
+use reqwest::header;
+
 fn http_send(url: &str, payload: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let header_json = header::HeaderValue::from_static("application/json");
     let client = reqwest::blocking::Client::new();
     let res = client.post(url)
         .body(payload.to_string())
+        .header(header::CONTENT_TYPE, header_json)
         .send()?;
     Ok(res.text().unwrap())
 }
@@ -14,7 +18,7 @@ fn transport_http(payload: &str, urls: &[&str]) -> Vec<Result<String, String>> {
     let mut responses = vec![];
     for url in urls {
         match http_send(url, payload) {
-            Err(_) => responses.push(Err("Transport error".to_string())),
+            Err(e) => responses.push(Err("Transport error".to_string())),
             Ok(res) => responses.push(Ok(res))
         }
     }
@@ -30,6 +34,5 @@ mod tests {
         let res = transport_http(r#"{"id":1,"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"in3":{"verification":"proof","version": "2.1.0"}}"#
                                  , &["https://in3-v2.slock.it/mainnet/nd-3"]);
         println!("{:?}", res);
-
     }
 }
