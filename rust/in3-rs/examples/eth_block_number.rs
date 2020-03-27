fd190b8b0bb6d0c72a316f85237d52a667a6bf82
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-03-27 19:45:21 +0100

    Set needsUpdate to false in example

--- rust/in3-rs/examples/eth_block_number.rs
+++ rust/in3-rs/examples/eth_block_number.rs
@@ -4,6 +4,7 @@ use in3::prelude::*;
 
 fn main() {
     let mut c = Client::new(chain::MAINNET);
+    let _ = c.configure(r#"{"autoUpdateList":false,"nodes":{"0x1":{"needsUpdate":false}}}}"#);
     match c.rpc(r#"{"method": "eth_blockNumber", "params": []}"#) {
         Ok(res) => println!("{}", res),
         Err(err) => println!("{}", err)
