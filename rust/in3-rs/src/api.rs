27261555a53d3e40052b99f3a75152415e09c7b3
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-04-07 22:00:54 +0200

    Missing api mod

---
+++ rust/in3-rs/src/api.rs
@@ -0,0 +1,79 @@
+use crate::error::In3Result;
+use crate::error::*;
+use crate::in3::*;
+use async_std::task;
+use serde_json::{Result, Value};
+use std::i64;
+use serde_json::json;
+pub struct EthApi {
+    client: Box<Client>,
+}
+impl EthApi {
+    pub fn new(config_str: &'static str) -> EthApi {
+        let mut client = Client::new(chain::MAINNET, false);
+        // let _ = client.configure(r#"{"autoUpdateList":false,"nodes":{"0x1":{"needsUpdate":false}}}}"#);
+        let _ = client.configure(config_str);
+        EthApi { client }
+    }
+
+    async fn send(mut self, params: &str) -> In3Result<String> {
+        //let req = Option::from(params).unwrap_or(r#"{"method": "eth_blockNumber", "params": []}"#);
+        //let req = Option::from(params);
+        self.client.send_request(params).await
+    }
+
+    pub async fn block_number(self) -> i64 {
+        let response = self
+            .send(r#"{"method": "eth_blockNumber", "params": []}"#)
+            .await;
+        let v: Value = serde_json::from_str(&response.unwrap()).unwrap();
+        let ret = v[0]["result"].as_str().unwrap();
+        let without_prefix = ret.trim_start_matches("0x");
+        let blocknum = i64::from_str_radix(without_prefix, 16);
+        blocknum.unwrap_or(-1)
+    }
+
+    pub async fn getBalance(self, address:String) -> f64 {
+        let payload = json!({
+            "method": "eth_getBalance",
+            "params": [
+                address,
+                "latest"
+            ]
+        });
+        let payload_str = payload.to_string().to_owned();
+        let serialized = serde_json::to_string(&payload).unwrap();
+        let response = self
+        .send(&serialized)
+        .await; 
+        let v: Value = serde_json::from_str(&response.unwrap()).unwrap();
+        let ret = v[0]["result"].as_str().unwrap();
+        println!("{}",ret);
+        let without_prefix = ret.trim_start_matches("0x");
+        let balance_i = u64::from_str_radix(without_prefix, 16).unwrap();
+        let balance = f64::from_bits(balance_i);
+        println!("{}",balance);
+        balance
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_block_number() {
+        let api = EthApi::new(r#"{"autoUpdateList":false,"nodes":{"0x1":{"needsUpdate":false}}}}"#);
+        //execute the call to the api on task::block_on
+        let num = task::block_on(api.block_number());
+        assert!(num > 9000000, "Block number is not correct");
+    }
+
+    #[test]
+    fn test_get_balance() {
+        let api = EthApi::new(r#"{"autoUpdateList":false,"nodes":{"0x1":{"needsUpdate":false}}}}"#);
+        //execute the call to the api on task::block_on
+        let num = task::block_on(api.getBalance("0xc94770007dda54cF92009BFF0dE90c06F603a09f".to_string()));
+        assert!(num > 0.0, "Balance is not correct");
+    }
+}
