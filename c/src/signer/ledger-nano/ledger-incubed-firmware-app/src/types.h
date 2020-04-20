e8039d89bfd7696944bde60f1b3dba6a8983889a
vineet pant <vineet.pant@slock.it>
2020-04-20 12:55:44 +0530

    formatted fw app code

--- c/src/signer/ledger-nano/ledger-incubed-firmware-app/src/types.h
+++ c/src/signer/ledger-nano/ledger-incubed-firmware-app/src/types.h
@@ -8,28 +8,22 @@
 #define TEXT_HEIGHT 15
 #define TEXT_SPACE 4
 
-
 typedef unsigned char uchar_t;
 
 typedef enum MODULES {
-        SIGN = 1,
-        GET_PUBLIC_KEY = 2,
-}MODULES;
+  SIGN           = 1,
+  GET_PUBLIC_KEY = 2,
+} MODULES;
 
-
-typedef enum  {
-        IDM_ED = 0,
-        IDM_SECP256K1 = 1,
-        IDM_SECP256R1 = 2,
-        IDM_NO_CURVE = 255,
-}curve_code;
+typedef enum {
+  IDM_ED        = 0,
+  IDM_SECP256K1 = 1,
+  IDM_SECP256R1 = 2,
+  IDM_NO_CURVE  = 255,
+} curve_code;
 
 typedef struct {
-        cx_curve_t curve;
-        uint8_t path_length;
-        uint32_t bip32_path[BIP32_PATH_LEN_MAX];
+  cx_curve_t curve;
+  uint8_t    path_length;
+  uint32_t   bip32_path[BIP32_PATH_LEN_MAX];
 } nvram_data;
-
-
-
-
