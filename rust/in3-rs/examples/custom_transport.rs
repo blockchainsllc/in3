773f42841bd5f8e88f85d156700e10cdeef5ca1f
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-04-21 15:37:09 +0200

    Fix tests to not use blocking transport

--- rust/in3-rs/examples/custom_transport.rs
+++ rust/in3-rs/examples/custom_transport.rs
@@ -1,5 +1,7 @@
 extern crate in3;
 
+use async_std::task;
+
 use async_trait::async_trait;
 use in3::prelude::*;
 
@@ -9,18 +11,18 @@ struct MockTransport<'a> {
 
 #[async_trait]
 impl Transport for MockTransport<'_> {
-    async fn fetch(&mut self, _request: &str, _uris: &[&str]) -> Vec<Result<String, String>> {
-        unimplemented!()
-    }
-
-    #[cfg(feature = "blocking")]
-    fn fetch_blocking(&mut self, request: &str, _uris: &[&str]) -> Vec<Result<String, String>> {
+    async fn fetch(&mut self, request: &str, _uris: &[&str]) -> Vec<Result<String, String>> {
         let response = self.responses.pop();
         let request: serde_json::Value = serde_json::from_str(request).unwrap();
         match response {
             Some(response) if response.0 == request[0]["method"] => vec![Ok(response.1.to_string())],
             _ => vec![Err(format!("Found wrong/no response while expecting response for {}", request))]
         }
+    }
+
+    #[cfg(feature = "blocking")]
+    fn fetch_blocking(&mut self, _request: &str, _uris: &[&str]) -> Vec<Result<String, String>> {
+        unimplemented!()
     }
 }
 
@@ -30,8 +32,8 @@ fn main() {
     c.set_transport(Box::new(MockTransport {
         responses: vec![("eth_blockNumber", r#"[{"jsonrpc":"2.0","id":1,"result":"0x96bacd"}]"#)]
     }));
-    match c.rpc_blocking(r#"{"method": "eth_blockNumber", "params": []}"#) {
+    match task::block_on(c.rpc(r#"{"method": "eth_blockNumber", "params": []}"#)) {
         Ok(res) => println!("{}", res),
-        Err(err) => println!("{}", err),
+        Err(err) => println!("Failed with error: {}", err),
     }
 }
