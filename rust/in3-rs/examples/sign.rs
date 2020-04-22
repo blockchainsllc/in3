a51733eb3063cc4a44c106874d8cc15c7a9b7728
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-04-22 11:59:17 +0200

    Updated, clean sign example

--- rust/in3-rs/examples/sign.rs
+++ rust/in3-rs/examples/sign.rs
@@ -2,7 +2,7 @@ extern crate in3;
 use async_std::task;
 use in3::prelude::*;
 use std::{fmt::Write, num::ParseIntError};
-
+use std::ffi::{CStr, CString};
 pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
     (0..s.len())
         .step_by(2)
@@ -10,24 +10,35 @@ pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
         .collect()
 }
 
-fn sign_native_rust_decoding() {
+fn decode_test(){
     let mut in3 = Client::new(chain::MAINNET);
-    let pk_ =
-        decode_hex("d46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8").unwrap();
-    let pk = pk_.as_ptr();
-    let data_ =
-        decode_hex("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef").unwrap();
-    let data = data_.as_ptr();
-    let signa = in3.eth_sign(1, pk, data, 32);
-    println!("\n");
+    let pk_ = decode_hex("d46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8").unwrap();
+    let pk_rust = pk_.as_ptr();
+    let data_ = decode_hex("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef").unwrap();
+    let data_rust = data_.as_ptr();
+    let pk_c = in3.hex_to_bytes("0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8");
+    let data_c = in3.new_bytes("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef");
+    //TODO: maybe assert this byte by byte
+    // assert!(pk_c, pk_);
+    // assert!(data_c, data_);
 }
 
-fn sign_decode_with_in3c_hex_to_bytes() {
+unsafe fn sign_sha() {
     let mut in3 = Client::new(chain::MAINNET);
-    let pk = in3.hex_to_bytes("0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8");
-    let data = in3.new_bytes("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef");
-    println!("{:?} {:?}", data, pk);
-    let signa = in3.eth_sign(1, pk, data, 32);
+    let mut ctx = Ctx::new(&mut in3, "");
+    let c_string = CString::new("foo").expect("CString::new failed");
+    let data = c_string.into_raw() as *mut u8;
+    in3.set_pk_signer("0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8");
+    let signa = ctx.sign(1, data, 32);
+}
+
+unsafe fn sign_raw() {
+    let mut in3 = Client::new(chain::MAINNET);
+    let mut ctx = Ctx::new(&mut in3, "");
+    in3.set_pk_signer("0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8");
+    let c_string = CString::new("foo").expect("CString::new failed");
+    let data = c_string.into_raw() as *mut u8;
+    let signa = ctx.sign(0, data, 32);
 }
 
 async fn sign_execute() {
@@ -36,15 +47,17 @@ async fn sign_execute() {
         c.set_pk_signer("0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8");
         // c.configure(r#"{"autoUpdateList":false,"nodes":{"0x1":{"needsUpdate":false}}}}"#);
         // let request = r#"{"method": "in3_signData",   "params": ["0x0102030405060708090a0b0c0d0e0f","0xa8b8759ec8b59d7c13ef3630e8530f47ddb47eba12f00f9024d3d48247b62852","raw"] }"#;
-        let request = r#"{"method":"eth_sendTransaction", "params":[{ "gas": "0x76c0","nonce": "0x15","gasPrice": "0x9184e72a000","from": "0xb60e8dd61c5d32be8058bb8eb970870f07233155", "to":"0x45d45e6ff99e6c34a235d263965910298985fcfe", "value":"0xff" }]}"#;
+        // let request = r#"{"method":"eth_sendTransaction", "params":[{ "gas": "0x76c0","nonce": "0x15","gasPrice": "0x9184e72a000","from": "0xb60e8dd61c5d32be8058bb8eb970870f07233155", "to":"0x45d45e6ff99e6c34a235d263965910298985fcfe", "value":"0xff" }]}"#;
+        // let request = r#"{"method":"eth_sendTransaction", "params":[{ "gas": "0x76c0","nonce": "0x15","gasPrice": "0x9184e72a000","from": "0xb60e8dd61c5d32be8058bb8eb970870f07233155", "to":"0x45d45e6ff99e6c34a235d263965910298985fcfe", "value":"0xff" }]}"#;
         // let request = r#"{"method":"eth_sendTransaction", "params":[{"from": "0xb60e8dd61c5d32be8058bb8eb970870f07233155", "to":"0x45d45e6ff99e6c34a235d263965910298985fcfe", "value":"0xff" }]}"#;
+        let request = r#"{"method": "eth_blockNumber", "params": []}"#;
         let mut ctx = Ctx::new(&mut c, request);
         let _res = ctx.execute().await;
     }
 }
 
 fn main() {
-    // sign_decode_with_in3c_hex_to_bytes();
-    // sign_native_rust_decoding();
+    // sign_sha();
+    // sign_raw();
     task::block_on(sign_execute());
 }
