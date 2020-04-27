28cb6036830770aeb924b38e947bccccba1e7ccd
Simon Jentzsch <simon@slock.it>
2020-04-27 15:53:33 +0200

    fixed wasm-docker image

--- scripts/build_asmjs.sh
+++ scripts/build_asmjs.sh
@@ -4,6 +4,6 @@ docker run \
   --rm \
   -v $(pwd):$(pwd) \
   docker.slock.it/build-images/cmake:clang10 \
-  /bin/bash -c "cd /$(pwd)/build;  emconfigure cmake -DWASM=true -DASMJS=true -DWASM_EMMALLOC=true  -DWASM_EMBED=false  -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DCMAKE_BUILD_TYPE=MINSIZEREL .. && make -j8 in3w"
+  /bin/bash -c "cd /$(pwd)/build;  emcmake cmake -DWASM=true -DASMJS=true -DWASM_EMMALLOC=true  -DWASM_EMBED=false  -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DCMAKE_BUILD_TYPE=MINSIZEREL .. && make -j8 in3w"
 
 cd scripts
