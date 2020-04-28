1f4ea13cad31432aff8f0be8dd321faf4089e1e5
pari <parithosh@slock.it>
2020-04-28 09:05:43 +0200

    updated to clang11

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -3,7 +3,7 @@ cd ..; rm -rf build/*
 docker run \
   --rm \
   -v $(pwd):$(pwd) \
-  docker.slock.it/build-images/cmake:clang10 \
+  docker.slock.it/build-images/cmake:clang11 \
   /bin/bash -c "cd /$(pwd)/build;  emconfigure cmake -DWASM=true -DASMJS=false -DWASM_EMMALLOC=true  -DWASM_EMBED=true  -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DCMAKE_BUILD_TYPE=DEBUG .. && make -j8 in3w"
 
 cd scripts
