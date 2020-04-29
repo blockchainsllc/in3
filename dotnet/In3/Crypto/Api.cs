f3b587f72a0f2620243844e11d310945a5efa436
leonardocardoso <leonardo.t.cardoso@slock.it>
2020-04-29 14:41:38 +0200

    Add Crypto support for dotnet binding

--- dotnet/In3/Crypto/Api.cs
+++ dotnet/In3/Crypto/Api.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Text.Json;
 using In3.Utils;
 
 namespace In3.Crypto
@@ -8,6 +10,10 @@ namespace In3.Crypto
 
         private const string CryptoPk2Address = "in3_pk2address";
         private const string CryptoSignData = "in3_signData";
+        private static string CryptoPk2Public = "in3_pk2public";
+        private static string CryptoEcRecover = "in3_ecrecover";
+        private static string CryptoDecryptKey = "in3_decryptKey";
+        private static string CryptoSha3 = "web3_sha3";
 
         public Api(IN3 client)
         {
@@ -25,5 +31,30 @@ namespace In3.Crypto
             string jsonResponse = in3.SendRpc(CryptoSignData, new object[] { msg, key, sigType?.Value });
             return RpcHandler.From<SignedData>(jsonResponse);
         }
+
+        public string Pk2Public(string key)
+        {
+            string jsonResponse = in3.SendRpc(CryptoPk2Public, new object[] { key });
+            return RpcHandler.From<string>(jsonResponse);
+        }
+
+        public Account EcRecover(string msg, string sig, SignatureType sigType)
+        {
+            string jsonResponse = in3.SendRpc(CryptoEcRecover, new object[] { msg, sig, sigType?.Value });
+            return RpcHandler.From<Account>(jsonResponse);
+        }
+
+        public string DecryptKey(string key, string passphrase)
+        {
+            Dictionary<string, object> res = JsonSerializer.Deserialize<Dictionary<string, object>>(key);
+            string jsonResponse = in3.SendRpc(CryptoDecryptKey, new object[] { res, passphrase });
+            return RpcHandler.From<string>(jsonResponse);
+        }
+
+        public string Sha3(string data) {
+            string jsonResponse = in3.SendRpc(CryptoSha3, new object[] { data });
+            return RpcHandler.From<string>(jsonResponse);
+        }
+        
     }
 }
