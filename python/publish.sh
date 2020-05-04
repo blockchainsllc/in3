91d4788f2008866ea65358031aa462c66175d040
J. Paul Depraz <2923413+cerealkill@users.noreply.github.com>
2020-05-04 16:01:25 +0100

    Pypi publish information tested

--- python/publish.sh
+++ python/publish.sh
@@ -4,7 +4,7 @@ rm -rf dist
 
 python3 setup.py sdist
 
-twine upload --verbose --repository-url https://upload.pypi.org/legacy/ -u __token__ -p $PIP_TOKEN dist/*
+twine upload --verbose --repository-url https://upload.pypi.org/legacy/ -u __token__ -p "$PIP_TOKEN" dist/*
 
 #pip3 install twine
 
--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,6 @@
 import pathlib
 import setuptools
-import os
+from os import environ
 
 """
 Information on how to build this file
@@ -8,19 +8,15 @@ https://pypi.org/
 https://pypi.org/classifiers/
 """
 
-envs = os.environ
-version = envs.get("version", "0.0.15")
-url = envs.get("url", "https://github.com/slockit/in3-c")
-download_url = envs.get("download_url",
-                        "https://git.slock.it/in3/c/in3-python/-/archive/dev_0.1.0/in3-python-dev_0.1.0.tar.gz")
-License = envs.get("license", "LGPL")
-description = envs.get(
-    "description", "Incubed client and provider for web3. Based on in3-c for cython runtime.")
-keywords = envs.get("keywords", "in3").split(",")
+version = environ.get("version", "2.3.1")
+url = environ.get("url", "https://github.com/slockit/in3-c")
+License = environ.get("license", "LGPL")
+description = environ.get("description", "Incubed client and provider for web3. Based on in3-c runtime.")
+keywords = environ.get("keywords", "in3,c,arm,x86,x64,macos,windows,linux,blockchain,ethereum").split(",")
 readme = (pathlib.Path(__file__).parent / "README.md").read_text()
-name = envs.get("name", "in3")
-author = envs.get("author", "github.com/slockit/in3-c")
-author_email = envs.get("author_email", "products@slock.it")
+name = environ.get("name", "in3")
+author = environ.get("author", "github.com/slockit/in3-c")
+author_email = environ.get("author_email", "products@slock.it")
 setuptools.setup(
     name=name,
     version=version,
@@ -31,7 +27,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url=url,
     packages=setuptools.find_packages(exclude=["docs", "tests"]),
-    install_requires=['requests>=2.23.0'],
+    install_requires=['requests==2.23.0'],
     keywords=keywords,
     classifiers=[
         "Programming Language :: Python :: 3",
