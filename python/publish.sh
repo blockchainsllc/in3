a50db8da5e819ae782a3475f8583e0fb1dabdfd2
J. Paul Depraz <2923413+cerealkill@users.noreply.github.com>
2020-05-05 11:43:22 +0100

    Fixed dist script missing files

--- python/publish.sh
+++ python/publish.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 rm -rf dist
-
+cp ../LICENSE ../LICENSE.AGPL ./
 python3 setup.py sdist
+rm LICENSE LICENSE.AGPL
 
 twine upload --verbose --repository-url https://upload.pypi.org/legacy/ -u __token__ -p "$PIP_TOKEN" dist/*
 
