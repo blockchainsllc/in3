cc5c658a5dc038df995a17eeeb5556add1d0b196
Simon Jentzsch <simon@slock.it>
2020-05-05 15:31:52 +0200

    change to bdist

--- python/publish.sh
+++ python/publish.sh
@@ -2,7 +2,7 @@
 
 rm -rf dist
 cp ../LICENSE ../LICENSE.AGPL ./
-python3 setup.py bdist_wheel
+python3 setup.py bdist
 rm LICENSE LICENSE.AGPL
 
 twine upload --verbose --repository-url https://upload.pypi.org/legacy/ -u __token__ -p "$PIP_TOKEN" dist/*
