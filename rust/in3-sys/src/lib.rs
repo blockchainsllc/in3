b8250ce03a8a1ebdf85f6c38c70977efb42cc5d6
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-05-18 14:26:24 +0200

    Fix documentation in in3-sys/lib.rs

--- rust/in3-sys/src/lib.rs
+++ rust/in3-sys/src/lib.rs
@@ -1,4 +1,4 @@
-//! Low-level, unsafe Rust bindings for the [`In3`][in3] disassembly library.
+//! Low-level, unsafe Rust bindings for the [`In3`][in3] library.
 //!
 //!
 //! We recommend against using this crate directly.
@@ -6,12 +6,7 @@
 //!
 //! [in3-rs]: https://github.com/in3-rust/in3-rs
 //!
-//! **Note**: documentation for functions/types was taken directly from
-//! [In3 C headers][in3 headers].
-//!
 //! [in3 headers]: https://github.com/in3-rust/in3-sys/blob/master/in3/include/in3.h
-//! <sup>1</sup>: Defined as a ["constified" enum modules](https://docs.rs/bindgen/0.30.0/bindgen/struct.Builder.html#method.constified_enum_module)
-//!               because discriminant values are not unique. Rust requires discriminant values to be unique.
 
 // Suppress errors from In3 names
 #![allow(non_upper_case_globals)]
