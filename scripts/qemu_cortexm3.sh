71a790713b1e13ce2be9c393e1b0819da72d80f7
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-05-31 09:47:52 +0200

    Test fix armm3

--- scripts/qemu_cortexm3.sh
+++ scripts/qemu_cortexm3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -net none -pidfile qemu.pid -serial mon:stdio -semihosting -kernel build/zephyr/zephyr.elf 2> /dev/null
+qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -net none -pidfile qemu.pid -serial mon:stdio -semihosting -kernel build/zephyr/zephyr.elf
 RESULT=$?
 # the goal here is to return a success exit code regarding of the qemu output 
 echo success
