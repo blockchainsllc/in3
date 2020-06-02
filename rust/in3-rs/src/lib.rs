5395c523c31a485362c233f992ed0779b4ed68ac
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-06-02 05:38:49 +0200

    Add mod IPFS to lib.rs

--- rust/in3-rs/src/lib.rs
+++ rust/in3-rs/src/lib.rs
@@ -1,19 +1,19 @@
 pub mod error;
 pub mod eth1;
 pub mod in3;
+pub mod ipfs;
+pub mod json_rpc;
 pub mod signer;
 pub mod traits;
 pub mod types;
-
 pub mod transport;
 
 pub mod prelude {
     pub use crate::error::*;
     pub use crate::in3::*;
-    pub use crate::signer::SignerRust;
     pub use crate::signer::*;
+    pub use crate::traits::{Signer, Storage, Transport};
     pub use crate::traits::Api as ApiTrait;
     pub use crate::traits::Client as ClientTrait;
-    pub use crate::traits::{Signer, Storage, Transport};
     pub use crate::transport::{HttpTransport, MockTransport};
 }
