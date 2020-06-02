54404c9bc25cbc86d20e9610167c5bb1a851e30d
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-06-02 05:38:17 +0200

    Impl IPFS support

---
+++ rust/in3-rs/src/ipfs.rs
@@ -0,0 +1,68 @@
+use serde_json::{from_str, json};
+
+use crate::error::In3Result;
+use crate::json_rpc::{Request, rpc};
+use crate::traits::{Api as ApiTrait, Client as ClientTrait};
+use crate::types::Bytes;
+
+pub type Multihash = String;
+
+pub struct Api {
+    client: Box<dyn ClientTrait>,
+}
+
+impl ApiTrait for Api {
+    fn new(client: Box<dyn ClientTrait>) -> Self {
+        Api { client }
+    }
+
+    fn client(&mut self) -> &mut Box<dyn ClientTrait> {
+        &mut self.client
+    }
+}
+
+impl Api {
+    pub async fn put(&mut self, content: Bytes) -> In3Result<Multihash> {
+        let resp = rpc(self.client(), Request {
+            method: "ipfs_put",
+            params: json!([content]),
+        }).await?;
+        let res: Multihash = from_str(resp[0]["result"].to_string().as_str())?;
+        Ok(res)
+    }
+
+    pub async fn get(&mut self, hash: Multihash) -> In3Result<Bytes> {
+        let resp = rpc(self.client(), Request {
+            method: "ipfs_get",
+            params: json!([hash]),
+        }).await?;
+        let res: Bytes = from_str(resp[0]["result"].to_string().as_str())?;
+        Ok(res)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use async_std::task;
+
+    use crate::prelude::*;
+
+    use super::*;
+
+    #[test]
+    fn test_ipfs_get() -> In3Result<()> {
+        let mut api = Api::new(Client::new(chain::MAINNET));
+        api.client
+            .configure(r#"{"autoUpdateList":false,"nodes":{"0x1":{"needsUpdate":false}}}}"#)?;
+        api.client.set_transport(Box::new(MockTransport {
+            responses: vec![(
+                "ipfs_get",
+                r#"[{"jsonrpc":"2.0","id":1,"result":"Lorem ipsum dolor sit amet"}]"#,
+            )],
+        }));
+        let data: Bytes = task::block_on(
+            api.get("QmbGySCLuGxu2GxVLYWeqJW9XeyjGFvpoZAhGhXDGEUQu8".to_string())
+        ).unwrap();
+        Ok(assert_eq!(data, "Lorem ipsum dolor sit amet".as_bytes().into()))
+    }
+}
