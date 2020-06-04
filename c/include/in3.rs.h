d4e48b95a5d7d4d03b66f16ad9b7dbc46236a616
Simon Jentzsch <simon@slock.it>
2020-06-04 07:55:12 +0200

    fix rust header

--- c/include/in3.rs.h
+++ c/include/in3.rs.h
@@ -1,8 +1,8 @@
 // AUTO-GENERATED FILE
 // See scripts/build_includeh.sh
 #include "../src/core/client/context_internal.h"
-#include "../signer/pk-signer/signer-priv.h"
-#include "../signer/pk-signer/signer.h"
+#include "../src/signer/pk-signer/signer-priv.h"
+#include "../src/signer/pk-signer/signer.h"
 #include "in3/bytes.h"
 #include "in3/client.h"
 #include "in3/context.h"
