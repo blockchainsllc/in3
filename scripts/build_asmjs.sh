ee44f084ae16d9223278fc7d201eda35bcf4c482
Simon Jentzsch <simon@slock.it>
2020-06-20 20:41:22 +0200

    added custom rule to build wasm

--- scripts/build_asmjs.sh
+++ scripts/build_asmjs.sh
@@ -4,6 +4,6 @@ docker run \
   --rm \
   -v $(pwd):$(pwd) \
   docker.slock.it/build-images/cmake:clang11 \
-  /bin/bash -c "cd /$(pwd)/build;  emcmake cmake -DWASM=true -DASMJS=true -DWASM_EMMALLOC=true  -DWASM_EMBED=false  -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DCMAKE_BUILD_TYPE=MINSIZEREL .. && make -j8 in3w"
+  /bin/bash -c "cd /$(pwd)/build;  emcmake cmake -DWASM=true -DASMJS=true -DWASM_EMMALLOC=true  -DWASM_EMBED=false  -DTRANSPORTS=false -DBUILD_DOC=false -DIN3API=true -DIN3_LIB=false -DCMD=false -DUSE_CURL=false -DCMAKE_BUILD_TYPE=DEBUG .. && make -j8 in3_wasm"
 
 cd scripts
