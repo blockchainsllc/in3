8f5a74ca5c2b3f4ada462b932bfb05e7c06be122
Simon Jentzsch <simon@slock.it>
2020-06-20 19:59:41 +0200

    fix abiencoding bug and toNumber(null)

--- wasm/src/in3_util.js
+++ wasm/src/in3_util.js
@@ -151,11 +151,12 @@ function ecSign(pk, data, hashMessage = true, adjustV = true) {
 }
 
 function abiDecode(sig, data) {
+    const types = splitTypes(sig.substr(sig.indexOf(':') + 1))
     data = toUint8Array(data)
     let res = JSON.parse(call_string('abi_decode', sig, data, data.byteLength))
-    if (!Array.isArray(res)) res = [res]
+    if (types.length == 1) res = [res]
     if (!res.length) return []
-    return convertTypes(splitTypes(sig.substr(sig.indexOf(':') + 1)), res)
+    return convertTypes(types, res)
 }
 
 function convertType(val, t) {
@@ -262,6 +263,7 @@ function toNumber(val) {
         case 'null':
             return 0
         default:
+            if (!val) return 0
             if (val.readBigInt64BE) //nodejs Buffer
                 return val.length == 0 ? 0 : parseInt(toMinHex(val))
             else if (val.redIMul)
