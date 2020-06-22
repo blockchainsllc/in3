285d2ed2d35c9e73bbc11f7ca80268903f8af865
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-06-22 11:41:29 +0200

    Fix test_signature()

--- rust/in3-rs/src/signer.rs
+++ rust/in3-rs/src/signer.rs
@@ -59,47 +59,18 @@ impl Signer for In3Signer<'_> {
     }
 }
 
+
 #[cfg(test)]
 mod tests {
-    use std::fmt::Write;
-
     use super::*;
-
-    fn signature_hex_string(data: [u8; 64]) -> String {
-        let mut sign_str = "".to_string();
-        for byte in &data[0..64] {
-            let mut tmp = "".to_string();
-            write!(&mut tmp, "{:02x}", byte).unwrap(); // cannot fail
-            sign_str.push_str(tmp.as_str());
-        }
-        //Equivalent to recoverycode ethereum += 27
-        let mut tmp = "".to_string();
-        write!(&mut tmp, "{:02x}", 28).unwrap(); // cannot fail
-        sign_str.push_str(tmp.as_str());
-        sign_str
-    }
 
     #[test]
     fn test_signature() {
         let msg = "9fa034abf05bd334e60d92da257eb3d66dd3767bba9a1d7a7575533eb0977465";
         let pk = "889dbed9450f7a4b68e0732ccb7cd016dab158e6946d16158f2736fda1143ca6";
-        let msg_hex = msg.from_hex().unwrap(); // cannot fail
-        let pk_hex = pk.from_hex().unwrap(); // cannot fail
-        let mut hasher = Keccak256Full::new();
-        // write input message
-        hasher.input(msg_hex);
-        // read hash digest
-        let result = hasher.result();
-        let mut msg_slice: [u8; 32] = Default::default();
-        msg_slice.copy_from_slice(&result[0..32]);
-        let mut pk_slice: [u8; 32] = Default::default();
-        pk_slice.copy_from_slice(&pk_hex[0..32]);
-        let seckey = SecretKey::parse(&pk_slice).unwrap(); // cannot fail
-        let message = Message::parse(&msg_slice);
-        let (signature, _) = sign(&message, &seckey);
-        let signature_arr = signature.serialize();
-        let sign_str = signature_hex_string(signature_arr);
-        println!(" signature {}", sign_str);
-        assert_eq!(sign_str, "349338b22f8c19d4c8d257595493450a88bb51cc0df48bb9b0077d1d86df3643513e0ab305ffc3d4f9a0f300d501d16556f9fb43efd1a224d6316012bb5effc71c");
+        let mut s = In3Signer::new(pk);
+        let signature = async_std::task::block_on(s.sign(msg));
+        let sign_str = format!("{:?}", signature.unwrap());
+        assert_eq!(sign_str, "0x349338b22f8c19d4c8d257595493450a88bb51cc0df48bb9b0077d1d86df3643513e0ab305ffc3d4f9a0f300d501d16556f9fb43efd1a224d6316012bb5effc71c");
     }
 }
