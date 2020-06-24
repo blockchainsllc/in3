679695cb46c80caf85b58fda10d89b08c37142a6
chirag-parmar <chirag.parmar@slock.it>
2020-06-24 12:25:47 +0200

    use ipfs api for rust example

--- rust/in3-rs/examples/ipfs_put.rs
+++ rust/in3-rs/examples/ipfs_put.rs
@@ -1,31 +1,18 @@
-use serde::{Deserialize, Serialize};
+extern crate in3;
 
 use async_std::task;
-use serde_json;
 
 use in3::prelude::*;
+use in3::ipfs::*;
 
-//Define a struct to collect the parsed JSON response into. The struct needs an extra macro definition
-//of serialize and desserialize as per the requirements of serde JSON
-#[derive(Serialize, Deserialize)]
-struct Response{
-    result: String
-}
 
 fn main() {
-    let mut c = Client::new(chain::IPFS);
+    let mut ipfs_api = Api::new(Client::new(chain::IPFS));
 
     //c.rpc is an asynchrous request (due to the internal C library). Therefore to block execution
     //we use async_std's block_on function
-    match task::block_on(c.rpc(r#"{"method":"ipfs_put","params":["I love Incubed","utf8"]}"#)) {
-        Ok(res) => {
-            //the parsed output is a vector of responses because the JSON response from in3
-            //is packed inside an array i.e. the JSON string looks something like "[{JSON object}]"
-            let v: Vec<Response> = serde_json::from_str(&res).unwrap();
-
-            //print out the IPFS hash
-            println!("The hash is {:?}", v[0].result)
-        },
+    match task::block_on(ipfs_api.put("I love incubed".as_bytes().into())) {
+        Ok(res) => println!("The hash is {:?}", res),
         Err(err) => println!("Failed with error: {}", err),
     }
 }
