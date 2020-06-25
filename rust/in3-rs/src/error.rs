1b05e8ed794ad414112fb382d99071081fed1ed3
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-06-25 14:55:10 +0200

    Rename old error type to SysError and introduce new error type

--- rust/in3-rs/src/error.rs
+++ rust/in3-rs/src/error.rs
@@ -1,15 +1,14 @@
 //! Errors used throughout the library.
-use core::fmt;
 use core::result;
-use std::{convert, ffi};
+use std::convert;
 
 use in3_sys::in3_ret_t::*;
 
 macro_rules! in3_error_def {
     ( $( $( #[$attr:meta] )* => $rust_variant:ident = $cs_variant:ident; )* ) => {
-        #[derive(Clone, Debug, Eq, Hash, PartialEq)]
+        #[derive(Debug, PartialEq, Eq)]
 
-        pub enum Error {
+        pub enum SysError {
             $(
                 $(
                     #[$attr]
@@ -19,25 +18,24 @@ macro_rules! in3_error_def {
 
             UnknownIn3Error,
             TryAgain,
-            CustomError(String),
         }
 
-        impl From<in3_sys::in3_ret_t::Type> for Error {
+        impl From<in3_sys::in3_ret_t::Type> for SysError {
             fn from(err: in3_sys::in3_ret_t::Type) -> Self {
                 match err {
                     $(
-                        $cs_variant => Error::$rust_variant,
+                        $cs_variant => SysError::$rust_variant,
                     )*
-                    _ => Error::UnknownIn3Error,
+                    _ => SysError::UnknownIn3Error,
                 }
             }
         }
 
-        impl From<&Error> for i32 {
-            fn from(err: &Error) -> i32 {
+        impl From<&SysError> for i32 {
+            fn from(err: &SysError) -> i32 {
                 match err {
                     $(
-                         Error::$rust_variant => $cs_variant,
+                         SysError::$rust_variant => $cs_variant,
                     )*
                     _ => std::i32::MIN,
                 }
@@ -70,30 +68,23 @@ in3_error_def!(
 #[must_use]
 pub type In3Result<T> = result::Result<T, Error>;
 
-impl fmt::Display for Error {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        write!(fmt, "{}", self.description())
-    }
+#[derive(Debug)]
+pub enum Error {
+    InternalError(SysError),
+    CustomError(String),
+    JsonError(serde_json::error::Error),
+    Base64Error(base64::DecodeError),
 }
 
-impl Error {
-    fn description(&self) -> &str {
-        use self::Error::*;
-        match self {
-            CustomError(ref msg) => msg,
-            UnknownIn3Error => "Unknown error",
-            _ => unsafe {
-                ffi::CStr::from_ptr(in3_sys::in3_errmsg(self.into()))
-                    .to_str()
-                    .unwrap()
-            },
-        }
+impl convert::From<SysError> for Error {
+    fn from(err: SysError) -> Self {
+        Self::InternalError(err)
     }
 }
 
 impl convert::From<serde_json::error::Error> for Error {
-    fn from(_: serde_json::error::Error) -> Self {
-        Self::DataInvalid
+    fn from(err: serde_json::error::Error) -> Self {
+        Self::JsonError(err)
     }
 }
 
@@ -113,20 +104,19 @@ impl convert::From<String> for Error {
 mod tests {
     use in3_sys::in3_ret_t;
 
-    use super::Error;
+    use super::SysError;
 
     #[test]
     fn test_error() {
         let errors = [
-            Error::NoMemory,
-            Error::UnknownIn3Error,
-            Error::CustomError("Custom error".to_string()),
-            Error::from(in3_ret_t::IN3_ECONFIG),
-            Error::from(500 as in3_ret_t::Type),
+            SysError::NoMemory,
+            SysError::UnknownIn3Error,
+            SysError::from(in3_ret_t::IN3_ECONFIG),
+            SysError::from(500 as in3_ret_t::Type),
         ];
 
         for error in errors.iter() {
-            println!("{}", error);
+            println!("{:?}", error);
         }
     }
 }
