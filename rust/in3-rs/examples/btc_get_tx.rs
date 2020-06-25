b5494df0b488bcfdcb78af49a0a84bea0248ac88
chirag-parmar <chirag.parmar@slock.it>
2020-06-25 14:27:35 +0200

    fix comments in newly adde4d examples

--- rust/in3-rs/examples/btc_get_tx.rs
+++ rust/in3-rs/examples/btc_get_tx.rs
@@ -9,7 +9,7 @@ use in3::eth1::Hash;
 fn main() {
     let mut btc_api = Api::new(Client::new(chain::BTC));
 
-    //c.rpc is an asynchrous request (due to the internal C library). Therefore to block execution
+    //`get_transaction_bytes` is an asynchrous request (due to the internal C library). Therefore to block execution
     //we use async_std's block_on function
     let txid: Hash = serde_json::from_str::<Hash>(r#""0x83ce5041679c75721ec7135e0ebeeae52636cfcb4844dbdccf86644df88da8c1""#).unwrap();
     let tx_data = task::block_on(btc_api.get_transaction_bytes(txid)).unwrap();
--- rust/in3-rs/examples/ipfs_put.rs
+++ rust/in3-rs/examples/ipfs_put.rs
@@ -9,7 +9,7 @@ use in3::ipfs::*;
 fn main() {
     let mut ipfs_api = Api::new(Client::new(chain::IPFS));
 
-    //c.rpc is an asynchrous request (due to the internal C library). Therefore to block execution
+    //`put` is an asynchrous request (due to the internal C library). Therefore to block execution
     //we use async_std's block_on function
     match task::block_on(ipfs_api.put("I love incubed".as_bytes().into())) {
         Ok(res) => println!("The hash is {:?}", res),
