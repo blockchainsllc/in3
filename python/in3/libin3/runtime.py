e22a6f6574bb6dfbd8c8d7076b8f63edd66da5ae
J. Paul Depraz <2923413+cerealkill@users.noreply.github.com>
2020-06-27 12:58:42 +0200

    Runtime clean=up

--- python/in3/libin3/runtime.py
+++ python/in3/libin3/runtime.py
@@ -1,65 +1,13 @@
 """
 Encapsulates low-level rpc calls into a comprehensive runtime.
 """
-import ctypes as c
 import json
 from enum import Enum
 
+import in3.libin3.transport as transport
 from in3.exception import ClientException
 from in3.libin3.enum import RPCCode
 from in3.libin3.lib_loader import libin3_new, libin3_free, libin3_call, libin3_set_pk
-from in3.libin3.transport import NativeRequest, In3Request, In3Response, factory
-
-
-class RPCExecRequest:
-    """
-    RPC request to libin3
-    Args:
-        fn_name: Name of function that will be called in libin3
-        args: Arguments matching the parameters order of this function
-        _id: (optional) Message id
-        rpc_version: (optional) Version of the json-rpc api
-    """
-
-    def __init__(self, fn_name: str, args: tuple = (), verification: str = "proof", _id: int = 1,
-                 rpc_version: str = "2.0"):
-        self.id = _id
-        self.rpc_version = rpc_version
-        self.libin3_function = fn_name
-        self.args = args
-        self.in3_headers = {
-            "verification": verification,
-            "version": "2.1.0"
-        }
-
-    def __bytes__(self):
-        """
-        [{"id":1,"jsonrpc":"2.0","method":"in3_nodeList","params":[],"in3":{"verification":"proof","version": "2.1.0"}}]
-        """
-        return json.dumps({
-            "id": self.id,
-            "jsonrpc": self.rpc_version,
-            "method": self.libin3_function,
-            "params": [arg for arg in self.args],
-            "in3": self.in3_headers
-        }).encode('utf8')
-
-
-class RPCExecResponse:
-    """
-    RPC response from libin3.
-    Args:
-        id: Message id
-        jsonrpc: Version of the json-rpc api
-        result: Function returned value(s)
-        error: (Optional) Filled in case of error
-    """
-
-    def __init__(self, id: str, jsonrpc: str, result: str = None, error: str = None):
-        self.id = id
-        self.jsonrpc = jsonrpc
-        self.result = result
-        self.error = error
 
 
 class RPCCallRequest:
@@ -85,10 +33,11 @@ class In3Runtime:
     Instantiate libin3 and frees it when garbage collected.
     Args:
         chain_id (int): Chain-id based on EIP-155. If None provided, will connect to the Ethereum network. i.e: 0x1 for mainNet
+        transport_fn: Transport function to handle the HTTP Incubed Network requests.
     """
 
-    def __init__(self, chain_id: int, transport):
-        self.transport_handler = factory(transport)
+    def __init__(self, chain_id: int, transport_fn):
+        self.transport_handler = transport.factory(transport_fn)
         self.in3 = libin3_new(chain_id, self.transport_handler)
         # libin3_set_storage_handler(self.in3, storage.retrieve, storage.store, storage.delete_all)
         self.chain_id = chain_id
@@ -120,40 +69,3 @@ class In3Runtime:
             secret: SK 256 bit number. example: int(0x387a8233c96e1fc0ad5e284353276177af2186e7afa85296f106336e376669f7, 16)
         """
         return libin3_set_pk(self.in3, hex(secret).encode('utf8'))
-
-    # TODO: remove
-    # def execute(self, fn_name: str or Enum, *args) -> str or dict:
-    #     """
-    #     Make a remote procedure call to a function in libin3
-    #     Args:
-    #         fn_name (str or Enum): Name of the function to be called
-    #         *args: Arguments matching the parameters order of this function
-    #     Returns:
-    #         fn_return (str): String of values returned by the function, if any.
-    #     """
-    #     request = RPCExecRequest(str(fn_name), args=args)
-    #     request_bytes = bytes(request)
-    #     response_bytes = libin3_exec(self.in3, rpc=request_bytes)
-    #     response_str = response_bytes.decode('utf8').replace('\n', ' ')
-    #     if 'error' in response_str:
-    #         raise ClientException(response_str)
-    #     response_dict = json.loads(response_str)
-    #     response = RPCExecResponse(**response_dict)
-    #     return response.result
-
-
-def asdasd(custom_transport):
-    """
-    Factory-like higher-order function that decorates a transport function augmenting its capabilities for native interoperability
-    """
-
-    # This is needed to prevent the handler function to be freed. Without it, there would be a fault.
-    def transport_factory(native_request: NativeRequest):
-        request = In3Request(native_request)
-        response = In3Response(native_request)
-        return custom_transport(request, response)
-
-    # the transport function to be implemented by the transport provider.
-    # typedef in3_ret_t (*in3_transport_send)(in3_request_t* request);
-    custom_transport_signature = c.CFUNCTYPE(c.c_int, c.POINTER(NativeRequest))
-    return custom_transport_signature(transport_factory)
