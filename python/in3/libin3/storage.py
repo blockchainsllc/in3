97c9d7af7510643ba6676e11f42375e6cf45d911
J. Paul Depraz <2923413+cerealkill@users.noreply.github.com>
2020-06-27 17:21:14 +0200

    safety commit during a storm

--- python/in3/libin3/storage.py
+++ python/in3/libin3/storage.py
@@ -1,38 +1,53 @@
 import ctypes as c
 
+in3_storage_get_item = c.CFUNCTYPE(c.POINTER(c.c_char), c.c_void_p, c.POINTER(c.c_char))
+in3_storage_set_item = c.CFUNCTYPE(None, c.c_void_p, c.POINTER(c.c_char_p), c.POINTER(c.c_char))
+in3_storage_clear = c.CFUNCTYPE(None, c.c_void_p)
+
 
 class NativeRequest(c.Structure):
     """
     Request sent by the libin3 to the In3 Network, transported over the _http_transport function
     Based on in3/client/.h in3_request_t struct
     """
+    _fields_ = [("get_item", c.POINTER(in3_storage_get_item)),
+                ("set_item", c.POINTER(in3_storage_set_item)),
+                ("clear", c.POINTER(in3_storage_clear)),
+                ("cptr", c.c_void_p)]
+
+
+def factory(get_item_fn, set_item_fn, clear_fn):
     """
-    _fields_ = [("payload", c.POINTER(c.c_char)),
-                ("urls", c.POINTER(c.POINTER(c.c_char))),
-                ("urls_len", c.c_int),
-                ("results", c.c_void_p),
-                ("timeout", c.c_uint32),
-                ("times", c.c_uint32)]
+    C level abstraction of a transport handler.
+    Decorates a transport function augmenting its capabilities for native interoperability
+
+    void in3_set_default_storage(in3_storage_handler_t* cacheStorage) {
+      default_storage = cacheStorage;
+    }
+
+    typedef struct in3_storage_handler {
+      in3_storage_get_item get_item; /**< function pointer returning a stored value for the given key.*/
+      in3_storage_set_item set_item; /**< function pointer setting a stored value for the given key.*/
+      in3_storage_clear    clear;    /**< function pointer clearing all contents of cache.*/
+      void*                cptr;     /**< custom pointer which will be passed to functions */
+    } in3_storage_handler_t;
+
+    typedef bytes_t* (*in3_storage_get_item)(
+        void*       cptr, /**< a custom pointer as set in the storage handler*/
+        const char* key   /**< the key to search in the cache */
+    );
+
+    typedef void (*in3_storage_set_item)(
+        void*       cptr, /**< a custom pointer as set in the storage handler*/
+        const char* key,  /**< the key to store the value.*/
+        bytes_t*    value /**< the value to store.*/
+    );
+
+    typedef void (*in3_storage_clear)(
+        void* cptr /**< a custom pointer as set in the storage handler*/
+    );
+
     """
-#
-#     @c.CFUNCTYPE(c.c_void_p, c.POINTER(c.c_int), c.POINTER(c.c_char_p), c.POINTER(c.c_char))
-#
-#     @c.CFUNCTYPE(c.c_ubyte, c.POINTER(c.c_int), c.POINTER(c.c_char))
-#
-#     @c.CFUNCTYPE(c.c_void_p, c.POINTER(c.c_int))
-#
-# def factory(transport_fn):
-#     """
-#     C level abstraction of a transport handler.
-#     Decorates a transport function augmenting its capabilities for native interoperability
-#     """
-#
-#     def new(native_request: NativeRequest):
-#         request = In3Request(native_request)
-#         response = In3Response(native_request)
-#         return transport_fn(request, response)
-#
-#     # the transport function to be implemented by the transport provider.
-#     # typedef in3_ret_t (*in3_transport_send)(in3_request_t* request);
-#     c_transport_fn = c.CFUNCTYPE(c.c_int, c.POINTER(NativeRequest))
-#     return c_transport_fn(new)
+
+    c_transport_fn = c.CFUNCTYPE(c.c_int, c.POINTER(NativeRequest))
+    return c_transport_fn(in3_storage_get_item, in3_storage_set_item, in3_storage_clear, None)
