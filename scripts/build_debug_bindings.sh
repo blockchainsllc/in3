fd86fc5878f1aae182868287e64e1dd7d456f2a0
Paul Depraz <paul.depraz@slock.it>
2020-06-30 08:58:30 +0000

    Apply suggestion to scripts/build_debug_bindings.sh

--- scripts/build_debug_bindings.sh
+++ scripts/build_debug_bindings.sh
@@ -4,5 +4,5 @@ mkdir -p build
 cd build
 rm -rf *
 export CTEST_OUTPUT_ON_FAILURE=1
-cmake -DCMAKE_BUILD_TYPE=Debug -DIN3_SERVER=true -DPOA=false -DJAVA=true -DUSE_SEGGER_RTT=false -DIN3_LIB=true -DTEST=true -DCMAKE_EXPORT_COMPILE_COMMANDS=true .. && make -j8
+cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j8
 cd ../scripts
