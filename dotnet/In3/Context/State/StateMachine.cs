e2b321eb9b7142cff8a6f1908d6d0b857f259bab
Simon Jentzsch <simon@slock.it>
2020-07-06 12:37:23 +0200

    no waiting-state yet

--- dotnet/In3/Context/State/StateMachine.cs
+++ dotnet/In3/Context/State/StateMachine.cs
@@ -13,7 +13,9 @@ namespace In3.Context.Action
             {
                 {ContextState.CTX_SUCCESS, new OkState()},
                 {ContextState.CTX_WAITING_TO_SEND, new WaitingState()},
-                {ContextState.CTX_WAITING_FOR_RESPONSE, new WaitingState()},
+                // TODO as long as we fetch all responses (as we do now), CTX_WAITING_FOR_RESPONSE will never happen.
+                // so we might be able to skip it for now
+//                {ContextState.CTX_WAITING_FOR_RESPONSE, new WaitingState()}, 
                 {ContextState.CTX_ERROR, new ErrorState()}
             };
 
