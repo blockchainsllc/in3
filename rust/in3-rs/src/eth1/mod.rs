ec3b7fb001cd04944d121cf43d67467152d22951
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-07-13 22:12:47 +0200

    Add ens module

--- rust/in3-rs/src/eth1/mod.rs
+++ rust/in3-rs/src/eth1/mod.rs
@@ -1,8 +1,10 @@
 //! IN3 API implementation for the Ethereum blockchain.
 pub use self::abi::*;
 pub use self::api::*;
+pub use self::ens::*;
 pub use self::types::*;
 
 pub mod abi;
 pub mod api;
+pub mod ens;
 pub mod types;
