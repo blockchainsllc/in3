6e8cda0ec06188264dccb4f08dbba53803941ef8
Shoaib <shoaib.ahmed@slock.it>
2020-07-21 22:55:57 +0200

    Use fixed version 0.0.0 in manifest instead of placeholder

--- scripts/build_rust.sh
+++ scripts/build_rust.sh
@@ -5,6 +5,7 @@ mkdir -p rust/in3-sys/in3-core/c
 cp -r c/CMakeLists.txt c/macro.cmake c/compiler.cmake c/docs c/src c/include rust/in3-sys/in3-core/c/
 cp CMakeLists.txt rust/in3-sys/in3-core/
 export UPDATE_IN3_BINDINGS=1
+#find rust -name 'Cargo.toml' -exec sed -i '' -e "s/version = \"<ci-tag>\"/version = \"0.0.1\"/g" {} \;
 cd rust && cargo clean && cargo build
 # cd rust && cargo build
 cd ../scripts
