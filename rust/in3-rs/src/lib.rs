bcf4d4b3f8037ba68ab777ea4023427406891ed9
Shoaib <shoaib.ahmed@slock.it>
2020-08-07 12:44:12 +0200

    Add in3::init() for rust

--- rust/in3-rs/src/lib.rs
+++ rust/in3-rs/src/lib.rs
@@ -40,3 +40,39 @@ pub mod prelude {
     pub use crate::transport::{HttpTransport, MockJsonTransport, MockTransport};
     pub use crate::types::*;
 }
+
+
+#[inline]
+pub fn init() {
+    use std::sync::Once;
+
+    /// Used to prevent concurrent or duplicate initialization.
+    static INIT: Once = Once::new();
+
+    /// An exported constructor function. On supported platforms, this will be
+    /// invoked automatically before the program's `main` is called.
+    #[cfg_attr(
+    any(target_os = "linux", target_os = "freebsd", target_os = "android"),
+    link_section = ".init_array"
+    )]
+    #[cfg_attr(target_os = "macos", link_section = "__DATA,__mod_init_func")]
+    #[cfg_attr(target_os = "windows", link_section = ".CRT$XCU")]
+    static INIT_CTOR: extern "C" fn() = init_inner;
+
+    /// This is the body of our constructor function.
+    #[cfg_attr(
+    any(target_os = "linux", target_os = "android"),
+    link_section = ".text.startup"
+    )]
+    extern "C" fn init_inner() {
+        INIT.call_once(|| {
+            unsafe {
+                in3_sys::in3_init();
+            }
+        });
+    }
+
+    // We invoke our init function through our static to ensure the symbol isn't
+    // optimized away by a bug: https://github.com/rust-lang/rust/issues/47384
+    INIT_CTOR();
+}
