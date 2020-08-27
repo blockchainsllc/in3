4a7e08621e70c6f31d5089b455c4290686c80f84
Simon Jentzsch <simon@slock.it>
2020-08-27 14:02:06 +0200

    more memory

--- c/src/pay/zksync/provider.c
+++ c/src/pay/zksync/provider.c
@@ -5,7 +5,7 @@
 #include "zksync.h"
 in3_ret_t send_provider_request(in3_ctx_t* parent, zksync_config_t* conf, char* method, char* params, d_token_t** result) {
   if (params == NULL) params = "";
-  char* in3 = conf ? alloca(strlen(conf->provider_url) + 16) : NULL;
+  char* in3 = conf ? alloca(strlen(conf->provider_url) + 26) : NULL;
   if (in3) sprintf(in3, "{\"rpc\":\"%s\"}", conf->provider_url);
   return ctx_send_sub_request(parent, method, params, in3, result);
 }
