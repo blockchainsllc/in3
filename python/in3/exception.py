3111d5ab1022af30c1683ab98b4654d8f07077fe
unparalleled-js <yingthi@live.com>
2020-08-30 09:00:06 -0500

    Change base of ENS format exception

--- python/in3/exception.py
+++ python/in3/exception.py
@@ -33,6 +33,6 @@ class TransportException(IN3BaseException):
     pass
 
 
-class EnsDomainFormatAssertionException(AssertionError):
+class EnsDomainFormatAssertionException(IN3BaseException):
     def __init__(self):
         super().__init__('Client: ENS domain name must end with .eth')
