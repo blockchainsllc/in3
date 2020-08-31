78e60478be6c5505b51b01d06850c35b4d3df4e1
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-08-31 22:26:59 +0200

    Fix sentry when not enabled

--- c/src/core/client/context.c
+++ c/src/core/client/context.c
@@ -150,9 +150,10 @@ in3_ret_t ctx_set_error_intern(in3_ctx_t* ctx, char* message, in3_ret_t errnumbe
       strcpy(dst, message);
     }
     ctx->error = dst;
-
+#ifdef SENTRY
     sentry_ctx_t sctx = {.msg = message};
-    in3_plugin_execute_first(ctx, PLGN_ACT_LOG_ERROR, &sctx);
+    in3_plugin_execute_first_or_none(ctx, PLGN_ACT_LOG_ERROR, &sctx);
+#endif
     in3_log_trace("Intermediate error -> %s\n", message);
   }
   else if (!ctx->error) {
