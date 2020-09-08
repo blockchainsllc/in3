a86100522c8158285b241a1e6e49cb9774b361b7
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-09-08 13:11:58 +0200

    Fix leak

--- c/src/api/eth1/eth_api.c
+++ c/src/api/eth1/eth_api.c
@@ -634,13 +634,15 @@ static eth_tx_receipt_t* parse_tx_receipt(d_token_t* result) {
 }
 
 void eth_tx_receipt_free(eth_tx_receipt_t* txr) {
-  eth_log_t *curr = txr->logs, *next = NULL;
-  while (curr != NULL) {
-    next = curr->next;
-    eth_log_free(curr);
-    curr = next;
+  if (txr && txr->logs) {
+    eth_log_t *curr = txr->logs, *next = NULL;
+    while (curr != NULL) {
+      next = curr->next;
+      eth_log_free(curr);
+      curr = next;
+    }
+    _free(txr);
   }
-  _free(txr);
 }
 
 eth_tx_receipt_t* eth_getTransactionReceipt(in3_t* in3, bytes32_t tx_hash) {
