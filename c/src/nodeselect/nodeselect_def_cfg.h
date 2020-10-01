a38852a5a029e1b1d352bbbbefe16bae9bb5cdfa
Shoaib <shoaib.ahmed@slock.it>
2020-10-01 21:18:40 +0530

    Add contract/registry to boot node config

--- c/src/nodeselect/nodeselect_def_cfg.h
+++ c/src/nodeselect/nodeselect_def_cfg.h
@@ -4,6 +4,8 @@
 #include "../core/client/client.h"
 
 #define BOOT_NODES_MAINNET "{"                                                                                             \
+                           " \"contract\": \"0xac1b824795e1eb1f6e609fe0da9b9af8beaab60f\","                                \
+                           " \"registryId\": \"0x23d5345c5c13180a8080bd5ddbe7cde64683755dcce6e734d95b7b573845facb\","      \
                            " \"servers\": {"                                                                               \
                            "  \"" STR(CHAIN_ID_MAINNET) "\": {"                                                            \
                                                         "   \"nodeList\": [{"                                              \
@@ -33,6 +35,8 @@
 
 #ifdef IN3_STAGING
 #define BOOT_NODES_KOVAN "{"                                                                                           \
+                         " \"contract\": \"0x0604014f2a5fdfafce3f2ec10c77c31d8e15ce6f\","                              \
+                         " \"registryId\": \"0xd440f01322c8529892c204d3705ae871c514bafbb2f35907832a07322e0dc868\","    \
                          " \"servers\": {"                                                                             \
                          "  \"" STR(CHAIN_ID_KOVAN) "\": {"                                                            \
                                                     "   \"nodeList\": [{"                                              \
@@ -48,6 +52,8 @@
                                                     " }"                                                               \
                                                     "}"
 #define BOOT_NODES_GOERLI "{"                                                                                            \
+                          " \"contract\": \"0x814fb2203f9848192307092337340dcf791a3fed\","                               \
+                          " \"registryId\": \"0x0f687341e0823fa5288dc9edd8a00950b35cc7e481ad7eaccaf61e4e04a61e08\","     \
                           " \"servers\": {"                                                                              \
                           "  \"" STR(CHAIN_ID_GOERLI) "\": {"                                                            \
                                                       "   \"nodeList\": [{"                                              \
@@ -64,6 +70,8 @@
                                                       "}"
 #else /* IN3_STAGING */
 #define BOOT_NODES_KOVAN "{"                                                                                           \
+                         " \"contract\": \"0x4c396dcf50ac396e5fdea18163251699b5fcca25\","                              \
+                         " \"registryId\": \"0x92eb6ad5ed9068a24c1c85276cd7eb11eda1e8c50b17fbaffaf3e8396df4becf\","    \
                          " \"servers\": {"                                                                             \
                          "  \"" STR(CHAIN_ID_KOVAN) "\": {"                                                            \
                                                     "   \"nodeList\": [{"                                              \
@@ -80,6 +88,8 @@
                                                     "}"
 
 #define BOOT_NODES_GOERLI "{"                                                                                            \
+                          " \"contract\": \"0x5f51e413581dd76759e9eed51e63d14c8d1379c8\","                               \
+                          " \"registryId\": \"0x67c02e5e272f9d6b4a33716614061dd298283f86351079ef903bf0d4410a44ea\","     \
                           " \"servers\": {"                                                                              \
                           "  \"" STR(CHAIN_ID_GOERLI) "\": {"                                                            \
                                                       "   \"nodeList\": [{"                                              \
@@ -96,6 +106,8 @@
                                                       "}"
 
 #define BOOT_NODES_IPFS "{"                                                                                          \
+                        " \"contract\": \"0xa93b57289070550c82edb1106e12bb37138948b8\","                             \
+                        " \"registryId\": \"0xf0162ec6d785ee990e36bad865251f45af0916cf136169540c02b0dd9cb69196\","   \
                         " \"servers\": {"                                                                            \
                         "  \"" STR(CHAIN_ID_IPFS) "\": {"                                                            \
                                                   "   \"nodeList\": [{"                                              \
@@ -112,6 +124,8 @@
                                                   "}"
 
 #define BOOT_NODES_BTC "{"                                                                                         \
+                       " \"contract\": \"0xc2c05fbfe76ee7748ae5f5b61b57a46cc4061c32\","                            \
+                       " \"registryId\": \"0x53786c93e54c21d9852d093c394eee9df8d714d8f2534cdf92f9c9998c528d19\","  \
                        " \"servers\": {"                                                                           \
                        "  \"" STR(CHAIN_ID_BTC) "\": {"                                                            \
                                                 "   \"nodeList\": [{"                                              \
@@ -128,6 +142,8 @@
                                                 "}"
 
 #define BOOT_NODES_EWC "{"                                                                                         \
+                       " \"contract\": \"0x039562872008f7a76674a6e7842804f0ad37cb13\","                                                                    \
+                       " \"registryId\": \"0x313454c05fc6e5336a3315ed2233da6b831d4cb826d836c3d603f2e2a9f1ed75\","                                                                  \
                        " \"servers\": {"                                                                           \
                        "  \"" STR(CHAIN_ID_EWC) "\": {"                                                            \
                                                 "   \"nodeList\": [{"                                              \
@@ -144,6 +160,7 @@
                                                 "}"
 
 #define BOOT_NODES_LOCAL "{"                                                                                           \
+                         " \"contract\": \"0xf0fb87f4757c77ea3416afe87f36acaa0496c7e9\","                                                                      \
                          " \"servers\": {"                                                                             \
                          "  \"" STR(CHAIN_ID_LOCAL) "\": {"                                                            \
                                                     "   \"nodeList\": [{"                                              \
