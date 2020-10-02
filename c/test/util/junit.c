0361554f32b4e20bff77d48d763a7e2d18b99aa6
Simon Jentzsch <simon@slock.it>
2020-10-02 08:21:06 +0200

    fix typo

--- c/test/util/junit.c
+++ c/test/util/junit.c
@@ -118,8 +118,7 @@ static void escape(sb_t* sb, char* c) {
       case 27:
         break;
       default:
-        if ()
-          sb_add_char(sb, (*c < 10 || *c > 128) ? 32 : *c);
+        sb_add_char(sb, (*c < 10 || *c > 128) ? 32 : *c);
         break;
     }
   }
