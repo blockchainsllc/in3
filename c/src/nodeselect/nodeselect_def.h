b461ba10ad29f9dc2710a768526a154d9d284295
Shoaib <shoaib.ahmed@slock.it>
2020-10-06 16:40:24 +0530

    Export in3_nodeselect_def()

--- c/src/nodeselect/nodeselect_def.h
+++ c/src/nodeselect/nodeselect_def.h
@@ -2,10 +2,21 @@
 #define IN3_NODE_SELECT_DEF_H
 
 #include "../core/client/context.h"
+#include "../core/client/plugin.h"
 #include "nodelist.h"
 
 #ifdef NODESELECT_DEF
+
+/**
+ * default nodeselect implementation
+ */
+in3_ret_t in3_nodeselect_def(void* plugin_data, in3_plugin_act_t action, void* plugin_ctx);
+
+/**
+ * registers the default nodeselect implementation
+ */
 in3_ret_t in3_register_nodeselect_def(in3_t* c);
-#endif
+
+#endif //NODESELECT_DEF
 
 #endif //IN3_NODE_SELECT_DEF_H
