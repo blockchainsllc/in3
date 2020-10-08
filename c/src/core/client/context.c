2c15cb0f542ca2a0e6c6122ef5318a2e68325edb
Camilo Soto Valenzuela <camilo.soto.valenzuela@slock.it>
2020-10-08 22:30:35 +0200

    Fix leak

--- c/src/core/client/context.c
+++ c/src/core/client/context.c
@@ -161,8 +161,12 @@ in3_ret_t ctx_set_error_intern(in3_ctx_t* ctx, char* message, in3_ret_t errnumbe
       dst = _malloc(l + 1);
       strcpy(dst, message);
     }
-    ctx->error           = dst;
-    error_log_ctx_t sctx = {.msg = message, .error = -errnumber, .ctx_req = ctx->request_context->c, .response = ctx->raw_response->data.data};
+    ctx->error = dst;
+    char* res  = _malloc(2 + strlen(ctx->request_context->c));
+    strcpy(res, ctx->request_context->c);
+    char* req = _malloc(2 + ctx->raw_response->data.len);
+    strcpy(req, ctx->raw_response->data.data);
+    error_log_ctx_t sctx = {.msg = message, .error = -errnumber, .ctx_req = req, .response = res};
     in3_plugin_execute_first_or_none(ctx, PLGN_ACT_LOG_ERROR, &sctx);
 
     in3_log_trace("Intermediate error -> %s\n", message);
