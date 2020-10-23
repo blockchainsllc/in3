2c0b77945ceea59c5d7119aac67a2953a226c926
Shoaib <shoaib.ahmed@slock.it>
2020-10-23 18:13:14 +0530

    Impl macro2bin.sh

---
+++ c/prebuild/macro2bin.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+HEADERFILE=$1
+MACRO=0
+MULTILINE=0
+DEF=""
+NEWDEF=""
+
+while IFS="" read -r p || [ -n "$p" ]; do
+  if [[ $p == *"#define "*" DECOMPOSE("* ]]; then
+    NEWDEF="${p%DECOMPOSE*}"
+    if [[ $p != *"\\" ]]; then
+      DEF=${p##*(}
+      DEF=$(echo "${p##*(}" | tr -d '[:space:]' | sed -e 's/^"//' -e 's/"$//')
+      DEF=${DEF:0:$((${#DEF} - 2))}
+      printf '// %s\n' "$p"
+      echo -n "$NEWDEF"
+      DEF=$(echo -n "$DEF" | sed 's/\\//g' | json)
+      DEF=${DEF:0:$((${#DEF} - 1))}
+      echo '"'"${DEF}"'"'
+      DEF=""
+      NEWDEF=""
+      continue
+    fi
+    MACRO=1
+  fi
+
+  if [[ $p == *"\\" ]]; then
+    MULTILINE=1
+  else
+    MULTILINE=0
+  fi
+
+  if [[ $MACRO == 1 ]]; then
+    printf '// %s\n' "$p"
+    if [[ $MULTILINE == 0 ]]; then
+      MACRO=0
+      temp="$p"
+      temp=$(echo "$temp" | tr -d '[:space:]' | sed -e 's/^"//' -e 's/"$//')
+      temp=${temp:0:$((${#temp} - 2))}
+      DEF="$DEF$temp"
+      echo -n "$NEWDEF"
+      DEF=$(echo -n "$DEF" | sed 's/\\//g' | json)
+      DEF=${DEF:0:$((${#DEF} - 1))}
+      echo '"'"${DEF}"'"'
+      DEF=""
+      NEWDEF=""
+    else
+      temp=${p##*(}
+      temp=$(echo "$temp" | tr -d '[:space:]')
+      temp=${temp:0:$((${#temp} - 1))}
+      temp=$(echo "$temp" | sed -e 's/^"//' -e 's/"$//')
+      DEF="$DEF$temp"
+    fi
+  else
+    printf '%s\n' "$p"
+  fi
+done <$HEADERFILE
