e9b084df537ab4f6d1e5955e506d476cf3c6bb7b
leonardocardoso <leonardo.t.cardoso@slock.it>
2020-11-04 15:30:48 +0100

    Fix java examples

--- java/examples/Configure.java
+++ java/examples/Configure.java
@@ -18,16 +18,14 @@ public class Configure {
     ClientConfiguration clientConfig = in3.getConfig();
     clientConfig.setReplaceLatestBlock(6); // define that latest will be -6
     clientConfig.setAutoUpdateList(false); // prevents node automatic update
-    clientConfig.setMaxAttempts(1);        // sets max attempts to 1 before giving up
-    clientConfig.setProof(Proof.none);     // does not require proof (not recommended)
+    clientConfig.setMaxAttempts(1); // sets max attempts to 1 before giving up
+    clientConfig.setProof(Proof.none); // does not require proof (not recommended)
 
     // Setup the ChainConfiguration object for the nodes on a certain chain
-    ChainConfiguration chainConfiguration = new ChainConfiguration(Chain.GOERLI, clientConfig);
+    NodeRegistryConfiguration chainConfiguration = clientConfig.getNodeRegistry();
     chainConfiguration.setNeedsUpdate(false);
     chainConfiguration.setContract("0xac1b824795e1eb1f6e609fe0da9b9af8beaab60f");
     chainConfiguration.setRegistryId("0x23d5345c5c13180a8080bd5ddbe7cde64683755dcce6e734d95b7b573845facb");
-
-    in3.setConfig(clientConfig);
 
     Block block = in3.getEth1API().getBlockByNumber(Block.LATEST, true);
     System.out.println(block.getHash());
