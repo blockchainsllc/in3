d723a5edbdc9ec1fc2dfe7f72ea9a3faa89f5630
Shoaib <shoaib.ahmed@slock.it>
2020-11-06 13:31:17 +0530

    Add node and dont_blacklist to vctx

--- c/src/core/client/plugin.h
+++ c/src/core/client/plugin.h
@@ -368,15 +368,17 @@ void in3_set_storage_handler(
  * verification context holding the pointers to all relevant toknes.
  */
 typedef struct {
-  in3_ctx_t*   ctx;                   /**< Request context. */
-  in3_chain_t* chain;                 /**< the chain definition. */
-  d_token_t*   result;                /**< the result to verify */
-  d_token_t*   request;               /**< the request sent. */
-  d_token_t*   proof;                 /**< the delivered proof. */
-  in3_t*       client;                /**< the client. */
-  uint64_t     last_validator_change; /**< Block number of last change of the validator list */
-  uint64_t     currentBlock;          /**< Block number of latest block */
-  int          index;                 /**< the index of the request within the bulk */
+  in3_ctx_t*    ctx;                   /**< Request context. */
+  in3_chain_t*  chain;                 /**< the chain definition. */
+  d_token_t*    result;                /**< the result to verify */
+  d_token_t*    request;               /**< the request sent. */
+  d_token_t*    proof;                 /**< the delivered proof. */
+  in3_t*        client;                /**< the client. */
+  uint64_t      last_validator_change; /**< Block number of last change of the validator list */
+  uint64_t      currentBlock;          /**< Block number of latest block */
+  int           index;                 /**< the index of the request within the bulk */
+  node_match_t* node;                  /**< the node who delivered this response */
+  bool          dont_blacklist;        /**< indicates whether the plugin would like the node to be blacklisted */
 } in3_vctx_t;
 
 #ifdef LOGGING
