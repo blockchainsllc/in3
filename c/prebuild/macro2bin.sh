c05f7a6cafd20c2a0f6c56f6aeef89beabc3eef5
Shoaib <shoaib.ahmed@slock.it>
2020-11-10 15:51:36 +0530

    Fix macro2bin.sh for multi-line macros

--- c/prebuild/macro2bin.sh
+++ c/prebuild/macro2bin.sh
@@ -8,8 +8,10 @@ NEWDEF=""
 
 while IFS="" read -r p || [ -n "$p" ]; do
   if [[ $p == "#define "*" JSON_TO_BIN("* ]]; then
+    # macro start
     NEWDEF=$(echo $p | sed -e 's/#define \(.*\) JSON_TO_BIN\(.*\)/\1/')
     if [[ $p != *"\\" ]]; then
+      # one-liner
       DEF=${p##*(}
       DEF=$(echo "${p##*(}" | tr -d '[:space:]' | sed -e 's/^"//' -e 's/"$//')
       DEF=${DEF:0:$((${#DEF} - 2))}
@@ -22,18 +24,25 @@ while IFS="" read -r p || [ -n "$p" ]; do
       NEWDEF=""
       continue
     fi
+
+    # use old-style comments for multi-liners
+    printf '/*\n'
     MACRO=1
   fi
 
+  # detect multi-line comment
   if [[ $p == *"\\" ]]; then
     MULTILINE=1
   else
     MULTILINE=0
   fi
 
+  # handle multi-line macros
   if [[ $MACRO == 1 ]]; then
-    printf '// %s\n' "$p"
+    printf '%s\n' "$p"
     if [[ $MULTILINE == 0 ]]; then
+      # multi-line macro ends
+      printf '*/\n'
       MACRO=0
       temp="$p"
       temp=$(echo "$temp" | tr -d '[:space:]' | sed -e 's/^"//' -e 's/"$//')
@@ -45,6 +54,7 @@ while IFS="" read -r p || [ -n "$p" ]; do
       DEF=""
       NEWDEF=""
     else
+      # macro lines
       temp=${p##*(}
       temp=$(echo "$temp" | tr -d '[:space:]')
       temp=${temp:0:$((${#temp} - 1))}
@@ -52,6 +62,7 @@ while IFS="" read -r p || [ -n "$p" ]; do
       DEF="$DEF$temp"
     fi
   else
+    # reproduce non-macro lines as is
     printf '%s\n' "$p"
   fi
 done <$HEADERFILE
