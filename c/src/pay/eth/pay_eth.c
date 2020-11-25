e6240ae4708cf60fed015a76f5cfc02e620e01e6
Shoaib <shoaib.ahmed@slock.it>
2020-11-25 17:45:04 +0530

    Impl plugin acts for pay_eth

--- c/src/pay/eth/pay_eth.c
+++ c/src/pay/eth/pay_eth.c
@@ -49,47 +49,43 @@ uint64_t calc_request_units(in3_ctx_t* ctx) {
   return ctx->len;
 }
 
-in3_ret_t pay_eth_follow_up(void* _ctx, void* _node, d_token_t* in3, d_token_t* error, void* _cptr) {
-  in3_pay_eth_config_t* conf = _cptr;
-  in3_ctx_t*            ctx  = _ctx;
-  node_match_t*         node = _node;
-  d_token_t *           pay  = d_get(in3, key("pay")), *t;
+in3_ret_t pay_eth_follow_up(in3_pay_eth_t* data, in3_pay_followup_ctx_t* plugin_ctx) {
+  in3_ctx_t* ctx = plugin_ctx->ctx;
+  d_token_t *pay = d_get(plugin_ctx->resp_in3, key("pay")), *t;
   if (!pay || !ctx) return IN3_OK;
 
   // TODO Is this a attackvector? We are trusting the nonce without proof, which means if a node sends a
   // nonce as nonce +2, the node would not get any payment. But also other would not get any payment either.
-  if ((t = d_get(pay, key("gasPrice")))) conf->gas_price = d_long(t);
-  if ((t = d_get(pay, key("nonce")))) conf->nonce = d_long(t);
+  if ((t = d_get(pay, key("gasPrice")))) data->gas_price = d_long(t);
+  if ((t = d_get(pay, key("nonce")))) data->nonce = d_long(t);
 
-  if (node->weight) {
-    if ((t = d_get(pay, key("payed")))) node->weight->payed = d_long(t);
-    if ((t = d_get(pay, key("price")))) node->weight->price = d_long(t);
-    if (error && d_get_intk(error, K_CODE) == IN3_EPAYMENT_REQUIRED) {
-      // TODO now we need to decide whether it's worth to pay
-      if (node->weight->price && (conf->max_price == 0 || node->weight->price < conf->max_price))
-        return IN3_WAITING;
-    }
-  }
+  // fixme 666: interaction with nodelist?
+  //  node_match_t* node = plugin_ctx->node;
+  //  if (node->weight) {
+  //    if ((t = d_get(pay, key("payed")))) node->weight->payed = d_long(t);
+  //    if ((t = d_get(pay, key("price")))) node->weight->price = d_long(t);
+  //    if (plugin_ctx->resp_error && d_get_intk(plugin_ctx->resp_error, K_CODE) == IN3_EPAYMENT_REQUIRED) {
+  //      // TODO now we need to decide whether it's worth to pay
+  //      if (node->weight->price && (conf->max_price == 0 || node->weight->price < conf->max_price))
+  //        return IN3_WAITING;
+  //    }
+  //  }
 
   return IN3_OK;
 }
 
-in3_ret_t pay_eth_prepare(void* _ctx, void* _cptr) {
-  if (_ctx == NULL || _cptr == NULL) return IN3_EINVAL;
+in3_ret_t pay_eth_prepare(in3_pay_eth_t* data, in3_ctx_t* ctx) {
+  if (data == NULL || ctx == NULL) return IN3_EINVAL;
   return IN3_OK;
 }
 
-void pay_eth_free(void* cptr) {
-  _free(cptr);
-}
-
-static void create_signed_tx(bytes32_t key, sb_t* sb, address_t to, uint64_t _value, in3_pay_eth_config_t* conf, uint64_t chainId) {
+static void create_signed_tx(in3_pay_eth_t* data, bytes32_t key, sb_t* sb, address_t to, uint64_t _value, uint64_t chainId) {
   bytes32_t buffer;
   bytes_t   nonce = bytes(buffer, 8), gas_price = bytes(buffer + 8, 8), gas_limit = bytes(buffer + 16, 8), value = bytes(buffer + 24, 8);
   uint8_t   sig[65];
   long_to_bytes(_value, value.data);
-  long_to_bytes(conf->nonce, nonce.data);
-  long_to_bytes(conf->gas_price, gas_price.data);
+  long_to_bytes(data->nonce, nonce.data);
+  long_to_bytes(data->gas_price, gas_price.data);
   long_to_bytes(21000, gas_limit.data);
   // create raw without signature
   bytes_t* raw = serialize_tx_raw(nonce, gas_price, gas_limit, bytes(to, 20), value, bytes(NULL, 0), chainId, bytes(NULL, 0), bytes(NULL, 0));
@@ -103,62 +99,93 @@ static void create_signed_tx(bytes32_t key, sb_t* sb, address_t to, uint64_t _va
   b_free(raw);
 }
 
-in3_ret_t pay_eth_handle_request(void* _ctx, sb_t* sb, in3_request_config_t* rc, void* _cptr) {
-  in3_pay_eth_config_t* conf    = _cptr;
-  in3_ctx_t*            ctx     = _ctx;
-  const uint64_t        units   = calc_request_units(ctx);
-  bool                  started = false;
-  if (!rc) return IN3_OK;
+in3_ret_t pay_eth_handle_request(in3_pay_eth_t* data, in3_pay_handle_ctx_t* plugin_ctx) {
+  in3_ctx_t*     ctx     = plugin_ctx->ctx;
+  const uint64_t units   = calc_request_units(ctx);
+  bool           started = false;
+  sb_t*          sb      = plugin_ctx->payload;
 
-  // TODO if we have a request Count > 1 we are currently still sending the same payload,
-  // but for payments, we may need different payload for each.
-  for (node_match_t* node = ctx->nodes; node; node = node->next) {
-    if (node->weight) {
-      if (node->weight->payed < units && node->weight->price && ctx->client->key) {
-        // we need to pay....
-        // TODO we don't even know if we have enough balance, we simply give it a try.
-
-        // TODO we should support ERC20-tokens instead of ether!
-
-        if (!started)
-          sb_add_chars(sb, ",\"pay\":{ \"type\":\"eth\", \"tx\":[");
-        else
-          sb_add_char(sb, ',');
-        started = true;
-
-        uint64_t val = conf->bulk_size * node->weight->price;
-        uint64_t v   = ctx->requests_configs->chain_id ? ctx->requests_configs->chain_id : ctx->client->chain_id;
-        if (v > 0xFF) v = 0; // this is only valid for ethereum chains.
-        create_signed_tx(ctx->client->key, sb, node->node->address->data, conf->bulk_size * node->weight->price, conf, v);
-        conf->nonce++;
-        node->weight->payed += val;
-      }
-    }
-  }
+  // fixme 666: interaction with nodelist?
+  //  // TODO if we have a request Count > 1 we are currently still sending the same payload,
+  //  // but for payments, we may need different payload for each.
+  //  for (node_match_t* node = ctx->nodes; node; node = node->next) {
+  //    if (node->weight) {
+  //      if (node->weight->payed < units && node->weight->price && ctx->client->key) {
+  //        // TODO we don't even know if we have enough balance, we simply give it a try.
+  //        // TODO we should support ERC20-tokens instead of ether!
+  //
+  //        if (!started)
+  //          sb_add_chars(sb, ",\"pay\":{ \"type\":\"eth\", \"tx\":[");
+  //        else
+  //          sb_add_char(sb, ',');
+  //        started = true;
+  //
+  //        uint64_t val = data->bulk_size * node->weight->price;
+  //        uint64_t v   = ctx->client->chain.chain_id;
+  //        if (v > 0xFF) v = 0; // this is only valid for ethereum chains.
+  //        create_signed_tx(ctx->client->key, sb, node->node->address->data, data->bulk_size * node->weight->price, data, v);
+  //        data->nonce++;
+  //        node->weight->payed += val;
+  //      }
+  //    }
+  //  }
 
   if (started) sb_add_chars(sb, "]}");
   return IN3_OK;
 }
+static in3_ret_t config_set(in3_pay_eth_t* data, in3_configure_ctx_t* ctx) {
+  char*       res   = NULL;
+  json_ctx_t* json  = ctx->json;
+  d_token_t*  token = ctx->token;
 
-char* pay_eth_configure(in3_t* c, d_token_t* config) {
-  if (c == NULL || config == NULL) return "missing config";
-  if (c->pay) {
-    if (c->pay->free && c->pay->cptr) c->pay->free(c->pay->cptr);
-    _free(c->pay);
+  if (token->key == key("bulkSize")) {
+    EXPECT_TOK_U64(token);
+    data->bulk_size = d_long(token);
   }
-  c->pay = _calloc(sizeof(in3_pay_t), 1);
-  if (!c->pay) return "no memory for payment";
-  c->pay->prepare            = pay_eth_prepare;
-  c->pay->handle_request     = pay_eth_handle_request;
-  c->pay->free               = pay_eth_free;
-  c->pay->follow_up          = pay_eth_follow_up;
-  c->pay->cptr               = _calloc(1, sizeof(in3_pay_eth_config_t));
-  in3_pay_eth_config_t* conf = c->pay->cptr;
-  conf->bulk_size            = d_get_longkd(config, key("bulkSize"), 1000);
-  conf->max_price            = d_get_longkd(config, key("maxPrice"), 10);
-  return NULL;
+  else if (token->key == key("maxPrice")) {
+    EXPECT_TOK_U64(token);
+    data->max_price = d_long(token);
+  }
+  else {
+    return IN3_EIGNORE;
+  }
+
+cleanup:
+  ctx->error_msg = res;
+  return ctx->error_msg ? IN3_ECONFIG : IN3_OK;
 }
 
-void in3_register_pay_eth() {
-  in3_register_payment("eth", pay_eth_configure);
+static in3_ret_t config_get(in3_pay_eth_t* data, in3_get_config_ctx_t* ctx) {
+  sb_t* sb = ctx->sb;
+  add_uint(sb, ',', "bulkSize", data->bulk_size);
+  add_uint(sb, ',', "maxPrice", data->max_price);
+  return IN3_OK;
+}
+
+in3_ret_t in3_pay_eth(void* plugin_data, in3_plugin_act_t action, void* plugin_ctx) {
+  in3_pay_eth_t* data = plugin_data;
+  switch (action) {
+    case PLGN_ACT_TERM:
+      _free(data);
+      return IN3_OK;
+    case PLGN_ACT_CONFIG_SET:
+      return config_set(data, (in3_configure_ctx_t*) plugin_ctx);
+    case PLGN_ACT_CONFIG_GET:
+      return config_get(data, (in3_get_config_ctx_t*) plugin_ctx);
+    case PLGN_ACT_PAY_PREPARE:
+      return pay_eth_prepare(data, plugin_ctx);
+    case PLGN_ACT_PAY_FOLLOWUP:
+      return pay_eth_follow_up(data, plugin_ctx);
+    case PLGN_ACT_PAY_HANDLE:
+      return pay_eth_handle_request(data, plugin_ctx);
+    default: break;
+  }
+  return IN3_EIGNORE;
+}
+
+in3_ret_t in3_register_pay_eth(in3_t* c) {
+  in3_pay_eth_t* data = _calloc(1, sizeof(*data));
+  data->bulk_size     = 1000;
+  data->max_price     = 10;
+  return in3_plugin_register(c, PLGN_ACT_TERM | PLGN_ACT_CONFIG_SET | PLGN_ACT_CONFIG_GET | PLGN_ACT_PAY_PREPARE | PLGN_ACT_PAY_FOLLOWUP | PLGN_ACT_PAY_HANDLE | PLGN_ACT_PAY_SIGN_REQ, in3_pay_eth, data, false);
 }
