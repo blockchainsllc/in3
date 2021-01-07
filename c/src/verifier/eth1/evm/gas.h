35d3b81b4060bd0df04e504eb81d3780455063bf
Leonardo Souza <lsouza@blockchains.com>
2021-01-07 10:52:15 +0100

    fix state modification gas not being deduced after CREATE op

--- c/src/verifier/eth1/evm/gas.h
+++ c/src/verifier/eth1/evm/gas.h
@@ -155,6 +155,8 @@ void update_account_code(evm_t* evm, account_t* new_account);
     /**************************************************************/                                       \
     bool no_address = false;                                                                               \
     if (!address) {                                                                                        \
+      /* Flag EVM with "creating transaction", so the gas for modifying state will be deduced later */     \
+      evm.properties |= EVM_PROP_TXCREATE;                                                                 \
       new_account = evm_create_account(&evm, evm.call_data.data, evm.call_data.len, code_address, caller); \
       no_address  = true;                                                                                  \
     }                                                                                                      \
