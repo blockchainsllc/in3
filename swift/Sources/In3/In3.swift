d0c405db4457782f1ed5f87d0b99d4078865dfba
Simon Jentzsch <simon@slock.it>
2021-04-03 18:23:23 +0200

    add RPC

--- swift/Sources/In3/In3.swift
+++ swift/Sources/In3/In3.swift
@@ -1,4 +1,5 @@
 import CIn3
+import Foundation
 
 enum IncubedError: Error {
     case config(message: String)
@@ -21,14 +22,33 @@ public class In3 {
 
   public init(_ config: String) throws {
     in3 = in3_for_chain_auto_init(1)
+    try configure(config)
+  }
+
+  deinit {
+    in3_free(in3)
+  }
+
+  public func configure(_ config: String) throws {
     let error = in3_configure(in3, config)
     if let msg = error {
       throw IncubedError.config(message: String(cString: msg))
     }
   }
 
-  deinit {
-    in3_free(in3)
+  public func execLocal(_ method: String, _ params: RPCObject) throws -> RPCObject {
+    let jsonReqData = try JSONEncoder().encode(JSONRequest(id: 1, method: method, params: JSONObject(params)))
+    let rawResult = execute(String(decoding: jsonReqData, as: UTF8.self))
+    let response = try JSONDecoder().decode(JSONResponse.self, from: rawResult.data(using: .utf8)!)
+    if let error = response.error {
+      throw IncubedError.rpc(message: error.message)
+    }
+    else if let result = response.result {
+      return RPCObject(result)
+    }
+    else {
+      throw IncubedError.rpc(message: "No Result in response")
+    }
   }
 
   public func execute(_ rpc: String) -> String {
--- swift/Tests/In3Tests/In3Tests.swift
+++ swift/Tests/In3Tests/In3Tests.swift
@@ -4,6 +4,14 @@ import XCTest
 final class In3Tests: XCTestCase {
     func testExample() throws {
         let in3 = try In3( "{\"chainId\":\"mainnet\"}")
+        let hash = try in3.execLocal("keccak",RPCObject(["simon"]))
+        switch hash {
+            case let .string(value):
+              XCTAssertEqual(value , "0x12c66c32d34a85291ac705641fb4d8cdf784dd6f84ecec01170f8d0735d54a4a")
+            default:
+              XCTAssertTrue(false, "Invalid return type")
+        }
+
         let res = in3.execute("{\"method\":\"keccak\",\"params\":[\"simon\"]}")
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
