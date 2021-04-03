7ea6de8c378626f43710dbbd12deab6e5ef47f5e
Simon Jentzsch <simon@slock.it>
2021-04-03 13:04:10 +0200

    fix macos-version

--- swift/Package.swift
+++ swift/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "In3",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
