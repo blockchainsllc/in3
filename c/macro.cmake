e657c79e3d73c8f8818f39293c79fb7b0e13b0e3
Simon Jentzsch <simon@slock.it>
2021-04-13 19:38:54 +0200

    define autoregister

--- c/macro.cmake
+++ c/macro.cmake
@@ -1,6 +1,6 @@
 
 macro(add_static_library )
-    cmake_parse_arguments(_LIB "" NAME "SOURCES;DEPENDS" ${ARGN} )
+    cmake_parse_arguments(_LIB "" "NAME;REGISTER" "SOURCES;DEPENDS" ${ARGN} )
 
     # create objects
     add_library(${_LIB_NAME}_o OBJECT ${_LIB_SOURCES})
@@ -12,4 +12,8 @@ macro(add_static_library )
     target_link_libraries(${_LIB_NAME} ${_LIB_DEPENDS})
     get_property(tmp GLOBAL PROPERTY IN3_OBJECTS)
     set_property(GLOBAL PROPERTY IN3_OBJECTS ${tmp} $<TARGET_OBJECTS:${_LIB_NAME}_o>)
+    if(_LIB_REGISTER)
+        get_property(tmp GLOBAL PROPERTY IN3_REGISTERS)
+        set_property(GLOBAL PROPERTY IN3_REGISTERS ${tmp} ${_LIB_REGISTER})
+    endif(_LIB_REGISTER)
 endmacro()
