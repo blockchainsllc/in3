5e1c27f73eec7fcf89c5be23d5167815b3274dc9
Simon Jentzsch <simon@slock.it>
2021-06-25 13:22:45 +0200

    fix dep

--- c/src/api/eth1/abi_parse.c
+++ c/src/api/eth1/abi_parse.c
@@ -3,6 +3,7 @@
 #include "../../core/util/bytes.h"
 #include "../../core/util/data.h"
 #include "../../core/util/mem.h"
+#include "../../core/util/stringbuilder.h"
 #include "../../core/util/utils.h"
 #include "abi.h"
 #include <stdbool.h>
