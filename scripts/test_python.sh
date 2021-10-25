38655561d983573c29cec9f2ce04f7f32c35cc07
leonardotc <leonardo.t.cardoso@slock.it>
2021-10-25 12:54:00 +0200

    Fix python build

--- scripts/test_python.sh
+++ scripts/test_python.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+MAC_FILE=../build/lib/libin3.dylib
+LINUX_FILE=../build/lib/libin3.so
+
 CWD=$PWD
 cd $(dirname $0)/../build
 make
 cd ../python
 mkdir -p in3/libin3/shared
-cp ../build/lib/libin3.dylib in3/libin3/shared/libin3.x64.dylib
+if test -f "$LINUX_FILE"; then
+    cp "$LINUX_FILE" in3/libin3/shared/libin3.x64.so
+else 
+    cp "$MAC_FILE" in3/libin3/shared/libin3.x64.dylib
+fi
+
 pip3 install -r requirements.txt
 coverage run -m pytest --pylama --junitxml=report.xml
 cd $CWD
