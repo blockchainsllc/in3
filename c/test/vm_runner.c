9b41371c35164e111b47304463af0fbdf6d6451d
Simon Jentzsch <simon@slock.it>
2022-01-29 18:16:16 +0100

    no memory check

--- c/test/vm_runner.c
+++ c/test/vm_runner.c
@@ -1,34 +1,34 @@
 /*******************************************************************************
  * This file is part of the Incubed project.
  * Sources: https://github.com/blockchainsllc/in3
- * 
+ *
  * Copyright (C) 2018-2020 slock.it GmbH, Blockchains LLC
- * 
- * 
+ *
+ *
  * COMMERCIAL LICENSE USAGE
- * 
- * Licensees holding a valid commercial license may use this file in accordance 
- * with the commercial license agreement provided with the Software or, alternatively, 
- * in accordance with the terms contained in a written agreement between you and 
- * slock.it GmbH/Blockchains LLC. For licensing terms and conditions or further 
+ *
+ * Licensees holding a valid commercial license may use this file in accordance
+ * with the commercial license agreement provided with the Software or, alternatively,
+ * in accordance with the terms contained in a written agreement between you and
+ * slock.it GmbH/Blockchains LLC. For licensing terms and conditions or further
  * information please contact slock.it at in3@slock.it.
- * 	
+ *
  * Alternatively, this file may be used under the AGPL license as follows:
- *    
+ *
  * AGPL LICENSE USAGE
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Affero General Public License as published by the Free Software 
+ * terms of the GNU Affero General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
- * [Permissions of this strong copyleft license are conditioned on making available 
- * complete source code of licensed works and modifications, which include larger 
- * works using a licensed work, under the same license. Copyright and license notices 
+ * [Permissions of this strong copyleft license are conditioned on making available
+ * complete source code of licensed works and modifications, which include larger
+ * works using a licensed work, under the same license. Copyright and license notices
  * must be preserved. Contributors provide an express grant of patent rights.]
- * You should have received a copy of the GNU Affero General Public License along 
+ * You should have received a copy of the GNU Affero General Public License along
  * with this program. If not, see <https://www.gnu.org/licenses/>.
  *******************************************************************************/
 
@@ -128,10 +128,6 @@ int run_test(json_ctx_t* jc, d_token_t* test, int counter, char* name, uint32_t 
     print_error("Unknown TestType!");
   }
 
-  if (mem_get_memleak_cnt()) {
-    in3_log_debug(" -- Memory Leak detected by malloc #%i!", mem_get_memleak_cnt());
-    if (!fail) fail = 1;
-  }
   if (!fail) print_success("OK");
 
   in3_log_debug(" ( %" PRIu64 " ms) ", ms);
@@ -157,6 +153,9 @@ int runRequests(char** names, int test_index, uint32_t props) {
       ERROR("Error parsing the requests");
       return -1;
     }
+
+    //    for (int i = 0; i < parsed->len; i++) d_to_bytes(parsed->result + i);
+
     // parse the data;
     int        i;
     char*      str_proof = NULL;
