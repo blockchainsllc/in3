2610b565ffae969d6adcc1dd8fee1163a8aa61ee
Simon Jentzsch <simon@slock.it>
2022-04-01 22:51:43 +0200

    fix signing

--- c/src/verifier/eth1/basic/sign_tx.c
+++ c/src/verifier/eth1/basic/sign_tx.c
@@ -183,7 +183,7 @@ static in3_ret_t get_nonce_and_gasprice(eth_tx_data_t* tx, in3_req_t* ctx) {
 /** gets the v-value from the chain_id */
 static inline uint64_t get_v(chain_id_t chain) {
   uint64_t v = chain;
-  if (v > 0xFF && v != 1337) v = 0; // this is only valid for ethereum chains.
+  if (v == CHAIN_ID_IPFS) v = 0; // this is only valid for ethereum chains.
   return v;
 }
 
