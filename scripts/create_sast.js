7c8b0bee807b2a13be93034fcc85797baa3b8e96
Simon Jentzsch <simon@slock.it>
2022-04-17 00:40:04 +0200

    add valgrind

--- scripts/create_sast.js
+++ scripts/create_sast.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const crypto = require('crypto')
 const path = require("path")
+const { url } = require("inspector")
 var stdinBuffer = fs.readFileSync(0)
 const input = stdinBuffer.toString().split('\n').filter(_ => !(_.startsWith('cc1:') || _.startsWith('make[') || _.startsWith('make:')))
 const tool = process.argv.pop()
@@ -8,10 +9,62 @@ const root = path.resolve(process.argv.pop())
 
 //input.forEach(_ => console.log('::: ' + _))
 const res = []
-input.forEach(line => {
+input.forEach((line, i) => {
+    const valgrind = /==([0-9]+)==\sERROR SUMMARY:\s([0-9]+)\ errors/g;
     const regex = /^(\/.*?):([0-9]+):([0-9]+):\s*([a-z]+):\s*(.*)/m;
     let m;
-    if ((m = regex.exec(line)) !== null) {
+    if ((m = valgrind.exec(line)) !== null && m[2] != '0') {
+        let content = ''
+        let last_path = ''
+        let last_line = 0
+        let last_method = ''
+        let is_malloc = false
+        //        ==2129==    by 0x4243F6: eth_set_pk_signer (c/src/signer/pk-signer/signer.c:215)
+
+        for (let n = i; n >= 0; n--) {
+            const r = /==([0-9]+)==.*:([0-9a-zA-Z_]+)\s*\(([0-9a-zA-Z_/\.]+):([0-9]+)\)/g;
+            if (!input[n].startsWith('==' + m[1] + '==')) break;
+            let mm = r.exec(input[n])
+            if (mm && !is_malloc) {
+                last_line = parseInt(mm[4])
+                last_path = mm[3]
+                last_method = mm[2]
+            }
+            else if (!is_malloc && input[n].indexOf('malloc') >= 0) is_malloc = true
+            content = input[n].substring(4 + m[1].length) + '\n' + content
+        }
+        res.push({
+            id,
+            category: 'sast',
+            message: 'Valgrind Memory Leak in ' + last_method,
+            cve: '',
+            scanner: {
+                id: tool,
+                name: tool
+            },
+            description: '',
+            raw_source_code_extract: '',
+            severity: 'Critical',
+            location: {
+                file: last_path,
+                start_line: last_line
+            },
+            details: {
+                path: {
+                    type: "code",
+                    lang: 'c',
+                    value: content
+                }
+            },
+            confidence: 'High',
+            identifiers: [{
+                "type": tool,
+                "name": tool + " warning",
+                "value": tool
+            }]
+        })
+    }
+    else if ((m = regex.exec(line)) !== null) {
         const [all, path, lin, col, level, message] = m
         if (level == 'note' || path.indexOf('third-party') > 0) return
         const id = crypto.createHash('sha256')
@@ -48,6 +101,21 @@ input.forEach(line => {
                 "value": tool
             }]
         })
+
+        const cwe = /\[CWE-([0-9]+)\]/g
+        if ((m = regex.exec(message)) !== null) {
+            res[res.length - 1].details.more = {
+                type: 'url',
+                href: 'https://cwe.mitre.org/data/definitions/' + m[1] + '.html'
+            }
+            res[res.length - 1].identifiers.push({
+                "type": "cwe",
+                "name": "CWE-" + m[1],
+                "value": m[1],
+                "url": "https://cwe.mitre.org/data/definitions/" + m[1] + ".html"
+            })
+
+        }
     }
     else if (res.length) {
         res[res.length - 1].raw_source_code_extract += '\n' + line
