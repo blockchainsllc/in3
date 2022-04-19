ebdec2302735937db8a67d839fb8635f873fe5d9
Simon Jentzsch <simon@slock.it>
2022-04-19 09:35:26 +0200

    fix rpc and check for types

--- scripts/generator/validate_schema.js
+++ scripts/generator/validate_schema.js
@@ -2,14 +2,76 @@ const fs = require('fs')
 const jsonschema = require('jsonschema')
 const yaml = require('yaml')
 const schema = require('./rpc_schema.json')
-
-process.argv.filter(_ => _.endsWith('rpc.yml')).forEach(file => {
+const files = process.argv.filter(_ => _.endsWith('.yml'))
+let types = {}
+let apis = {}
+let content = {}
+files.forEach(file => {
     const data = yaml.parse(fs.readFileSync(file, 'utf8'))
-    const result = jsonschema.validate(data, schema)
-    if (result.errors.length) {
-        for (let err of result.errors)
-            console.log('Error in ' + file + ' : ' + err.toString().replace('instance.', ''))
-        process.exitCode = 1
+    content[file] = data
+    if (file.endsWith("rpc.yml")) {
+        Object.keys(data.types || {}).forEach(_ => types[_] = data.types[_])
+        Object.keys(data).filter(_ => _ != 'types').forEach(_ => apis[_] = { ...apis[_], ...data[_] })
+        const result = jsonschema.validate(data, schema)
+        if (result.errors.length) {
+            for (let err of result.errors)
+                console.error('Error in ' + file + ' : ' + err.toString().replace('instance.', ''))
+            process.exitCode = 1
+        }
     }
 })
 
+files.forEach(file => {
+    const data = content[file]
+    if (file.endsWith("rpc.yml")) {
+        Object.keys(data.types || {}).forEach(type => {
+            check_properties("Error in " + file + ': The predefined type ' + type, data.types[type])
+        })
+        Object.keys(data).filter(_ => !_.startsWith('types')).forEach(api => {
+            const api_data = data[api]
+            Object.keys(api_data).filter(_ => !_.startsWith('_') && _ != 'fields' && _ != 'descr' && _ != 'config').forEach(func => {
+                check_properties("Error in " + file + ': The function ' + func + ' in params', api_data[func].params || {})
+            })
+
+        })
+
+    }
+})
+
+function check_properties(prefix, props) {
+    Object.keys(props).filter(_ => !_.startsWith('_')).forEach(prop => {
+        check_type(prefix + ` with property ${prop} : `, props[prop].type)
+    })
+
+}
+
+function check_type(prefix, t) {
+    if (!t) return
+    if (typeof (t) === 'object') {
+        return check_properties(prefix, t)
+    }
+    switch (t) {
+        case 'uint32':
+        case 'uint64':
+        case 'uint16':
+        case 'bytes':
+        case 'bytes32':
+        case 'bytes96':
+        case 'address':
+        case 'bool':
+        case 'uint256':
+        case 'int':
+        case 'byte':
+        case 'bytes256':
+        case 'bytes128':
+        case 'bytes4':
+        case 'any':
+        case 'string':
+        case 'double':
+            return
+        default:
+            if (types[t]) return
+    }
+    console.error(prefix + ` Type '${t}' is unknown!`)
+    process.exitCode = 1
+}
