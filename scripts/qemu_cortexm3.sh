9d4cf27f0f8b2765d0aeb1868243957d38b48405
Vipin Das Balachandran <vvdbalachandran@blockchains.com>
2022-04-26 23:27:53 +0200

    updated zephyr image
    * image updated to zephyr-build:v0.23.1
    
    Issue: 829

--- scripts/qemu_cortexm3.sh
+++ scripts/qemu_cortexm3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+ls -la /opt/toolchains/zephyr-sdk-0.14.1/sysroots/x86_64-pokysdk-linux/usr/bin/
 # qemu invocation: execute in3 for cortex m3 get receipt and process the out to test_out.txt
-qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -net none -pidfile qemu.pid -serial mon:stdio -semihosting -kernel build/zephyr/zephyr.elf 2> /dev/null > test_out.txt
+/opt/toolchains/zephyr-sdk-0.14.1/sysroots/x86_64-pokysdk-linux/usr/bin/qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -net none -pidfile qemu.pid -serial mon:stdio -semihosting --no-reboot -kernel build/zephyr/zephyr.elf > test_out.txt
+
 # search for the result of the test
 grep -i PASSED ./test_out.txt
 out="$?"
