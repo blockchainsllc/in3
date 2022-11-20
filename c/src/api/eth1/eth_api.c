7d0a1940a63240eb9718f03026b2a00baabe454d
Simon Jentzsch <sjentzsch@blockchains.com>
2022-11-20 08:16:32 +0100

    fix checksum

--- c/src/api/eth1/eth_api.c
+++ c/src/api/eth1/eth_api.c
@@ -733,7 +733,7 @@ bytes_t* eth_sendRawTransaction(in3_t* in3, bytes_t data) {
 
 in3_ret_t to_checksum(address_t adr, chain_id_t chain_id, char out[43]) {
   char tmp[64], msg[41], *hexadr;
-  int  p = chain_id ? sprintf(tmp, "%i0x", (uint32_t) chain_id) : 0;
+  int  p = chain_id ? snprintx(tmp, 64, "%U0x", (uint64_t) chain_id) : 0;
   bytes_to_hex(adr, 20, tmp + p);
   hexadr = tmp + p;
   bytes32_t hash;
